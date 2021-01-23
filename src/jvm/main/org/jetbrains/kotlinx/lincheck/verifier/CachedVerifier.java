2d087fca3bc7a15bedd49d4999a0482bcae4abaa
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-01-23 16:51:23 +0300

    Do not save crashes

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/verifier/CachedVerifier.java
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/verifier/CachedVerifier.java
@@ -37,7 +37,9 @@ public abstract class CachedVerifier implements Verifier {
 
     @Override
     public boolean verifyResults(ExecutionScenario scenario, ExecutionResult results) {
-        boolean newResult = previousResults.computeIfAbsent(scenario, s -> new HashSet<>()).add(results);
+        // Stacktrace is a large object, that is useless for verification, as it is used only for report.
+        ExecutionResult resultWithoutCrashes = ExecutionResultKt.getWithoutCrashes(results);
+        boolean newResult = previousResults.computeIfAbsent(scenario, s -> new HashSet<>()).add(resultWithoutCrashes);
         if (!newResult) return true;
         return verifyResultsImpl(scenario, results);
     }
