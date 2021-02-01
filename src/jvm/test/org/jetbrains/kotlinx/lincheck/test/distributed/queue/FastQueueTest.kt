1427ce3a02726df95eda33da5047a1a82c77318e
Olga Lupuleac <olga.lupuleac@gmail.com>
2021-02-01 17:08:00 +0300

    Separate message queue from DistributedRunner

---
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck/test/distributed/queue/FastQueueTest.kt
@@ -0,0 +1,51 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2021 JetBrains s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>
+ */
+
+package org.jetbrains.kotlinx.lincheck.test.distributed.queue
+
+import org.jetbrains.kotlinx.lincheck.Options
+import org.jetbrains.kotlinx.lincheck.annotations.OpGroupConfig
+import org.jetbrains.kotlinx.lincheck.annotations.Operation
+import org.jetbrains.kotlinx.lincheck.distributed.queue.FastQueue
+import org.jetbrains.kotlinx.lincheck.test.AbstractLincheckTest
+import java.util.concurrent.LinkedBlockingQueue
+
+
+
+class FastQueueTest : AbstractLincheckTest() {
+    private val queue = FastQueue<Int>()
+
+    @Operation
+    fun put(value: Int) = queue.put(value)
+
+    @Operation
+    fun poll() = queue.poll()
+
+    override fun <O : Options<O, *>> O.customize() {
+        //actorsBefore(2)
+        threads(4)
+        actorsPerThread(2)
+        //actorsAfter(2)
+       // iterations(1)
+        //minimizeFailedScenario(true)
+        //verifier(EpsilonVerifier::class.java)
+        requireStateEquivalenceImplCheck(false)
+    }
+}
