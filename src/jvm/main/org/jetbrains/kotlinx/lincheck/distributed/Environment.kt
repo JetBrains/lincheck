a8cadeec982cd26f0c9339b551404a8e0e05121f
Olga Lupuleac <olga.lupuleac@gmail.com>
2021-02-26 22:23:35 +0300

    Modify 'Environment' interface

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/distributed/Environment.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/distributed/Environment.kt
@@ -1,7 +1,5 @@
 package org.jetbrains.kotlinx.lincheck.distributed
 
-import java.util.concurrent.TimeUnit
-
 /**
  * Environment interface for communication with other processes.
  */
@@ -17,17 +15,9 @@ interface Environment<Message, Log> {
     val numberOfNodes: Int
 
     /**
-     * Returns the identifier of the process of the exact class.
-     * E.g.: if there are several clients and one server, the client
-     * can get the server id.
-     *
-     * @param cls is a class which instance is to be identified
-     * @param i is the index
-     * @return
-     */
-    fun getAddress(cls: Class<out Node<Message>>, i: Int): Int
-
-    fun getNumberOfNodeType(cls: Class<out Node<Message>>) : Int
+     * Returns identifiers of nodes of the exact class [cls].
+     **/
+    fun getAddressesForClass(cls: Class<out Node<Message>>): List<Int>
 
     /**
      * Sends the specified [message] to the process [receiver] (from 0 to [numberOfNodes]).
@@ -47,27 +37,46 @@ interface Environment<Message, Log> {
         }
     }
 
-    val log : MutableList<Log>
+    /**
+     * Logs for current node.
+     */
+    val log: MutableList<Log>
 
     /**
-     * Sends local message. Can be used to point out that some action is completed
-     * (e.g. message is delivered to a user, or lock is acquired). After the end of the execution all local messages are available and can be checked by the user.
+     * Test execution events for all nodes (including sending and receiving messages,
+     * node failures, logs and etc.)
+     * Should be called only in validation functions to check the invariants.
      */
-    fun sendLocal(message: Message)
-
     val events: List<Event>
+
+    /**
+     * Causes the execution to await until the signal is received.
+     */
+    suspend fun await()
+
+    /**
+     * Resumes the awaiting execution.
+     */
+    fun signal()
 }
 
 sealed class Event
 data class MessageSentEvent<Message>(val message: Message, val sender: Int, val receiver: Int, val id: Int) : Event()
-data class MessageReceivedEvent<Message>(val message: Message, val sender: Int, val receiver: Int, val id: Int) : Event()
-data class LocalMessageSentEvent<Message>(val message: Message, val sender: Int) : Event()
+data class MessageReceivedEvent<Message>(val message: Message, val sender: Int, val receiver: Int, val id: Int) :
+    Event()
+
+data class LogEvent<Log>(val message: Log, val sender: Int) : Event()
 data class ProcessFailureEvent(val processId: Int) : Event()
 data class ProcessRecoveryEvent(val processId: Int) : Event()
 
-fun <Message, Log> Environment<Message, Log>.correctProcesses() = (0 until numberOfNodes).subtract(events.filterIsInstance<ProcessFailureEvent>().map { it.processId })
-fun <Message, Log> Environment<Message, Log>.sentMessages(processId: Int = nodeId) = events.filterIsInstance<MessageSentEvent<Message>>().filter { it.sender == processId }
-fun <Message, Log> Environment<Message, Log>.receivedMessages(processId: Int = nodeId) = events.filterIsInstance<MessageReceivedEvent<Message>>().filter { it.receiver == processId }
-fun <Message, Log> Environment<Message, Log>.localMessages(processId: Int = nodeId) = events.filterIsInstance<LocalMessageSentEvent<Message>>().filter { it.sender == processId }.map { it.message }
+fun <Message, Log> Environment<Message, Log>.correctProcesses() =
+    (0 until numberOfNodes).subtract(events.filterIsInstance<ProcessFailureEvent>().map { it.processId })
+
+fun <Message, Log> Environment<Message, Log>.sentMessages(processId: Int = nodeId) =
+    events.filterIsInstance<MessageSentEvent<Message>>().filter { it.sender == processId }
+
+fun <Message, Log> Environment<Message, Log>.receivedMessages(processId: Int = nodeId) =
+    events.filterIsInstance<MessageReceivedEvent<Message>>().filter { it.receiver == processId }
+
 fun <Message> List<Message>.isDistinct(): Boolean = distinctBy { System.identityHashCode(it) } == this
 fun <Message, Log> Environment<Message, Log>.isCorrect() = correctProcesses().contains(nodeId)
