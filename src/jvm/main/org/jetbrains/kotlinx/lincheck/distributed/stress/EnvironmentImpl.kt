bfec6a5dc447eff51ed2f1610dc6554584183393
Olga Lupuleac <olga.lupuleac@gmail.com>
2021-03-03 00:52:36 +0300

    Extract EnvironmentImp into separate file

---
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/distributed/stress/EnvironmentImpl.kt
@@ -0,0 +1,98 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2021 JetBrains s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>
+ */
+
+package org.jetbrains.kotlinx.lincheck.distributed.stress
+
+import kotlinx.coroutines.sync.Semaphore
+import org.jetbrains.kotlinx.lincheck.distributed.Environment
+import org.jetbrains.kotlinx.lincheck.distributed.Event
+import org.jetbrains.kotlinx.lincheck.distributed.MessageSentEvent
+import org.jetbrains.kotlinx.lincheck.distributed.Node
+import java.lang.IllegalArgumentException
+
+internal class EnvironmentImpl<Message, Log>(
+    val context: DistributedRunnerContext<Message, Log>,
+    override val nodeId: Int,
+    override val log: MutableList<Log> = mutableListOf()
+) :
+    Environment<Message, Log> {
+    companion object {
+        const val TICK_TIME = 1
+    }
+
+    override val numberOfNodes = context.addressResolver.totalNumberOfNodes
+
+    private val probability = Probability(context.tesCfg, numberOfNodes)
+
+    override fun getAddressesForClass(cls: Class<out Node<Message>>) = context.addressResolver[cls]
+
+    override suspend fun send(message: Message, receiver: Int) {
+        logMessage(LogLevel.ALL_EVENTS) {
+            "[$nodeId]: In send $isFinished"
+        }
+        if (isFinished) {
+            return
+        }
+        if (context.failureInfo[nodeId] || context.failureInfo[receiver]) {
+            return
+            //TODO: it doesn't work:(
+        }
+        if (probability.nodeFailed() &&
+            context.failureInfo.trySetFailed(nodeId)
+        ) {
+            throw NodeFailureException(nodeId)
+        }
+        context.incClock(nodeId)
+
+        val event = MessageSentEvent(
+            message = message,
+            sender = nodeId,
+            receiver = receiver,
+            id = context.messageId.getAndIncrement(),
+            clock = context.vectorClock[nodeId].copyOf()
+        )
+        val aaa = if (nodeId == receiver) {
+            "AAAAAAAAAAAAAAAAAAAA"
+        } else {
+            ""
+        }
+        logMessage(LogLevel.ALL_EVENTS) {
+            "[$nodeId]: $aaa Before sending $event"
+        }
+        context.events[nodeId].add(event)
+        repeat(probability.duplicationRate()) {
+            context.incomeMessages[event.receiver].send(event)
+            logMessage(LogLevel.ALL_EVENTS) {
+                "[$nodeId]: Send $event to $receiver ${context.incomeMessages[event.receiver].hashCode()}"
+            }
+        }
+    }
+
+    override fun events(): Array<List<Event>> = if (isFinished) {
+        context.events.map { it.toList() }.toTypedArray()
+    } else {
+        throw IllegalAccessException("Cannot access events until the execution is over ")
+    }
+
+    @Volatile
+    internal var isFinished = false
+    override suspend fun <T> withTimeout(ticks: Int, block: suspend () -> T): T? =
+        withTimeout(ticks * TICK_TIME, block)
+}
