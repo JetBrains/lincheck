69d691635f3b06d42a4fe72f460eb0e4b766de6e
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-03-25 01:28:49 +0300

    Add TAS failing tests

--- src/jvm/test/org/jetbrains/kotlinx/lincheck/test/verifier/nlr/TestAndSetTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck/test/verifier/nlr/TestAndSetTest.kt
@@ -23,6 +23,7 @@
 package org.jetbrains.kotlinx.lincheck.test.verifier.nlr
 
 import org.jetbrains.kotlinx.lincheck.LinChecker
+import org.jetbrains.kotlinx.lincheck.LincheckAssertionError
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
 import org.jetbrains.kotlinx.lincheck.annotations.Param
 import org.jetbrains.kotlinx.lincheck.annotations.Recoverable
@@ -31,8 +32,8 @@ import org.jetbrains.kotlinx.lincheck.nvm.api.nonVolatile
 import org.jetbrains.kotlinx.lincheck.paramgen.ThreadIdGen
 import org.jetbrains.kotlinx.lincheck.strategy.stress.StressCTest
 import org.jetbrains.kotlinx.lincheck.verifier.VerifierState
+import org.junit.Ignore
 import org.junit.Test
-import java.util.concurrent.atomic.AtomicInteger
 
 private const val THREADS_NUMBER = 5
 
@@ -63,13 +64,6 @@ internal class SequentialTestAndSet : VerifierState() {
     fun testAndSet() = value.also { value = 1 }
     fun testAndSet(ignore: Int) = testAndSet()
     override fun extractState() = value
-}
-
-internal class LinearizableTestAndSet : VerifierState() {
-    private val value = AtomicInteger(0)
-
-    fun testAndSet() = if (value.compareAndSet(0, 1)) 0 else 1
-    public override fun extractState() = value.get()
 }
 
 /**
@@ -124,9 +118,250 @@ internal class NRLTestAndSet(private val threadsCount: Int) : VerifierState(), T
         r[p].setAndFlush(3)
         return returnValue
     }
+}
 
-    private inline fun wailUntil(condition: () -> Boolean) {
-        while (!condition()) {
-        }
+private inline fun wailUntil(condition: () -> Boolean) {
+    while (!condition()) {
     }
 }
+
+@StressCTest(
+    sequentialSpecification = SequentialTestAndSet::class,
+    threads = THREADS_NUMBER,
+    recover = Recover.NRL,
+    actorsBefore = 0,
+    actorsPerThread = 1,
+    minimizeFailedScenario = false
+)
+internal abstract class TestAndSetFailingTest {
+    private val tas = createTAS()
+    abstract fun createTAS(): TAS
+
+    @Operation
+    fun testAndSet(@Param(gen = ThreadIdGen::class) threadId: Int) = tas.testAndSet(threadId)
+
+    @Test(expected = LincheckAssertionError::class)
+    fun test() = LinChecker.check(this::class.java)
+}
+
+internal class TestAndSetFailingTest1 : TestAndSetFailingTest() {
+    override fun createTAS() = NRLFailingTestAndSet1(THREADS_NUMBER + 2)
+}
+
+internal class TestAndSetFailingTest2 : TestAndSetFailingTest() {
+    override fun createTAS() = NRLFailingTestAndSet2(THREADS_NUMBER + 2)
+}
+
+internal class TestAndSetFailingTest3 : TestAndSetFailingTest() {
+    override fun createTAS() = NRLFailingTestAndSet3(THREADS_NUMBER + 2)
+}
+
+@Ignore("Can't find an error. To be fixed")
+internal class TestAndSetFailingTest4 : TestAndSetFailingTest() {
+    override fun createTAS() = NRLFailingTestAndSet4(THREADS_NUMBER + 2)
+}
+
+internal class NRLFailingTestAndSet1(private val threadsCount: Int) : VerifierState(), TAS {
+    private val r = MutableList(threadsCount) { nonVolatile(0) }
+    private val response = MutableList(threadsCount) { nonVolatile(0) }
+    private val winner = nonVolatile(-1)
+    private val doorway = nonVolatile(true)
+    private val tas = nonVolatile(0)
+
+    override fun extractState() = tas.value
+
+    @Recoverable(recoverMethod = "testAndSetRecover")
+    override fun testAndSet(p: Int): Int {
+        r[p].setAndFlush(1)
+        val returnValue: Int
+        if (!doorway.value) {
+            returnValue = 1
+        } else {
+            r[p].setAndFlush(2)
+            // here should be doorway.setAndFlush(false)
+            returnValue = if (tas.compareAndSet(0, 1)) 0 else 1
+            if (returnValue == 0) {
+                winner.setAndFlush(p)
+            }
+        }
+        response[p].setAndFlush(returnValue)
+        r[p].setAndFlush(3)
+        return returnValue
+    }
+
+    private fun testAndSetRecover(p: Int): Int {
+        if (r[p].value < 2) return testAndSet(p)
+        if (r[p].value == 3) return response[p].value
+        if (winner.value == -1) {
+            doorway.setAndFlush(false)
+            r[p].setAndFlush(4)
+            for (i in 0 until p) {
+                wailUntil { r[i].value.let { it == 0 || it == 3 } }
+            }
+            for (i in p + 1 until threadsCount) {
+                wailUntil { r[i].value.let { it == 0 || it > 2 } }
+            }
+            if (winner.value == -1) {
+                winner.setAndFlush(p)
+            }
+        }
+        val returnValue = if (winner.value == p) 0 else 1
+        response[p].setAndFlush(returnValue)
+        r[p].setAndFlush(3)
+        return returnValue
+    }
+}
+
+internal class NRLFailingTestAndSet2(private val threadsCount: Int) : VerifierState(), TAS {
+    private val r = MutableList(threadsCount) { nonVolatile(0) }
+    private val response = MutableList(threadsCount) { nonVolatile(0) }
+    private val winner = nonVolatile(-1)
+    private val doorway = nonVolatile(true)
+    private val tas = nonVolatile(0)
+
+    override fun extractState() = tas.value
+
+    @Recoverable(recoverMethod = "testAndSetRecover")
+    override fun testAndSet(p: Int): Int {
+        r[p].setAndFlush(1)
+        val returnValue: Int
+        if (!doorway.value) {
+            returnValue = 1
+        } else {
+            r[p].setAndFlush(2)
+            doorway.setAndFlush(false)
+            returnValue = if (tas.compareAndSet(0, 1)) 0 else 1
+            if (returnValue == 0) {
+                winner.setAndFlush(p)
+            }
+        }
+        response[p].setAndFlush(returnValue)
+        r[p].setAndFlush(3)
+        return returnValue
+    }
+
+    private fun testAndSetRecover(p: Int): Int {
+        if (r[p].value < 2) return testAndSet(p)
+        if (r[p].value == 3) return response[p].value
+        if (winner.value == -1) {
+            // here should be doorway.setAndFlush(false)
+            r[p].setAndFlush(4)
+            for (i in 0 until p) {
+                wailUntil { r[i].value.let { it == 0 || it == 3 } }
+            }
+            for (i in p + 1 until threadsCount) {
+                wailUntil { r[i].value.let { it == 0 || it > 2 } }
+            }
+            if (winner.value == -1) {
+                winner.setAndFlush(p)
+            }
+        }
+        val returnValue = if (winner.value == p) 0 else 1
+        response[p].setAndFlush(returnValue)
+        r[p].setAndFlush(3)
+        return returnValue
+    }
+}
+
+internal class NRLFailingTestAndSet3(private val threadsCount: Int) : VerifierState(), TAS {
+    private val r = MutableList(threadsCount) { nonVolatile(0) }
+    private val response = MutableList(threadsCount) { nonVolatile(0) }
+    private val winner = nonVolatile(-1)
+    private val doorway = nonVolatile(true)
+    private val tas = nonVolatile(0)
+
+    override fun extractState() = tas.value
+
+    @Recoverable(recoverMethod = "testAndSetRecover")
+    override fun testAndSet(p: Int): Int {
+        r[p].setAndFlush(1)
+        val returnValue: Int
+        if (!doorway.value) {
+            returnValue = 1
+        } else {
+            r[p].setAndFlush(2)
+            doorway.setAndFlush(false)
+            returnValue = if (tas.compareAndSet(0, 1)) 0 else 1
+            if (returnValue == 0) {
+                winner.setAndFlush(p)
+            }
+        }
+        response[p].setAndFlush(returnValue)
+        r[p].setAndFlush(3)
+        return returnValue
+    }
+
+    private fun testAndSetRecover(p: Int): Int {
+        if (r[p].value < 2) return testAndSet(p)
+        if (r[p].value == 3) return response[p].value
+        if (winner.value == -1) {
+            doorway.setAndFlush(false)
+            r[p].setAndFlush(4)
+            for (i in 0 until /* here should be p*/ p - 1) {
+                wailUntil { r[i].value.let { it == 0 || it == 3 } }
+            }
+            for (i in p + 1 until threadsCount) {
+                wailUntil { r[i].value.let { it == 0 || it > 2 } }
+            }
+            if (winner.value == -1) {
+                winner.setAndFlush(p)
+            }
+        }
+        val returnValue = if (winner.value == p) 0 else 1
+        response[p].setAndFlush(returnValue)
+        r[p].setAndFlush(3)
+        return returnValue
+    }
+}
+
+internal class NRLFailingTestAndSet4(private val threadsCount: Int) : VerifierState(), TAS {
+    private val r = MutableList(threadsCount) { nonVolatile(0) }
+    private val response = MutableList(threadsCount) { nonVolatile(0) }
+    private val winner = nonVolatile(-1)
+    private val doorway = nonVolatile(true)
+    private val tas = nonVolatile(0)
+
+    override fun extractState() = tas.value
+
+    @Recoverable(recoverMethod = "testAndSetRecover")
+    override fun testAndSet(p: Int): Int {
+        r[p].setAndFlush(1)
+        val returnValue: Int
+        if (!doorway.value) {
+            returnValue = 1
+        } else {
+            r[p].setAndFlush(2)
+            doorway.setAndFlush(false)
+            returnValue = if (tas.compareAndSet(0, 1)) 0 else 1
+            if (returnValue == 0) {
+                winner.setAndFlush(p)
+            }
+        }
+        response[p].setAndFlush(returnValue)
+        r[p].setAndFlush(3)
+        return returnValue
+    }
+
+    private fun testAndSetRecover(p: Int): Int {
+        if (r[p].value < 2) return testAndSet(p)
+        if (r[p].value == 3) return response[p].value
+        if (winner.value == -1) {
+            doorway.setAndFlush(false)
+            r[p].setAndFlush(4)
+            for (i in 0 until p) {
+                wailUntil { r[i].value.let { it == 0 || it == 3 } }
+            }
+            for (i in p + 1 until threadsCount) {
+                wailUntil { r[i].value.let { it == 0 || it > 2 } }
+            }
+            if (winner.value == -1) {
+                winner.setAndFlush(p)
+            }
+        }
+        val returnValue = if (winner.value == p) 0 else 1
+        // here should be response[p].setAndFlush(returnValue)
+        r[p].setAndFlush(3)
+        return returnValue
+    }
+}
+
