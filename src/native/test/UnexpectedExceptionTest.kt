8dc92a56bc49d5dc758b487bd22fcf5ff09b9e53
Krock21rus <krock21rus@gmail.com>
2021-05-06 12:34:15 +0300

    Fix UnexpecedExceptionTest

--- src/native/test/UnexpectedExceptionTest.kt
+++ src/native/test/UnexpectedExceptionTest.kt
@@ -38,8 +38,8 @@ class UnexpectedExceptionTest : AbstractLincheckStressTest<UnexpectedExceptionTe
     override fun extractState(): Any = canEnterForbiddenSection
 
     override fun <T : LincheckStressConfiguration<UnexpectedExceptionTest>> T.customize() {
-        iterations(250)
-        invocationsPerIteration(100)
+        iterations(500)
+        invocationsPerIteration(250)
 
         initialState { UnexpectedExceptionTest() }
 
