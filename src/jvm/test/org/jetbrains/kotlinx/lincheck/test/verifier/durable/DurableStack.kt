3797e6d6ac087e6ce38eddf3e4adaa565fbf2907
Maksim Zuev <maksim.zuev@jetbrains.com>
2021-08-02 18:52:45 +0300

    Add stack test

---
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck/test/verifier/durable/DurableStack.kt
@@ -0,0 +1,74 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2021 JetBrains s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>
+ */
+
+package org.jetbrains.kotlinx.lincheck.test.verifier.durable
+
+import org.jetbrains.kotlinx.lincheck.annotations.Operation
+import org.jetbrains.kotlinx.lincheck.nvm.Recover
+import org.jetbrains.kotlinx.lincheck.nvm.api.nonVolatile
+import org.jetbrains.kotlinx.lincheck.test.verifier.nlr.AbstractNVMLincheckTest
+import org.jetbrains.kotlinx.lincheck.verifier.VerifierState
+
+private const val THREADS = 3
+
+class DurableStackTest : AbstractNVMLincheckTest(Recover.DURABLE, THREADS, SequentialStack::class, false) {
+    private val stack = DurableStack()
+
+    @Operation
+    fun push(v: Int): Unit = stack.push(v)
+
+    @Operation
+    fun pop(): Int? = stack.pop()
+}
+
+internal class SequentialStack : VerifierState() {
+    private val stack = ArrayDeque<Int>()
+    fun push(v: Int) = stack.addLast(v)
+    fun pop() = stack.removeLastOrNull()
+    override fun extractState() = stack.toList()
+}
+
+private class Node(val next: Node? = null, val value: Int = 0)
+
+internal class DurableStack {
+    private val head = nonVolatile(Node())
+
+    fun push(v: Int) {
+        while (true) {
+            val first = head.value
+            if (head.compareAndSet(first, Node(first, v))) {
+                head.flush()
+                return
+            }
+        }
+    }
+
+    fun pop(): Int? {
+        while (true) {
+            val first = head.value
+            val next = first.next
+            if (next === null) return null
+            if (head.compareAndSet(first, next)) {
+                head.flush()
+                return first.value
+            }
+        }
+    }
+}
