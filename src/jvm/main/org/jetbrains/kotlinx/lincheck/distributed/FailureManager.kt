e34c56ac2deaed72f23558ec50f32441dcebbe85
Olga Lupuleac <olga.lupuleac@gmail.com>
2022-02-14 15:25:43 +0300

    Add comments to FailureManagerComponents

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/distributed/FailureManager.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/distributed/FailureManager.kt
@@ -104,12 +104,19 @@ internal abstract class FailureManager<Message>(
     abstract fun reset()
 }
 
+/**
+ * Implementation of FailureManager when each partition splits the nodes of a certain type into two components.
+ */
 internal class FailureManagerComponent<Message>(
     addressResolver: NodeAddressResolver<Message>,
     private val strategy: DistributedStrategy<Message>
 ) :
     FailureManager<Message>(addressResolver) {
+    // For each class store partitions. First element of the pair is a smaller part, second is a larger part.
+    // Initially, first part is empty.
     private val partitions = mutableMapOf<Class<out Node<Message>>, Pair<MutableSet<Int>, MutableSet<Int>>>()
+
+    // Number of unavailable nodes for each type. Node is unavailable if it is crashed or belongs to the smaller partition.
     private val unavailableNodeCount = mutableMapOf<Class<out Node<Message>>, Int>()
 
     init {
@@ -117,27 +124,42 @@ internal class FailureManagerComponent<Message>(
     }
 
     override fun canSend(sender: Int, receiver: Int): Boolean {
+        // If one of the node is crashed the message cannot be sent.
         if (crashedNodes[sender] || crashedNodes[receiver]) {
             return false
         }
         val cls1 = addressResolver[sender]
         val cls2 = addressResolver[receiver]
         if (cls1 != cls2) {
+            // The smaller part is isolated from nodes of different type.
+            // If the nodes have different type and one belongs to a smaller part,
+            // the message cannot be sent.
             return partitions[cls1]!!.second.contains(sender)
                     && partitions[cls2]!!.second.contains(receiver)
         }
+        // If sender and receiver has the same type, check if they belong to the same part.
         return partitions[cls1]!!.first.containsAll(listOf(sender, receiver))
                 || partitions[cls1]!!.second.containsAll(listOf(sender, receiver))
     }
 
+    /**
+     * Increments number of unavailable node if necessary.
+     * Note that it is called before the node is set to crashed or moved to the smaller part.
+     */
     private fun incrementCrashedNodes(iNode: Int) {
         val cls = addressResolver[iNode]
+        // If the node was already counted as unavailable, do nothing.
         if (crashedNodes[iNode] || partitions[cls]!!.first.contains(iNode)) return
         unavailableNodeCount[cls] = unavailableNodeCount[cls]!! + 1
     }
 
+    /**
+     * Decrements number of unavailable node if necessary.
+     * Note that it is called before the node is set to recovered or moved to the larger part.
+     */
     private fun decrementCrashedNodes(iNode: Int) {
         val cls = addressResolver[iNode]
+        // If the node is both crashed and belongs to the smaller part it will remain unavailable, so do nothing.
         if (crashedNodes[iNode] && partitions[cls]!!.first.contains(iNode)) return
         unavailableNodeCount[cls] = unavailableNodeCount[cls]!! - 1
     }
@@ -149,9 +171,12 @@ internal class FailureManagerComponent<Message>(
     }
 
     override fun canCrash(iNode: Int): Boolean {
+        // If the node is already crashed return false.
         if (crashedNodes[iNode]) return false
         val cls = addressResolver[iNode]
-        return unavailableNodeCount[cls]!! < addressResolver.maxNumberOfCrashes(cls)
+        // Node is already unavailable or max number of unavailable nodes is not reached.
+        return partitions[cls]!!.first.contains(iNode)
+                || unavailableNodeCount[cls]!! < addressResolver.maxNumberOfCrashes(cls)
     }
 
     override fun recoverNode(iNode: Int) {
@@ -160,20 +185,40 @@ internal class FailureManagerComponent<Message>(
         crashedNodes[iNode] = false
     }
 
+    /**
+     * Checks if firstNode can be added to the smaller part.
+     */
     override fun canAddPartition(firstNode: Int, secondNode: Int): Boolean {
         val cls = addressResolver[firstNode]
+        // firstNode is in the larger part and unavailable nodes limit is not reached.
+        // firstNode is not crashed, because otherwise this method will not be called.
         return unavailableNodeCount[cls]!! < addressResolver.maxNumberOfCrashes(cls)
                 && partitions[cls]!!.second.contains(firstNode)
     }
 
+    /**
+     * Rebuilds the partition for the type of [firstNode]. After that the firstNode will belong to the smaller parts with some other nodes
+     * which are chosen using [strategy].
+     * [secondNode] will belong to the larger part for its type.
+     */
     override fun addPartition(firstNode: Int, secondNode: Int): Pair<List<Int>, List<Int>> {
+        // Move firstNode to smaller part.
         addNodeToPartition(firstNode)
+        // Move second node to larger part
         removeNodeFromPartition(secondNode)
         val cls = addressResolver[firstNode]
+        // Get all nodes which can be included to the smaller part with firsNode.
         val nodes = addressResolver.nodeTypeToRange[cls]!!.filter { it != firstNode && it != secondNode }
+        // Max number of nodes which can be added to the smaller part.
+        // Because the first part should remain smaller, and it already contains firsNode,
+        // there should be added no more than (n / 2 - 1) nodes of its type.
         val limit =
-            min(addressResolver.maxNumberOfCrashes(cls) - unavailableNodeCount[cls]!!, addressResolver[cls].size / 2)
+            min(
+                addressResolver.maxNumberOfCrashes(cls) - unavailableNodeCount[cls]!!,
+                addressResolver[cls].size / 2 - 1
+            )
         val nodesForPartition = strategy.choosePartitionComponent(nodes, limit)
+        // Rebuild the partition and recalculate unavailable node count.
         for (node in nodes) {
             if (node in nodesForPartition) {
                 addNodeToPartition(node)
@@ -182,19 +227,29 @@ internal class FailureManagerComponent<Message>(
             }
         }
         val firstPart = nodesForPartition + firstNode
+        // Return the resulting parts: small part and all remaining nodes.
+        // Note that second part may contain different node types, which can be also split to parts.
         return firstPart to (0 until addressResolver.nodeCount).filter { it !in firstPart }
     }
 
+    /**
+     * Adds [iNode] to the smaller part.
+     */
     private fun addNodeToPartition(iNode: Int) {
         val cls = addressResolver[iNode]
+        // It is already in the smaller part, do nothing.
         if (partitions[cls]!!.first.contains(iNode)) return
         incrementCrashedNodes(iNode)
         partitions[cls]!!.second.remove(iNode)
         partitions[cls]!!.first.add(iNode)
     }
 
+    /**
+     * Adds [iNode] to the larger part.
+     */
     private fun removeNodeFromPartition(iNode: Int) {
         val cls = addressResolver[iNode]
+        // It is already in the larger part, do nothing.
         if (partitions[cls]!!.second.contains(iNode)) return
         decrementCrashedNodes(iNode)
         partitions[cls]!!.first.remove(iNode)
