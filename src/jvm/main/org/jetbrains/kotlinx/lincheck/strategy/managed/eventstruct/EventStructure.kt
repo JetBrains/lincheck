d1e93429769dc077120b7d64a1c0ab0e90950077
Evgenii Moiseenko <evg.moiseenko94@gmail.com>
2022-06-21 13:24:42 +0200

    implement `addReadEvent` and `addWriteEvent` methods
    * redesign synchronization operation, assume it is associative and commutative, `EmptyLabel` is unit
    * rename `Event.kt` --> `EventLabel.kt`, declare `Event` class in `EventStructure.kt`
    * implement revisiting in `addReadEvent` and `addWriteEvent`, but do not check porf-acyclicity, consistency, conflict-freeness and do not use indexing (use simple linear scan)
    
    Signed-off-by: Evgenii Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstruct/EventStructure.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstruct/EventStructure.kt
@@ -20,9 +20,66 @@
 
 package org.jetbrains.kotlinx.lincheck.strategy.managed.eventstruct
 
-import org.jetbrains.kotlinx.lincheck.strategy.managed.*
+import org.jetbrains.kotlinx.lincheck.strategy.managed.MemoryTracker
+import kotlin.collections.set
+
+class Event private constructor(
+    val id: EventID,
+    /**
+     * Event's position in a thread
+     * (i.e. number of its program-order predecessors).
+     */
+    val threadPos : Int = 0,
+    /**
+     * Event's label.
+     */
+    val label: EventLabel = EmptyLabel(),
+    /**
+     * Event's predecessor in program order.
+     */
+    val pred: Event? = null,
+    /**
+     * List of event's dependencies
+     * (e.g. reads-from write for a read event).
+     */
+    val deps: List<Event> = listOf()
+) {
+    /**
+     * List of event's children in program order.
+     *
+     * TODO: this should be carefully garbage collected upon restriction of event structure!
+     */
+    val children: MutableList<Event> = mutableListOf()
+
+    companion object {
+        private var nextId: EventID = 0
+
+        fun create(label: EventLabel, pred: Event?, deps: List<Event>): Event {
+            val id = nextId++
+            val threadPos = pred?.let { it.threadPos + 1 } ?: 0
+            val event = Event(id,
+                threadPos = threadPos,
+                label = label,
+                pred = pred,
+                deps = deps,
+            )
+            pred?.also { it.children.add(event) }
+            return event
+        }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        return (other is Event) && (id == other.id)
+    }
+
+    override fun hashCode(): Int {
+        return id.hashCode()
+    }
+}
 
 class EventStructure {
+
+
     // TODO: this pattern is covered by explicit backing fields KEEP
     //   https://github.com/Kotlin/KEEP/issues/278
     private val _events: ArrayList<Event> = arrayListOf()
@@ -40,46 +97,89 @@ class EventStructure {
      */
     private val programCounter: MutableMap<Int, Event> = mutableMapOf()
 
-    fun addEvent(iThread: Int, lab: EventLabel, deps: List<Event>) {
+    private fun createEvent(lab: EventLabel, deps: List<Event>): Event {
         // TODO: check that the given thread is initialized
-        val pred = programCounter[iThread]!!
-        val event = Event.create(iThread, lab, pred, deps)
-        _events.add(event)
-        programCounter[iThread] = event
+        val pred = programCounter[lab.threadId]!!
+        // we remove dependencies which coincide with program order predecessor of the new event,
+        // because we assume that as a result of synchronization the resulting event
+        // has the same threadId as one of the operands and should be put immediately after it in program order.
+        return Event.create(lab, pred, deps.filter { it != pred })
     }
 
-    fun addEvents(iThread: Int, labs: Collection<Pair<EventLabel, List<Event>>>): Event? {
-        // TODO: check that the given thread is initialized
-        val pred = programCounter[iThread]!!
-        val events = labs.map { (lab, deps) -> Event.create(iThread, lab, pred, deps) }
-        _events.addAll(events)
-        // TODO: use some other strategy to select the next event in the current exploration?
-        return events.lastOrNull()?.also { programCounter[iThread] = it }
+    private fun addEvent(lab: EventLabel, deps: List<Event>): Event {
+        val event = createEvent(lab, deps)
+        _events.add(event)
+        return event
     }
+
+    private fun addEvents(labsWithDeps: Collection<Pair<EventLabel, List<Event>>>): List<Event> {
+        val events = labsWithDeps.map { (lab, deps) -> createEvent(lab, deps) }
+        _events.addAll(events)
+        return events
+    }
+
+    fun addWriteEvent(iThread: Int, memoryLocationId: Int, value: Any?, typeDescriptor: String) {
+        val writeLabel = MemoryAccessLabel(
+            threadId = iThread,
+            kind = MemoryAccessKind.Write,
+            typeDesc = typeDescriptor,
+            memId = memoryLocationId,
+            value = value
+        )
+        val writeEvent = addEvent(writeLabel, listOf())
+        programCounter[iThread] = writeEvent
+        // TODO: check porf-acyclicity
+        // TODO: check consistency of reads!
+        // TODO: filter conflicting events!
+        val readLabelsWithDeps = getSynchronizedLabelsWithDeps(writeEvent)
+        // TODO: sort candidate events according to some strategy?
+        addEvents(readLabelsWithDeps)
+    }
+
+    fun addReadEvent(iThread: Int, memoryLocationId: Int, typeDescriptor: String): Any? {
+        // we first create read-request event with unknown (null) value, it will be filled in later
+        val readRequestLabel = MemoryAccessLabel(
+            threadId = iThread,
+            kind = MemoryAccessKind.ReadRequest,
+            typeDesc = typeDescriptor,
+            memId = memoryLocationId,
+            value = null
+        )
+        val readRequestEvent = addEvent(readRequestLabel, listOf())
+        programCounter[iThread] = readRequestEvent
+        // TODO: check consistency of reads!
+        // TODO: filter conflicting events!
+        val readLabelsWithDeps = getSynchronizedLabelsWithDeps(readRequestEvent)
+        // TODO: sort candidate events according to some strategy?
+        val readEvents = addEvents(readLabelsWithDeps)
+        // TODO: use some other strategy to select the next event in the current exploration?
+        val chosenRead = readEvents.lastOrNull()?.also { programCounter[iThread] = it }
+        return chosenRead?.let { (it.label as MemoryAccessLabel).value }
+    }
+
+    /**
+     * Returns a list of labels obtained as a result of synchronizing given event [event]
+     * with the events contained in the event structure, along with the list of dependencies.
+     * That is, if `e1 @ A` is the passed event labeled by `A` and
+     * `e @ B` is some event in the event structures labeled by `B`, then the resulting list
+     * will contain pair `(C = A \+ B, listOf(A, B))` if `C` is defined (i.e. not null).
+     */
+    private fun getSynchronizedLabelsWithDeps(event: Event): Collection<Pair<EventLabel, List<Event>>> =
+        // TODO: instead of linear scan we should maintain an index of read/write accesses to specific memory location
+        events.mapNotNull {
+            val syncLab = event.label.synchronize(it.label) ?: return@mapNotNull null
+            val deps = listOf(event, it)
+            syncLab to deps
+        }
 
 }
 
-class EventStructMemoryTracker(val eventStructure: EventStructure): MemoryTracker() {
-    override fun writeValue(iThread: Int, memoryLocationId: Int, value: Any?, typeDescriptor: String) {
-        // TODO: fix typeDesc
-        val lab = MemoryAccessLabel(MemoryAccessKind.Write, typeDescriptor, memoryLocationId, value)
-        eventStructure.addEvent(iThread, lab, listOf())
-    }
+class EventStructureMemoryTracker(val eventStructure: EventStructure): MemoryTracker() {
 
-    override fun readValue(iThread: Int, memoryLocationId: Int, typeDescriptor: String): Any? {
-        // we first create read label with unknown (null) value, it will be filled in later
-        // TODO: as a possible optimization we can avoid one allocation of temporarily label object here
-        //   by refactoring EventLabel.synchronize function. However, current design with
-        //   EventLabel.synchronize taking just two labels is more clear and concise.
-        val preLab = MemoryAccessLabel(MemoryAccessKind.Read, typeDescriptor, memoryLocationId, null)
-        // TODO: instead of linear scan we should maintain an index of read/write accesses to specific memory location
-        // TODO: check consistency of reads!
-        val labs = eventStructure.events.mapNotNull {
-            val lab = it.label.synchronize(preLab) ?: return@mapNotNull null
-            val deps = listOf(it)
-            return lab to deps
-        }
-        val chosenRead = eventStructure.addEvents(iThread, labs)
-        return chosenRead?.let { (it.label as MemoryAccessLabel).value }
-    }
+    override fun writeValue(iThread: Int, memoryLocationId: Int, value: Any?, typeDescriptor: String) =
+        eventStructure.addWriteEvent(iThread, memoryLocationId, value, typeDescriptor)
+
+    override fun readValue(iThread: Int, memoryLocationId: Int, typeDescriptor: String): Any? =
+        eventStructure.addReadEvent(iThread, memoryLocationId, typeDescriptor)
+
 }
