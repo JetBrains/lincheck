23cdd9cca911a63bbbb265267e121757ac9db821
Alexander Fedorov <fedorov2602@mail.ru>
2022-11-09 18:51:13 +0100

    Uncommented some previously commented tests

--- src/jvm/test/org/jetbrains/kotlinx/lincheck/test/verifier/AllowExtraSuspensionTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck/test/verifier/AllowExtraSuspensionTest.kt
@@ -31,76 +31,75 @@ import org.jetbrains.kotlinx.lincheck.test.*
 import org.jetbrains.kotlinx.lincheck.verifier.*
 import java.util.concurrent.atomic.*
 
-// TODO: support AFU/VarHandle/Unsafe for memory tracking and uncomment this test
-//class AllowExtraSuspensionCorrectTest : AbstractLincheckTest() {
-//    private val mutex = Mutex()
-//    private var counter = AtomicInteger()
-//
-//    @Operation(allowExtraSuspension = true)
-//    suspend fun inc(): Int = mutex.withLock {
-//        counter.getAndIncrement()
-//    }
-//
-//    @Operation
-//    suspend fun dec() = counter.getAndDecrement()
-//
-//    override fun <O : Options<O, *>> O.customize() {
-//        sequentialSpecification(CounterSequential::class.java)
-//    }
-//}
-//
-//class AllowExtraSuspensionIncorrectTest : AbstractLincheckTest(IncorrectResultsFailure::class) {
-//    private val mutex = Mutex()
-//    private var counter = AtomicInteger()
-//
-//    @Operation
-//    suspend fun inc(): Int = mutex.withLock {
-//        counter.getAndIncrement()
-//    }
-//
-//    @Operation
-//    suspend fun dec() = counter.getAndDecrement()
-//
-//    override fun <O : Options<O, *>> O.customize() {
-//        sequentialSpecification(CounterSequential::class.java)
-//    }
-//}
-//
-//// One of the operations should always succeed without suspension
-//class OnlyExtraSuspensionsHaveToBeAtomicTest : AbstractLincheckTest() {
-//    private val c = atomic(0)
-//
-//    @InternalCoroutinesApi
-//    @Operation(cancellableOnSuspension = true)
-//    suspend fun operation1() {
-//        val c = c.incrementAndGet()
-//        if (c == 6) return
-//        suspendCancellableCoroutine<Unit> {  }
-//    }
-//
-//    @InternalCoroutinesApi
-//    @Operation(allowExtraSuspension = true)
-//    suspend fun operation2() {
-//        val c = c.incrementAndGet()
-//        if (c == 6) return
-//        suspendCancellableCoroutine<Unit> {  }
-//    }
-//
-//    override fun <O : Options<O, *>> O.customize() {
-//        iterations(10)
-//        actorsBefore(0)
-//        threads(2)
-//        actorsPerThread(3)
-//        actorsAfter(0)
-//        requireStateEquivalenceImplCheck(false)
-//    }
-//}
-//
-//class CounterSequential : VerifierState() {
-//    private var counter = 0
-//
-//    suspend fun inc(): Int = counter++
-//    suspend fun dec() = counter--
-//
-//    override fun extractState() = counter
-//}
+class AllowExtraSuspensionCorrectTest : AbstractLincheckTest() {
+    private val mutex = Mutex()
+    private var counter = AtomicInteger()
+
+    @Operation(allowExtraSuspension = true)
+    suspend fun inc(): Int = mutex.withLock {
+        counter.getAndIncrement()
+    }
+
+    @Operation
+    suspend fun dec() = counter.getAndDecrement()
+
+    override fun <O : Options<O, *>> O.customize() {
+        sequentialSpecification(CounterSequential::class.java)
+    }
+}
+
+class AllowExtraSuspensionIncorrectTest : AbstractLincheckTest(IncorrectResultsFailure::class) {
+    private val mutex = Mutex()
+    private var counter = AtomicInteger()
+
+    @Operation
+    suspend fun inc(): Int = mutex.withLock {
+        counter.getAndIncrement()
+    }
+
+    @Operation
+    suspend fun dec() = counter.getAndDecrement()
+
+    override fun <O : Options<O, *>> O.customize() {
+        sequentialSpecification(CounterSequential::class.java)
+    }
+}
+
+// One of the operations should always succeed without suspension
+class OnlyExtraSuspensionsHaveToBeAtomicTest : AbstractLincheckTest() {
+    private val c = atomic(0)
+
+    @InternalCoroutinesApi
+    @Operation(cancellableOnSuspension = true)
+    suspend fun operation1() {
+        val c = c.incrementAndGet()
+        if (c == 6) return
+        suspendCancellableCoroutine<Unit> {  }
+    }
+
+    @InternalCoroutinesApi
+    @Operation(allowExtraSuspension = true)
+    suspend fun operation2() {
+        val c = c.incrementAndGet()
+        if (c == 6) return
+        suspendCancellableCoroutine<Unit> {  }
+    }
+
+    override fun <O : Options<O, *>> O.customize() {
+        iterations(10)
+        actorsBefore(0)
+        threads(2)
+        actorsPerThread(3)
+        actorsAfter(0)
+        requireStateEquivalenceImplCheck(false)
+    }
+}
+
+class CounterSequential : VerifierState() {
+    private var counter = 0
+
+    suspend fun inc(): Int = counter++
+    suspend fun dec() = counter--
+
+    override fun extractState() = counter
+}
