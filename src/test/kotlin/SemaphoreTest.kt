da68f358680d2eb8d85c84807e7e76c4cb2bf7dc
Alexander Fedorov <fedorov2602@mail.ru>
2023-02-06 00:38:46 +0100

    Added SemaphoreTest & description

---
+++ src/test/kotlin/SemaphoreTest.kt
@@ -0,0 +1,41 @@
+import org.jetbrains.kotlinx.lincheck.Options
+import org.jetbrains.kotlinx.lincheck.annotations.Operation
+import org.jetbrains.kotlinx.lincheck.check
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import org.junit.Test
+
+class SemaphoreTest {
+    private val semaphore = java.util.concurrent.Semaphore(1, true)
+
+    @Operation(cancellableOnSuspension = true)
+    fun acquire() {
+        semaphore.acquire()
+    }
+
+    @Operation
+    fun release() {
+        semaphore.release()
+    }
+
+//    TODO: does not work yet: uses custom lincheck branch.
+//    Note that the report is already present at reports.md
+//    @Test
+    fun test() {
+        ModelCheckingOptions()
+            .actorsBefore(0)
+            .actorsAfter(0)
+            .sequentialSpecification(SemaphoreSequential::class.java)
+            .check(this::class.java)
+    }
+}
+
+class SemaphoreSequential {
+    private val s = kotlinx.coroutines.sync.Semaphore(100, 99)
+
+    suspend fun acquire() {
+        s.acquire()
+    }
+    fun release() {
+        s.release()
+    }
+}
