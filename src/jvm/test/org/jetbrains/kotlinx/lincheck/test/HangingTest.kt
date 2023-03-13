aeadbf8e247abe618a6286b3350d867a7d590c07
Evgenii Moiseenko <evg.moiseenko94@gmail.com>
2023-03-13 17:22:34 +0100

    working on a test robustly reproducing bugs due to incorrect abort of hanged invocations
    Signed-off-by: Evgenii Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck/test/HangingTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck/test/HangingTest.kt
@@ -23,7 +23,12 @@ package org.jetbrains.kotlinx.lincheck.test
 
 import org.jetbrains.kotlinx.lincheck.*
 import org.jetbrains.kotlinx.lincheck.annotations.*
+import org.jetbrains.kotlinx.lincheck.annotations.Operation
 import org.jetbrains.kotlinx.lincheck.strategy.*
+import org.jetbrains.kotlinx.lincheck.strategy.managed.eventstructure.*
+import org.jetbrains.kotlinx.lincheck.verifier.*
+import java.util.concurrent.atomic.AtomicInteger
+import org.junit.Test
 
 class HangingTest : AbstractLincheckTest(DeadlockWithDumpFailure::class) {
     @Operation
@@ -38,6 +43,81 @@ class HangingTest : AbstractLincheckTest(DeadlockWithDumpFailure::class) {
         requireStateEquivalenceImplCheck(false)
         minimizeFailedScenario(false)
         invocationTimeout(100)
+    }
+
+}
+
+/* This test checks that strategy correctly aborts hanging invocations so that
+ * these invocations do not interfere with subsequent invocations (e.g. by writing to global shared memory).
+ */
+class HangingAbortTest {
+
+    companion object {
+        private var global: Int = 0
+    }
+
+    private val counter = AtomicInteger(0)
+
+    @Operation
+    fun badOperation() {
+        check(global == 0)
+        // some dummy code with shared memory accesses to create switch points
+        repeat(10) { counter.incrementAndGet() }
+        // TODO: model checking strategy may eliminate `sleep()` calls,
+        //   we need a more robust approach to enforce the behavior that we expect here.
+        Thread.sleep(200)
+        global = 1
+    }
+
+    private val testScenario = scenario {
+        parallel {
+            thread {
+                actor(HangingAbortTest::badOperation)
+            }
+            thread {
+                actor(HangingAbortTest::badOperation)
+            }
+        }
+    }
+
+    /* TODO: currently we can only guarantee the abort behavior from the model checking strategy,
+     *   because this strategy can implement necessary checks before all potentially dangerous interfering operations
+     *   (e.g. accesses to global shared memory).
+     *   Because stress strategy should use more lightweight instrumentation we cannot afford (?)
+     *   to perform similar checks before every interesting operation (e.g. before every access to global shared memory).
+     *   We need to think about potential solutions for this problem that would work for stress strategy.
+     */
+    @Test
+    fun modelCheckingTest() {
+        // repeat the test several times in order to try to check that aborted invocation
+        // does not interfere with the subsequent invocations
+        val strategy = createStrategy()
+        repeat(10) {
+            val failure: LincheckFailure? = strategy.run()
+            assert(failure is DeadlockWithDumpFailure)
+        }
+    }
+
+    fun EventStructureOptions.customize() {
+        requireStateEquivalenceImplCheck(false)
+        minimizeFailedScenario(false)
+        invocationTimeout(100)
+    }
+
+    private fun createConfiguration() =
+        EventStructureOptions()
+            .apply { customize() }
+            .createTestConfigurations(this::class.java)
+
+    private fun createStrategy(): EventStructureStrategy {
+        return createConfiguration()
+            .createStrategy(
+                testClass = this::class.java,
+                scenario = testScenario,
+                verifier = EpsilonVerifier(this::class.java),
+                validationFunctions = listOf(),
+                stateRepresentationMethod = null,
+            )
     }
 
 }
