e22b0becd58773ed0aa3a3c6a3fcf8de591d76fd
SampathKumarAmex <sampathkumar.krishnasamykuppusamy@aexp.com>
2023-04-04 12:27:33 +0100

    ShortGen to kotlin

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/paramgen/ShortGen.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/paramgen/ShortGen.kt
@@ -1,36 +1,33 @@
-package org.jetbrains.kotlinx.lincheck.paramgen;
-
 /*
- * #%L
  * Lincheck
- * %%
- * Copyright (C) 2015 - 2018 Devexperts, LLC
- * %%
+ *
+ * Copyright (C) 2019 - 2023 JetBrains s.r.o.
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Lesser Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Lesser Public
  * License along with this program.  If not, see
- * <http://www.gnu.org/licenses/lgpl-3.0.html>.
- * #L%
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>
  */
 
-public class ShortGen implements ParameterGenerator<Short> {
-    private final IntGen intGen;
+package org.jetbrains.kotlinx.lincheck.paramgen
 
-    public ShortGen(String configuration) {
-        intGen = new IntGen(configuration);
-        intGen.checkRange(Short.MIN_VALUE, Short.MAX_VALUE, "short");
+class ShortGen(configuration: String) : ParameterGenerator<Short> {
+
+    private var intGen: IntGen? = null
+
+    init {
+        intGen = IntGen(configuration)
+        intGen?.checkRange(Short.MIN_VALUE.toInt(), Short.MAX_VALUE.toInt(), "short")
     }
 
-    public Short generate() {
-        return (short) (int) intGen.generate();
-    }
+    override fun generate(): Short = (intGen?.generate() as Int).toShort()
 }
