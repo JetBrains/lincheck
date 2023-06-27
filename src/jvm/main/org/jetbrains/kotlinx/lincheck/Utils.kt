314fc43597871dbad4fb10374e7ea30eb78ef4cb
Aleksandr.Potapov <aleksandr.potapov@jetbrains.com>
2023-06-28 01:49:42 +0400

    Performance issue fixed

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/Utils.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/Utils.kt
@@ -333,7 +333,8 @@ internal val String.canonicalClassName get() = this.replace('/', '.')
 internal val String.internalClassName get() = this.replace('.', '/')
 
 internal fun exceptionCanBeValidExecutionResult(exception: Throwable): Boolean {
-    return exception !is InternalLincheckTestUnexpectedException &&
+    return exception !is ThreadDeath && // used to stop thread in FixedActiveThreadsExecutor by calling thread.stop method
+            exception !is InternalLincheckTestUnexpectedException &&
             exception !is ForcibleExecutionFinishException &&
             !isIllegalAccessOfUnsafeDueToJavaVersion(exception)
 }
