95d2e6123bc1361a45d6ed3b861cebb6ba6c6396
Evgenii Moiseenko <evg.moiseenko94@gmail.com>
2023-08-21 21:30:22 +0200

    implement plot: invocation average time by scenario size
    Signed-off-by: Evgenii Moiseenko <evg.moiseenko94@gmail.com>

--- scripts/benchmarks_plotter.py
+++ scripts/benchmarks_plotter.py
@@ -49,11 +49,14 @@ class BenchmarksReport:
     def benchmark_mode(self, benchmark_id: int):
         return self._data[str(benchmark_id)]["mode"]
 
+    def benchmark_scenarios_stats(self, benchmark_id: int):
+        return self._data[str(benchmark_id)]["scenariosStatistics"]
+
     def benchmark_runtime(self, benchmark_id: int, unit: TimeUnit = TimeUnit.MILLI):
         return as_time_unit(self._data[str(benchmark_id)]["runningTimeNano"], unit)
 
     def benchmarks_runtime(self, unit: TimeUnit = TimeUnit.MILLI):
-        return [self.benchmark_runtime(bid, unit=unit) for bid in self.benchmark_ids()]
+        return {bid: self.benchmark_runtime(bid, unit=unit) for bid in self.benchmark_ids()}
 
     def benchmarks_runtime_grouped_by_name(self, unit: TimeUnit = TimeUnit.MILLI):
         result = dict()
@@ -71,23 +74,31 @@ class BenchmarksReport:
             result[self.benchmark_name(bid)] = self.benchmark_runtime(bid, unit=unit)
         return result
 
-# def runtime_plot(ax: plt.Axes, benchmarks: List[str], runtime: List[int]):
-#     xs = np.arange(len(benchmarks))
-#     ax.bar(xs, runtime, align="center")
-#     ax.set_xticks(xs, benchmarks)
-#     ax.set_ylabel("running time")
-#     ax.set_title("Benchmarks running time")
+    def benchmark_average_runtime_by_scenario_params(self, benchmark_id: int, unit: TimeUnit = TimeUnit.MILLI):
+        result = list()
+        groups = groupby(
+            self.benchmark_scenarios_stats(benchmark_id),
+            lambda stats: (stats["threads"], stats["operations"])
+        )
+        for params, iterations in groups:
+            iterations = list(iterations)
+            runtime = np.sum(list(map(itemgetter("runningTimeNano"), iterations)))
+            invocations = np.sum(list(map(itemgetter("invocationsCount"), iterations)))
+            avg_runtime = as_time_unit(float(runtime) / invocations, unit)
+            result.append((params, avg_runtime))
+        return result
+
 
 def runtime_plot(ax: plt.Axes, report: BenchmarksReport):
     width = 0.25
     multiplier = 0
     time_unit = TimeUnit.SEC
-    max_runtime = max(report.benchmarks_runtime(unit=time_unit))
+    max_runtime = max(report.benchmarks_runtime(unit=time_unit).values())
     x = np.arange(len(report.benchmarks_names()))
     for mode in report.benchmarks_modes():
         offset = width * multiplier
         data = report.benchmarks_runtime_with_mode(mode=mode, unit=time_unit).values()
-        ax.bar(x + offset, data, width, label=mode)
+        ax.bar(x + offset, data, width=width, label=mode)
         multiplier += 1
     ax.set_title("Benchmarks running time")
     ax.set_ylabel("time (s)")
@@ -95,17 +106,36 @@ def runtime_plot(ax: plt.Axes, report: BenchmarksReport):
     ax.set_xticks(x + width / 2, report.benchmarks_names(), rotation=45)
     ax.legend(loc="upper left", bbox_to_anchor=(1, 1), ncol=1)
 
+
+def scenarios_average_runtime_plot(ax: plt.Axes, report: BenchmarksReport, benchmark_id: int):
+    width = 0.25
+    data = report.benchmark_average_runtime_by_scenario_params(benchmark_id, unit=TimeUnit.MILLI)
+    data.sort(key=itemgetter(0))
+    scenario_params = list(map(itemgetter(0), data))
+    runtimes = list(map(itemgetter(1), data))
+    x = np.arange(len(data))
+    ax.bar(x, runtimes, width=width)
+    ax.set_title("Invocation average running time by scenario size")
+    ax.set_ylabel("time (ms)")
+    ax.set_xlabel("(#threads, #operations)")
+    ax.set_xticks(x, scenario_params)
+
+
 def round_up_to(n, multiplier):
     return math.ceil(n / multiplier) * multiplier
+
 
 def main():
     with open("benchmarks-results.json", 'r') as json_file:
         data = json.load(json_file)
     report = BenchmarksReport(data)
-    runtime_plot(plt.axes(), report)
+    fig, ax = plt.subplots(nrows=2, ncols=1)
+    fig.set_figwidth(6)
+    fig.set_figheight(8)
+    runtime_plot(ax[0], report)
+    scenarios_average_runtime_plot(ax[1], report, benchmark_id=1)
     plt.tight_layout()
     plt.show()
-
 
 
 if __name__ == "__main__":
