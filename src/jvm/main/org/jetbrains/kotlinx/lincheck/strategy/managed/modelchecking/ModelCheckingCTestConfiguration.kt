eupp/kotlinx-lincheck

run tracker API
Signed-off-by: Evgenii Moiseenko <evg.moiseenko94@gmail.com>

src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/modelchecking/ModelCheckingCTestConfiguration.kt
@@ -43,7 +43,10 @@ class ModelCheckingCTestConfiguration(testClass: Class<*>, iterations: Int, thre
     eliminateLocalObjects = eliminateLocalObjects,
     customScenarios = customScenarios
 ) {
-    override fun createStrategy(testClass: Class<*>, scenario: ExecutionScenario, validationFunctions: List<Method>,
-                                stateRepresentationMethod: Method?, verifier: Verifier): Strategy
-        = ModelCheckingStrategy(this, testClass, scenario, validationFunctions, stateRepresentationMethod, verifier)
+    override fun createStrategy(
+        testClass: Class<*>,
+        scenario: ExecutionScenario,
+        validationFunctions: List<Method>,
+        stateRepresentationMethod: Method?,
+    ): Strategy = ModelCheckingStrategy(this, testClass, scenario, validationFunctions, stateRepresentationMethod)
 }

src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/modelchecking/ModelCheckingStrategy.kt
@@ -38,12 +38,7 @@ internal class ModelCheckingStrategy(
         scenario: ExecutionScenario,
         validationFunctions: List<Method>,
         stateRepresentation: Method?,
-        verifier: Verifier
-) : ManagedStrategy(testClass, scenario, verifier, validationFunctions, stateRepresentation, testCfg) {
-    // The number of invocations that the strategy is eligible to use to search for an incorrect execution.
-    private val maxInvocations = testCfg.invocationsPerIteration
-    // The number of already used invocations.
-    private var usedInvocations = 0
+) : ManagedStrategy(testClass, scenario, validationFunctions, stateRepresentation, testCfg) {
     // The maximum number of thread switch choices that strategy should perform
     // (increases when all the interleavings with the current depth are studied).
     private var maxNumberOfSwitches = 0
@@ -54,21 +49,22 @@ internal class ModelCheckingStrategy(
     // The interleaving that will be studied on the next invocation.
     private lateinit var currentInterleaving: Interleaving
 
-    override fun runImpl(): LincheckFailure? {
-        currentInterleaving = root.nextInterleaving() ?: return null
-        while (usedInvocations < maxInvocations) {
-            // run invocation and check its results
-            val invocationResult = runInvocation()
-            if (suddenInvocationResult is SpinCycleFoundAndReplayRequired) {
+    override fun nextInvocation(): Boolean {
+        currentInterleaving = root.nextInterleaving()
+            ?: return false
+        return true
+    }
+
+    override fun initializeInvocation() {
+        super.initializeInvocation()
+        currentInterleaving.initialize()
+    }
+
+    override fun runInvocation(): InvocationResult {
+        return super.runInvocation().also {
+            if (it is SpinCycleFoundAndReplayRequired)
                 currentInterleaving.rollbackAfterSpinCycleFound()
-                continue
-            }
-            usedInvocations++
-            checkResult(invocationResult)?.let { return it }
-            // get new unexplored interleaving
-            currentInterleaving = root.nextInterleaving() ?: break
         }
-        return null
     }
 
     override fun onNewSwitch(iThread: Int, mustSwitch: Boolean) {
@@ -87,11 +83,6 @@ internal class ModelCheckingStrategy(
         check(iThread == currentThread)
         currentInterleaving.newExecutionPosition(iThread)
         return currentInterleaving.isSwitchPosition()
-    }
-
-    override fun initializeInvocation() {
-        currentInterleaving.initialize()
-        super.initializeInvocation()
     }
 
     override fun beforePart(part: ExecutionPart) {

src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/stress/StressCTestConfiguration.kt
@@ -36,9 +36,12 @@ class StressCTestConfiguration(
     timeoutMs = timeoutMs,
     customScenarios = customScenarios
 ) {
-    override fun createStrategy(testClass: Class<*>, scenario: ExecutionScenario, validationFunctions: List<Method>,
-                                stateRepresentationMethod: Method?, verifier: Verifier) =
-        StressStrategy(this, testClass, scenario, validationFunctions, stateRepresentationMethod, verifier)
+    override fun createStrategy(
+        testClass: Class<*>,
+        scenario: ExecutionScenario,
+        validationFunctions: List<Method>,
+        stateRepresentationMethod: Method?
+    ) = StressStrategy(this, testClass, scenario, validationFunctions, stateRepresentationMethod)
 
     companion object {
         const val DEFAULT_INVOCATIONS = 10000

JetBrains/lincheck

src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/modelchecking/ModelCheckingCTestConfiguration.kt
@@ -44,7 +44,10 @@ class ModelCheckingCTestConfiguration(testClass: Class<*>, iterations: Int, thre
     eliminateLocalObjects = eliminateLocalObjects,
     customScenarios = customScenarios
 ) {
-    override fun createStrategy(testClass: Class<*>, scenario: ExecutionScenario, validationFunction: Actor?,
-                                stateRepresentationMethod: Method?, verifier: Verifier): Strategy
-        = ModelCheckingStrategy(this, testClass, scenario, validationFunction, stateRepresentationMethod, verifier)
+    override fun createStrategy(
+        testClass: Class<*>,
+        scenario: ExecutionScenario,
+        validationFunction: Actor?,
+        stateRepresentationMethod: Method?,
+    ): Strategy = ModelCheckingStrategy(this, testClass, scenario, validationFunction, stateRepresentationMethod)
 }

src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/modelchecking/ModelCheckingStrategy.kt
@@ -34,17 +34,12 @@ import kotlin.random.*
  * than the number of all possible interleavings on the current depth level.
  */
 internal class ModelCheckingStrategy(
-    testCfg: ModelCheckingCTestConfiguration,
-    testClass: Class<*>,
-    scenario: ExecutionScenario,
-    validationFunction: Actor?,
-    stateRepresentation: Method?,
-    verifier: Verifier
-) : ManagedStrategy(testClass, scenario, verifier, validationFunction, stateRepresentation, testCfg) {
-    // The number of invocations that the strategy is eligible to use to search for an incorrect execution.
-    private val maxInvocations = testCfg.invocationsPerIteration
-    // The number of already used invocations.
-    private var usedInvocations = 0
+        testCfg: ModelCheckingCTestConfiguration,
+        testClass: Class<*>,
+        scenario: ExecutionScenario,
+        validationFunction: Actor?,
+        stateRepresentation: Method?,
+) : ManagedStrategy(testClass, scenario, validationFunction, stateRepresentation, testCfg) {
     // The maximum number of thread switch choices that strategy should perform
     // (increases when all the interleavings with the current depth are studied).
     private var maxNumberOfSwitches = 0
@@ -55,21 +50,22 @@ internal class ModelCheckingStrategy(
     // The interleaving that will be studied on the next invocation.
     private lateinit var currentInterleaving: Interleaving
 
-    override fun runImpl(): LincheckFailure? {
-        currentInterleaving = root.nextInterleaving() ?: return null
-        while (usedInvocations < maxInvocations) {
-            // run invocation and check its results
-            val invocationResult = runInvocation()
-            if (suddenInvocationResult is SpinCycleFoundAndReplayRequired) {
+    override fun nextInvocation(): Boolean {
+        currentInterleaving = root.nextInterleaving()
+            ?: return false
+        return true
+    }
+
+    override fun initializeInvocation() {
+        super.initializeInvocation()
+        currentInterleaving.initialize()
+    }
+
+    override fun runInvocation(): InvocationResult {
+        return super.runInvocation().also {
+            if (it is SpinCycleFoundAndReplayRequired)
                 currentInterleaving.rollbackAfterSpinCycleFound()
-                continue
-            }
-            usedInvocations++
-            checkResult(invocationResult)?.let { return it }
-            // get new unexplored interleaving
-            currentInterleaving = root.nextInterleaving() ?: break
         }
-        return null
     }
 
     override fun onNewSwitch(iThread: Int, mustSwitch: Boolean) {
@@ -88,11 +84,6 @@ internal class ModelCheckingStrategy(
         check(iThread == currentThread)
         currentInterleaving.newExecutionPosition(iThread)
         return currentInterleaving.isSwitchPosition()
-    }
-
-    override fun initializeInvocation() {
-        currentInterleaving.initialize()
-        super.initializeInvocation()
     }
 
     override fun beforePart(part: ExecutionPart) {

src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/stress/StressCTestConfiguration.kt
@@ -36,9 +36,12 @@ class StressCTestConfiguration(
     timeoutMs = timeoutMs,
     customScenarios = customScenarios
 ) {
-    override fun createStrategy(testClass: Class<*>, scenario: ExecutionScenario, validationFunction: Actor?,
-                                stateRepresentationMethod: Method?, verifier: Verifier) =
-        StressStrategy(this, testClass, scenario, validationFunction, stateRepresentationMethod, verifier)
+    override fun createStrategy(
+        testClass: Class<*>,
+        scenario: ExecutionScenario,
+        validationFunction: Actor?,
+        stateRepresentationMethod: Method?
+    ) = StressStrategy(this, testClass, scenario, validationFunction, stateRepresentationMethod)
 
     companion object {
         const val DEFAULT_INVOCATIONS = 10000