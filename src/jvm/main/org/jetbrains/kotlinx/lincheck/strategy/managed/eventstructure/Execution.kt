f228ef8f373343b968596566295795534c2c6556
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-01-18 14:49:20 +0100

    extend Execution class documentation
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/Execution.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/Execution.kt
@@ -25,27 +25,92 @@ import org.jetbrains.kotlinx.lincheck.utils.*
 
 
 /**
- * Execution represents a set of events belonging to single program's execution.
+ * Execution represents a set of events belonging to a single program's execution.
+ *
+ * The set of events in the execution is causally closed, meaning that if
+ * an event belongs to the execution, all its causal predecessors must also belong to it.
+ *
+ * We assume that all the events within the same thread are totally ordered by the program order.
+ * Thus, the execution is represented as a list of threads,
+ * with each thread being a list of thread's events given in program order.
+ *
+ * Since we also assume that the program order is consistent the enumeration order of events,
+ * (that is, `(a, b) \in po` implies `a.id < b.id`),
+ * the threads' events lists are sorted with respect to the enumeration order.
+ *
+ * @param E the type of events stored in the execution, must extend `ThreadEvent`.
  */
 interface Execution<out E : ThreadEvent> : Collection<E> {
+
+    /**
+     * A map from thread IDs to the list of thread events.
+     */
     val threadMap: ThreadMap<SortedList<E>>
 
+    /**
+     * Retrieves the list of events for the specified thread ID.
+     *
+     * @param tid The thread ID for which to retrieve the events.
+     * @return The list of events for the specified thread ID,
+     *  or null if the requested thread does not belong to the execution.
+     */
     operator fun get(tid: ThreadID): SortedList<E>? =
         threadMap[tid]
 
+    /**
+     * Checks if the given event is present at the execution.
+     *
+     * @param element The event to check for presence in the execution.
+     * @return true if the execution contains the event, false otherwise.
+     */
     override fun contains(element: @UnsafeVariance E): Boolean =
         get(element.threadId, element.threadPosition) == element
 
+    /**
+     * Checks if all events in the given collection are present at the execution.
+     *
+     * @param elements The collection of events to check for presence in the execution.
+     * @return true if all events in the collection are present in the execution, false otherwise.
+     */
     override fun containsAll(elements: Collection<@UnsafeVariance E>): Boolean =
         elements.all { contains(it) }
 
+
+    /**
+     * Returns an iterator over the events in the execution.
+     *
+     * @return an iterator that iterates over the events of the execution.
+     */
     override fun iterator(): Iterator<E> =
         threadIDs.map { get(it)!! }.asSequence().flatten().iterator()
 
 }
 
+/**
+ * Mutable execution represents a modifiable set of events belonging to a single program's execution.
+ *
+ * Mutable execution supports events' addition.
+ * Events can only be added according to the causal order.
+ * That is, whenever a new event is added to the execution,
+ * all the events on which it depends, including its program order predecessors,
+ * should already be added into the execution.
+ *
+ * @param E the type of events stored in the execution, must extend `ThreadEvent`.
+ *
+ * @see Execution
+ * @see ExecutionFrontier
+ */
 interface MutableExecution<E: ThreadEvent> : Execution<E> {
+
+    /**
+     * Adds the specified event to the mutable execution.
+     * All the causal predecessors of the event must already be added to the execution.
+     *
+     * @param event the event to be added to the execution.
+     */
     fun add(event: E)
+
+    // TODO: support single (causally-last) event removal (?)
 }
 
 val Execution<*>.threadIDs: Set<ThreadID>
