0c75b9deb08bc885267f03ee12474fa0762f6372
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-01-18 16:51:04 +0100

    working on event indexing, implement memory access index
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/EventIndex.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/EventIndex.kt
@@ -20,29 +20,29 @@
 
 package org.jetbrains.kotlinx.lincheck.strategy.managed.eventstructure
 
+import org.jetbrains.kotlinx.lincheck.strategy.managed.*
 import org.jetbrains.kotlinx.lincheck.utils.*
 
-class EventIndex<E : Event, C : Enum<C>, K : Any> private constructor(
-    nCategories: Int,
-    private val classifier: (E) -> Pair<C, K>
-) {
-    private val index = Array<MutableMap<K, MutableSet<E>>>(nCategories) { mutableMapOf() }
+
+typealias EventIndexClassifier<E, C, K> = (E) -> Pair<C, K>?
+
+interface EventIndex<E : Event, C : Enum<C>, K : Any> {
+    operator fun get(category: C, key: K): Set<E>
+}
+
+interface MutableEventIndex<E : Event, C : Enum<C>, K : Any> : EventIndex<E, C, K> {
+
+    val classifier: EventIndexClassifier<E, C, K>
+
+    fun index(category: C, key: K, event: E)
 
     fun index(event: E) {
-        val (category, key) = classifier(event)
-        index[category.ordinal].updateInplace(key, default = linkedSetOf()) { add(event) }
+        val (category, key) = classifier(event) ?: return
+        index(category, key, event)
     }
 
     fun index(events: Collection<E>) {
         events.forEach { index(it) }
-    }
-
-    operator fun get(category: C, key: K): Set<E> {
-        return index[category.ordinal][key] ?: emptySet()
-    }
-
-    fun reset() {
-        index.forEach { it.clear() }
     }
 
     fun rebuild(events: Collection<E>) {
@@ -50,10 +50,46 @@ class EventIndex<E : Event, C : Enum<C>, K : Any> private constructor(
         index(events)
     }
 
+    fun reset()
+
+}
+
+inline fun <E : Event, reified C : Enum<C>, K : Any> EventIndex(
+    noinline classifier: EventIndexClassifier<E, C, K>
+): EventIndex<E, C, K> {
+    return MutableEventIndex(classifier)
+}
+
+inline fun <E : Event, reified C : Enum<C>, K : Any> MutableEventIndex(
+    noinline classifier: EventIndexClassifier<E, C, K>
+): MutableEventIndex<E, C, K> {
+    return EventIndexImpl.create(classifier)
+}
+
+// TODO: make this class private
+class EventIndexImpl<E : Event, C : Enum<C>, K : Any> private constructor(
+    nCategories: Int,
+    override val classifier: EventIndexClassifier<E, C, K>
+) : MutableEventIndex<E, C, K> {
+
+    private val index = Array<MutableMap<K, MutableSet<E>>>(nCategories) { mutableMapOf() }
+
+    override operator fun get(category: C, key: K): Set<E> {
+        return index[category.ordinal][key] ?: emptySet()
+    }
+
+    override fun index(category: C, key: K, event: E) {
+        index[category.ordinal].updateInplace(key, default = linkedSetOf()) { add(event) }
+    }
+
+    override fun reset() {
+        index.forEach { it.clear() }
+    }
+
     companion object {
         inline fun <E : Event, reified C : Enum<C>, K : Any> create(
-            noinline classifier: (E) -> Pair<C, K>
-        ): EventIndex<E, C, K> {
+            noinline classifier: EventIndexClassifier<E, C, K>
+        ): MutableEventIndex<E, C, K> {
             // We cannot directly call the private constructor taking the number of categories,
             // because calling private methods from public inline methods is forbidden.
             // Neither we want to expose this constructor, because passing
@@ -67,12 +103,101 @@ class EventIndex<E : Event, C : Enum<C>, K : Any> private constructor(
                 create()
         }
 
-        fun <E : Event, C : Enum<C>, K : Any> create(witness: C, classifier: (E) -> Pair<C, K>): EventIndex<E, C, K> {
-            return EventIndex(witness.declaringClass.enumConstants.size, classifier)
+        fun <E : Event, C : Enum<C>, K : Any> create(
+            witness: C,
+            classifier: (E) -> Pair<C, K>?
+        ): MutableEventIndex<E, C, K> {
+            return EventIndexImpl(witness.declaringClass.enumConstants.size, classifier)
         }
 
-        fun <E : Event, C : Enum<C>, K : Any> create(): EventIndex<E, C, K> {
-            return EventIndex(0) { throw UnsupportedOperationException() }
+        fun <E : Event, C : Enum<C>, K : Any> create(): MutableEventIndex<E, C, K> {
+            return EventIndexImpl(0) { throw UnsupportedOperationException() }
         }
     }
+}
+
+enum class AtomicMemoryAccessCategory {
+    ReadRequest,
+    ReadResponse,
+    Write,
+}
+
+interface AtomicMemoryAccessEventIndex : EventIndex<AtomicThreadEvent, AtomicMemoryAccessCategory, MemoryLocation> {
+
+    val locations: Set<MemoryLocation>
+
+    fun getReadRequests(location: MemoryLocation) : Set<AtomicThreadEvent> {
+        return get(AtomicMemoryAccessCategory.ReadRequest, location)
+    }
+
+    fun getReadResponses(location: MemoryLocation): Set<AtomicThreadEvent> {
+        return get(AtomicMemoryAccessCategory.ReadResponse, location)
+    }
+
+    fun getWrites(location: MemoryLocation): Set<AtomicThreadEvent> {
+        return get(AtomicMemoryAccessCategory.Write, location)
+    }
+
+}
+
+typealias AtomicMemoryAccessEventClassifier =
+    EventIndexClassifier<AtomicThreadEvent, AtomicMemoryAccessCategory, MemoryLocation>
+
+interface MutableAtomicMemoryAccessEventIndex :
+    AtomicMemoryAccessEventIndex,
+    MutableEventIndex<AtomicThreadEvent, AtomicMemoryAccessCategory, MemoryLocation>
+
+fun AtomicMemoryAccessEventIndex(objectRegistry: ObjectRegistry): AtomicMemoryAccessEventIndex =
+    MutableAtomicMemoryAccessEventIndex(objectRegistry)
+
+fun MutableAtomicMemoryAccessEventIndex(objectRegistry: ObjectRegistry): AtomicMemoryAccessEventIndex =
+    MutableAtomicMemoryAccessEventIndexImpl(objectRegistry)
+
+private class MutableAtomicMemoryAccessEventIndexImpl(
+    val objectRegistry: ObjectRegistry
+) : MutableAtomicMemoryAccessEventIndex {
+
+    override val classifier: AtomicMemoryAccessEventClassifier = { event ->
+        val label = (event.label as? MemoryAccessLabel)
+        when {
+            label is ReadAccessLabel && label.isRequest ->
+                (AtomicMemoryAccessCategory.ReadRequest to label.location)
+
+            label is ReadAccessLabel && label.isResponse ->
+                (AtomicMemoryAccessCategory.ReadResponse to label.location)
+
+            label is WriteAccessLabel ->
+                (AtomicMemoryAccessCategory.Write to label.location)
+
+            else -> null
+        }
+    }
+
+    override val locations = mutableSetOf<MemoryLocation>()
+
+    private val index = MutableEventIndex<AtomicThreadEvent, AtomicMemoryAccessCategory, MemoryLocation>(classifier)
+
+    override fun get(category: AtomicMemoryAccessCategory, key: MemoryLocation): Set<AtomicThreadEvent> =
+        index[category, key]
+
+    override fun index(category: AtomicMemoryAccessCategory, key: MemoryLocation, event: AtomicThreadEvent) =
+        index.index(category, key, event)
+
+    override fun index(event: AtomicThreadEvent) {
+        val label = (event.label as? MemoryAccessLabel) ?: return
+        val isNewLocation = locations.add(label.location)
+        if (isNewLocation) {
+            // if the indexed event is the first memory access to a given location,
+            // then also add the object allocation event to the index for this memory location
+            val objEntry = objectRegistry[label.location.objID]!!
+            index.index(AtomicMemoryAccessCategory.Write, label.location, objEntry.allocation)
+        }
+        index.index(event)
+    }
+
+    override fun reset() {
+        locations.clear()
+        index.reset()
+    }
+
 }
---
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/ObjectRegistry.kt
@@ -0,0 +1,56 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2024 JetBrains s.r.o.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>
+ */
+
+package org.jetbrains.kotlinx.lincheck.strategy.managed.eventstructure
+
+import org.jetbrains.kotlinx.lincheck.strategy.managed.*
+import org.jetbrains.kotlinx.lincheck.utils.*
+import java.util.*
+
+data class ObjectEntry(
+    val id: ObjectID,
+    val obj: OpaqueValue,
+    val allocation: AtomicThreadEvent,
+) {
+    init {
+        require(id != NULL_OBJECT_ID)
+        require(allocation.label is InitializationLabel || allocation.label is ObjectAllocationLabel)
+    }
+
+    val isExternal: Boolean
+        get() = (allocation.label is InitializationLabel)
+}
+
+class ObjectRegistry {
+
+    private val objectIdIndex = HashMap<ObjectID, ObjectEntry>()
+    private val objectIndex = IdentityHashMap<Any, ObjectEntry>()
+
+    operator fun get(id: ObjectID): ObjectEntry? =
+        objectIdIndex[id]
+
+    operator fun get(obj: Any): ObjectEntry? =
+        objectIndex[obj]
+
+    fun register(entry: ObjectEntry) {
+        objectIdIndex.put(entry.id, entry).ensureNull()
+        objectIndex.put(entry.obj.unwrap(), entry).ensureNull()
+    }
+}
