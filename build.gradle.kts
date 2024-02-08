ab2daaf7939892bb0d00621fcea2e76035b1f796
Nikita Koval <ndkoval@ya.ru>
2024-02-08 17:28:23 +0100

    add more memory

--- build.gradle.kts
+++ build.gradle.kts
@@ -91,6 +91,7 @@ tasks {
     }
     withType<Test> {
         maxParallelForks = 1
+        maxHeapSize = "6g"
         jvmArgs(
             "--add-opens", "java.base/jdk.internal.misc=ALL-UNNAMED",
             "--add-exports", "java.base/jdk.internal.util=ALL-UNNAMED",
