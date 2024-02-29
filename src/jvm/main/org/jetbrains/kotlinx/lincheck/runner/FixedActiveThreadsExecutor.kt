8dd07c295d3df41fb42f64255ef6ea90957ac8bd
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-02-29 21:01:52 +0100

    make spinners in FixedActiveThreadsExecutor yielding
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/runner/FixedActiveThreadsExecutor.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/runner/FixedActiveThreadsExecutor.kt
@@ -34,7 +34,7 @@ internal class FixedActiveThreadsExecutor(private val nThreads: Int, runnerHash:
     /**
      * Spinners for each thread used for spin-wait on tasks.
      */
-    private val taskSpinners = SpinnerGroup(nThreads, shouldYield = false)
+    private val taskSpinners = SpinnerGroup(nThreads)
 
     /**
      * null, waiting in [submitAndAwait] thread, DONE, or exception
@@ -44,7 +44,7 @@ internal class FixedActiveThreadsExecutor(private val nThreads: Int, runnerHash:
     /**
      * Spinners for each thread used for spin-wait on results.
      */
-    private val resultSpinners = SpinnerGroup(nThreads, shouldYield = false)
+    private val resultSpinners = SpinnerGroup(nThreads)
 
     /**
      * This flag is set to `true` when [await] detects a hang.
