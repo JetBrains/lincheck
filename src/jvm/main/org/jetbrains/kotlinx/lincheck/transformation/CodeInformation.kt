97c67d7954f9367d2ed95ad483e5e99d344b26c8
Nikita Koval <ndkoval@ya.ru>
2024-03-28 14:25:48 +0100

    small refactorings

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/CodeInformation.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/CodeInformation.kt
@@ -10,11 +10,13 @@
 
 package org.jetbrains.kotlinx.lincheck.transformation
 
-import org.jetbrains.kotlinx.lincheck.LincheckClassLoader.REMAPPED_PACKAGE_INTERNAL_NAME
-import org.objectweb.asm.ClassReader
-import org.objectweb.asm.ClassVisitor
-import org.objectweb.asm.FieldVisitor
-import org.objectweb.asm.Opcodes
+import org.jetbrains.kotlinx.lincheck.LincheckClassLoader.*
+import org.jetbrains.kotlinx.lincheck.transformation.FinalFields.FinalFieldsVisitor
+import org.jetbrains.kotlinx.lincheck.transformation.FinalFields.addFinalField
+import org.jetbrains.kotlinx.lincheck.transformation.FinalFields.addMutableField
+import org.jetbrains.kotlinx.lincheck.transformation.FinalFields.collectFieldInformation
+import org.jetbrains.kotlinx.lincheck.transformation.FinalFields.isFinalField
+import org.objectweb.asm.*
 
 /**
  * [CodeLocations] object is used to maintain the mapping between unique IDs and code locations.
@@ -64,56 +66,93 @@ internal object CodeLocations {
  *
  * However, sometimes due to the order of class processing, we may not have information about this field yet,
  * as its class it is not loaded for now.
- * Then, we fall back to the slow-path algorithm [findField]: we read bytecode and analyze it using [FinalFieldsVisitor].
+ * Then, we fall back to the slow-path algorithm [collectFieldInformation]: we read bytecode and analyze it using [FinalFieldsVisitor].
  * If field is found, we record this information and return the result, otherwise we scan superclass and all implemented
  * interfaces in the same way.
  */
 internal object FinalFields {
 
     /**
-     * Stores a map FIELD NAME -> IS FINAL for each processed class.
+     * Stores a map INTERNAL_CLASS_NAME -> { FIELD_NAME -> IS FINAL } for each processed class.
      */
-    private val finalFields = HashMap<String, HashMap<String, Boolean>>()
+    private val classToFieldsMap = HashMap<String, HashMap<String, Boolean>>()
 
     /**
-     * Registers the field [fieldName] as a final field of the class [className]
+     * Registers the field [fieldName] as a final field of the class [internalClassName].
      */
-    fun addFinalField(className: String, fieldName: String) {
-        val internalName = className.normalizedClassName
-        val fieldsForClass = finalFields[internalName]
-        if (fieldsForClass == null) {
-            finalFields[className] = hashMapOf(fieldName to true)
-        } else {
-            fieldsForClass[fieldName] = true
-        }
+    fun addFinalField(internalClassName: String, fieldName: String) {
+        val fields = classToFieldsMap.computeIfAbsent(internalClassName) { HashMap() }
+        fields[fieldName] = false
     }
 
     /**
-     * Registers the field [fieldName] as a mutable field of the class [className]
+     * Registers the field [fieldName] as a mutable field of the class [internalClassName].
      */
-    fun addMutableField(className: String, fieldName: String) {
-        val internalName = className.normalizedClassName
-        val fieldsForClass = finalFields[internalName]
-        if (fieldsForClass == null) {
-            finalFields[className] = hashMapOf(fieldName to false)
-        } else {
-            fieldsForClass[fieldName] = false
-        }
+    fun addMutableField(internalClassName: String, fieldName: String) {
+        val fields = classToFieldsMap.computeIfAbsent(internalClassName) { HashMap() }
+        fields[fieldName] = true
     }
 
     /**
      * Determines if this field is final or not.
      */
-    fun isFinalField(className: String, fieldName: String): Boolean {
-        val internalName = className.normalizedClassName
-        val fieldsForClass = finalFields.computeIfAbsent(internalName) { hashMapOf() }
-        // Fast-path, in case we have information about this field
-        fieldsForClass[fieldName]?.let { return it }
-        // If we haven't processed this class yet, fall back to a slow-path, reading byte-code of a class
-        findField(internalName, fieldsForClass, fieldName)
-        // Here we must have information about this field, as we scanned all the hierarchy of this class
-        val isFieldFinal = fieldsForClass[fieldName] ?: error("Internal error: can't find field with $fieldName in class $className")
-        return isFieldFinal
+    fun isFinalField(internalClassName: String, fieldName: String): Boolean {
+        val fields = classToFieldsMap.computeIfAbsent(internalClassName) { HashMap() }
+        // Fast-path, in case we already have information about this field.
+        fields[fieldName]?.let { return it }
+        // If we haven't processed this class yet, fall back to a slow-path, reading the class byte-code.
+        collectFieldInformation(internalClassName, fieldName, fields)
+        // Here we must have information about this field, as we scanned all the hierarchy of this class.
+        return fields[fieldName] ?: error("Internal error: can't find field with $fieldName in class $internalClassName")
+    }
+
+    /**
+     * The slow-path of deciding if this field is final or not.
+     * Reads class from the classloader, scans it and extracts information about declared fields.
+     * If the field is not found, recursively searches in the superclass and implemented interfaces.
+     */
+    private fun collectFieldInformation(
+        internalClassName: String,
+        fieldName: String,
+        fields: MutableMap<String, Boolean>
+    ): Boolean {
+        // Read the class from classLoader.
+        val classReader = getClassReader(internalClassName)
+        val visitor = FinalFieldsVisitor()
+        // Scan class.
+        classReader.accept(visitor, 0)
+        // Store information about all final and mutable fields.
+        visitor.finalFields.forEach { field -> fields[field] = true }
+        visitor.mutableFields.forEach { field -> fields[field] = false }
+        // If the field is found - return it.
+        if (fieldName in visitor.finalFields || fieldName in visitor.mutableFields) return true
+        // If field is not present in this class - search in the superclass recursively.
+        visitor.superClassName?.let { internalSuperClassName ->
+            val internalSuperClassNameNormalized = internalSuperClassName.normalizedClassName
+            val superClassFields = classToFieldsMap.computeIfAbsent(internalSuperClassNameNormalized) { hashMapOf() }
+            val fieldFound = collectFieldInformation(internalSuperClassNameNormalized, fieldName, superClassFields)
+            // Copy all field information found in the superclass to the current class map.
+            addFieldsInfoFromSuperclass(internalSuperClassNameNormalized, internalClassName)
+            if (fieldFound) return true
+        }
+        // If field is not present in this class - search in the all implemented interfaces recursively.
+        visitor.implementedInterfaces.forEach { interfaceName ->
+            val normalizedInterfaceName = interfaceName.normalizedClassName
+            val interfaceFields = classToFieldsMap.computeIfAbsent(normalizedInterfaceName) { hashMapOf() }
+            val fieldFound = collectFieldInformation(normalizedInterfaceName, fieldName, interfaceFields)
+            // Copy all field information found in the interface to the current class map.
+            addFieldsInfoFromSuperclass(normalizedInterfaceName, internalClassName)
+            if (fieldFound) return true
+        }
+        // There is no such field in this class.
+        return false
+    }
+
+    private fun getClassReader(internalClassName: String): ClassReader {
+        val resource = "$internalClassName.class"
+        val inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(resource)
+            ?: error("Cannot create ClassReader for type $internalClassName")
+        return inputStream.use { ClassReader(inputStream) }
     }
 
     /**
@@ -121,8 +160,8 @@ internal object FinalFields {
      * parent fields to child map to avoid potential scanning of a child class in the search of a parent field.
      */
     private fun addFieldsInfoFromSuperclass(superType: String, type: String) {
-        val superFields = finalFields[superType] ?: return
-        val fields = finalFields.computeIfAbsent(type) { hashMapOf() }
+        val superFields = classToFieldsMap[superType] ?: return
+        val fields = classToFieldsMap.computeIfAbsent(type) { hashMapOf() }
         superFields.forEach { (fieldName, isFinal) ->
             // If we have a field shadowing (the same field name in the child class),
             // it's important not to override this information.
@@ -133,55 +172,6 @@ internal object FinalFields {
                 fields[fieldName] = isFinal
             }
         }
-    }
-
-
-    /**
-     * The slow-path of deciding if this field is final or not.
-     * Reads class from the classloader, scans it and extracts information about declared fields.
-     * If the field is not found, recursively searches in the superclass and implemented interfaces.
-     */
-    private fun findField(
-        type: String,
-        typeIsFinalFieldMap: MutableMap<String, Boolean>,
-        fieldName: String
-    ): Boolean {
-        // Read class from classLoader.
-        val classReader = typeInfo(type)
-        val visitor = FinalFieldsVisitor()
-        // Scan class.
-        classReader.accept(visitor, 0)
-        // Store information about all final and mutable fields.
-        visitor.finalFields.forEach { field -> typeIsFinalFieldMap[field] = true }
-        visitor.mutableFields.forEach { field -> typeIsFinalFieldMap[field] = false }
-        // If the field is found - return it.
-        if (fieldName in visitor.finalFields || fieldName in visitor.mutableFields) return true
-        // If field is not present in this class - search in the superclass recursively.
-        visitor.superClassName?.let { superName ->
-            val normalizedSuperName = superName.normalizedClassName
-            val superclassFields = finalFields.computeIfAbsent(normalizedSuperName) { hashMapOf() }
-            val fieldFound = findField(normalizedSuperName, superclassFields, fieldName)
-            // Copy all field information found in the superclass to the current class map.
-            addFieldsInfoFromSuperclass(normalizedSuperName, type)
-            if (fieldFound) return true
-        }
-        // If field is not present in this class - search in the all implemented interfaces recursively.
-        visitor.implementedInterfaces.forEach { interfaceName ->
-            val normalizedInterfaceName = interfaceName.normalizedClassName
-            val interfaceFields = finalFields.computeIfAbsent(normalizedInterfaceName) { hashMapOf() }
-            val fieldFound = findField(normalizedInterfaceName, interfaceFields, fieldName)
-            // Copy all field information found in the interface to the current class map.
-            addFieldsInfoFromSuperclass(normalizedInterfaceName, type)
-            if (fieldFound) return true
-        }
-        // There is no such field in this class.
-        return false
-    }
-
-    private fun typeInfo(type: String): ClassReader {
-        val resource = "$type.class"
-        val inputStream = ClassLoader.getSystemClassLoader().getResourceAsStream(resource) ?: error("Cannot create ClassReader for type $type")
-        return inputStream.use { ClassReader(inputStream) }
     }
 
     private val String.normalizedClassName: String
@@ -197,16 +187,17 @@ internal object FinalFields {
      * This visitor collects information about fields, declared in this class,
      * about superclass and implemented interfaces.
      */
-    private class FinalFieldsVisitor : ClassVisitor(Opcodes.ASM9) {
-
-        var superClassName: String? = null
-        lateinit var implementedInterfaces: Array<String>
+    private class FinalFieldsVisitor : ClassVisitor(ASM_API) {
+        val implementedInterfaces = arrayListOf<String>()
         val finalFields = arrayListOf<String>()
         val mutableFields = arrayListOf<String>()
 
+        var superClassName: String? = null
+
+
         override fun visit(version: Int, access: Int, name: String, signature: String?, superName: String?, interfaces: Array<String>?) {
             superClassName = superName
-            implementedInterfaces = interfaces ?: emptyArray()
+            interfaces?.let { implementedInterfaces += interfaces }
             super.visit(version, access, name, signature, superName, interfaces)
         }
 
