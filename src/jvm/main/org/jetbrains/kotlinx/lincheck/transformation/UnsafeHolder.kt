a08dd32a4298b8b630b84425a27d28e80c2ebaaf
Nikita Koval <ndkoval@ya.ru>
2024-04-08 22:14:48 +0200

    Move `UnsafeHolder` to the `transformation` package

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/UnsafeHolder.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/UnsafeHolder.kt
@@ -8,7 +8,7 @@
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-package org.jetbrains.kotlinx.lincheck
+package org.jetbrains.kotlinx.lincheck.transformation
 
 import sun.misc.*
 
