2ecd42f706dbe75dbd739456b8fd6d106353dbe9
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-04-24 20:37:52 +0200

    make separate transformer classes for AFU, VarHandle, and Unsafe APIs
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/AtomicPrimitiveMethodTransformers.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/AtomicPrimitiveMethodTransformers.kt
@@ -12,6 +12,7 @@ package org.jetbrains.kotlinx.lincheck.transformation
 
 import org.objectweb.asm.Type
 import org.objectweb.asm.Type.INT_TYPE
+import org.objectweb.asm.Type.LONG_TYPE
 import org.objectweb.asm.commons.GeneratorAdapter
 import org.objectweb.asm.commons.InstructionAdapter.OBJECT_TYPE
 import sun.nio.ch.lincheck.Injections
@@ -24,10 +25,9 @@ internal open class AtomicPrimitiveMethodTransformer(
 ) : ManagedStrategyMethodVisitor(fileName, className, methodName, adapter) {
 
     /**
-     * Process methods like *.set(value)
+     * Process methods like *.set(receiver, value)
      */
     protected fun processSetFieldMethod(name: String, opcode: Int, owner: String, desc: String, itf: Boolean) = adapter.run {
-        // STACK: value, receiver
         val argumentTypes = Type.getArgumentTypes(desc)
         val argumentType = argumentTypes.last()
         val valueLocal = newLocal(OBJECT_TYPE)
@@ -54,9 +54,10 @@ internal open class AtomicPrimitiveMethodTransformer(
     }
 
     /**
-     * Process methods like *.set(index, value)
+     * Process methods like *.set(receiver, index, value)
      */
     protected fun processSetArrayElementMethod(name: String, opcode: Int, owner: String, desc: String, itf: Boolean) = adapter.run {
+        val argumentTypes = Type.getArgumentTypes(desc)
         val argumentType = argumentTypes.last()
         val valueLocal = newLocal(OBJECT_TYPE)
         val indexLocal = newLocal(INT_TYPE)
@@ -88,7 +89,7 @@ internal open class AtomicPrimitiveMethodTransformer(
     }
 
     /**
-     * Process methods like *.compareAndSet(expected, desired)
+     * Process methods like *.compareAndSet(receiver, expected, desired)
      */
     protected fun processCompareAndSetFieldMethod(name: String, opcode: Int, owner: String, desc: String, itf: Boolean) = adapter.run {
         val argumentTypes = Type.getArgumentTypes(desc)
@@ -127,7 +128,7 @@ internal open class AtomicPrimitiveMethodTransformer(
     }
 
     /**
-     * Process methods like *.compareAndSet(expected, desired)
+     * Process methods like *.compareAndSet(receiver, index, expected, desired)
      */
     protected fun processCompareAndSetArrayElementMethod(name: String, opcode: Int, owner: String, desc: String, itf: Boolean) = adapter.run {
         val argumentTypes = Type.getArgumentTypes(desc)
@@ -171,6 +172,69 @@ internal open class AtomicPrimitiveMethodTransformer(
         invokeStatic(Injections::onWriteToObjectFieldOrArrayCell)
     }
 
+    /**
+     * Process methods like *.compareAndSet(receiver, offset, expected, desired)
+     */
+    protected fun processCompareAndSetByOffsetMethod(name: String, opcode: Int, owner: String, desc: String, itf: Boolean) = adapter.run {
+        val nextValueLocal = newLocal(OBJECT_TYPE)
+        val currenValueLocal = newLocal(OBJECT_TYPE)
+        val offsetLocal = newLocal(LONG_TYPE)
+        val receiverLocal = newLocal(OBJECT_TYPE)
+        // STACK: nextValue, currentValue, offset, receiver
+        storeLocal(nextValueLocal)
+        // STACK: currentValue, offset, receiver
+        storeLocal(currenValueLocal)
+        // STACK: offset, receiver
+        storeLocal(offsetLocal)
+        // STACK: receiver
+        storeLocal(receiverLocal)
+        // STACK: <empty>
+        loadLocal(receiverLocal)
+        // STACK: receiver
+        loadLocal(offsetLocal)
+        // STACK: offset, receiver
+        loadLocal(currenValueLocal)
+        // STACK: currentValue, offset, receiver
+        loadLocal(nextValueLocal)
+        // STACK: nextValue, currentValue, offset, receiver
+        visitMethodInsn(opcode, owner, name, desc, itf)
+        // STACK: cas-result
+        loadLocal(receiverLocal)
+        // STACK: receiver, cas-result
+        loadLocal(nextValueLocal)
+        // STACK: nextValue, receiver, cas-result
+        invokeStatic(Injections::onWriteToObjectFieldOrArrayCell)
+    }
+
+    /**
+     * Process methods like *.getAndSet(receiver, offset, expected, desired)
+     */
+    protected fun processGetAndSetByOffsetMethod(name: String, opcode: Int, owner: String, desc: String, itf: Boolean) = adapter.run {
+        val nextValueLocal = newLocal(OBJECT_TYPE)
+        val offsetLocal = newLocal(LONG_TYPE)
+        val receiverLocal = newLocal(OBJECT_TYPE)
+        // STACK: nextValue, offset, receiver
+        storeLocal(nextValueLocal)
+        // STACK: offset, receiver
+        storeLocal(offsetLocal)
+        // STACK: receiver
+        storeLocal(receiverLocal)
+        // STACK: <empty>
+        loadLocal(receiverLocal)
+        // STACK: receiver
+        loadLocal(offsetLocal)
+        // STACK: offset, receiver
+        loadLocal(nextValueLocal)
+        // STACK: nextValue, offset, receiver
+        visitMethodInsn(opcode, owner, name, desc, itf)
+        // STACK: <empty>
+        loadLocal(receiverLocal)
+        // STACK: receiver
+        loadLocal(nextValueLocal)
+        // STACK: nextValue, receiver
+        invokeStatic(Injections::onWriteToObjectFieldOrArrayCell)
+    }
+
 }
 
 internal class AtomicFieldUpdaterMethodTransformer(
@@ -209,4 +273,78 @@ internal class AtomicFieldUpdaterMethodTransformer(
 
 }
 
+internal class VarHandleMethodTransformer(
+    fileName: String,
+    className: String,
+    methodName: String,
+    adapter: GeneratorAdapter,
+) : AtomicPrimitiveMethodTransformer(fileName, className, methodName, adapter) {
 
+    override fun visitMethodInsn(opcode: Int, owner: String, name: String, desc: String, itf: Boolean) = adapter.run {
+        if (owner != "java/lang/invoke/VarHandle") {
+            visitMethodInsn(opcode, owner, name, desc, itf)
+            return
+        }
+        invokeIfInTestingCode(
+            original = {
+                visitMethodInsn(opcode, owner, name, desc, itf)
+            },
+            code = {
+                val argumentCount = Type.getArgumentCount(desc)
+                when (name) {
+                    // TODO: getAndSet should be handled separately
+                    "set", "setVolatile", "setRelease", "setOpaque", "getAndSet" -> when (argumentCount) {
+                        2 -> processSetFieldMethod(name, opcode, owner, desc, itf)
+                        3 -> processSetArrayElementMethod(name, opcode, owner, desc, itf)
+                        else -> throw IllegalStateException()
+                    }
+                    "compareAndSet",  "weakCompareAndSet",
+                    "weakCompareAndSetRelease", "weakCompareAndSetAcquire", "weakCompareAndSetPlain",
+                    "compareAndExchange", "compareAndExchangeAcquire", "compareAndExchangeRelease" -> when (argumentCount) {
+                        3 -> processCompareAndSetFieldMethod(name, opcode, owner, desc, itf)
+                        4 -> processCompareAndSetArrayElementMethod(name, opcode, owner, desc, itf)
+                        else -> throw IllegalStateException()
+                    }
+                    else -> {
+                        visitMethodInsn(opcode, owner, name, desc, itf)
+                    }
+                }
+            }
+        )
+    }
+
+}
+
+internal class UnsafeMethodTransformer(
+    fileName: String,
+    className: String,
+    methodName: String,
+    adapter: GeneratorAdapter,
+) : AtomicPrimitiveMethodTransformer(fileName, className, methodName, adapter) {
+
+    override fun visitMethodInsn(opcode: Int, owner: String, name: String, desc: String, itf: Boolean) = adapter.run {
+        if (owner != "sun/misc/Unsafe" && owner != "jdk/internal/misc/Unsafe") {
+            visitMethodInsn(opcode, owner, name, desc, itf)
+            return
+        }
+        invokeIfInTestingCode(
+            original = {
+                visitMethodInsn(opcode, owner, name, desc, itf)
+            },
+            code = {
+                when (name) {
+                    "compareAndSwapObject" -> {
+                        processCompareAndSetByOffsetMethod(name, opcode, owner, desc, itf)
+                    }
+                    "getAndSetObject" -> {
+                        processGetAndSetByOffsetMethod(name, opcode, owner, desc, itf)
+                    }
+                    else -> {
+                        visitMethodInsn(opcode, owner, name, desc, itf)
+                    }
+                }
+            }
+        )
+    }
+
+}
