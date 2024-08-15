adc49aca128cca9d4a7876da559694f221fa2640
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-08-15 23:51:40 +0200

    move `SuspendedNotAwokeTest` into `representation` package
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/SuspendedNotAwokeTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/SuspendedNotAwokeTest.kt
@@ -8,7 +8,7 @@
  * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-package org.jetbrains.kotlinx.lincheck_test.verifier.linearizability
+package org.jetbrains.kotlinx.lincheck_test.representation
 
 import kotlinx.coroutines.channels.Channel
 import org.jetbrains.kotlinx.lincheck.annotations.Operation
