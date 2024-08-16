d6bee2cd8744c9c2e09c928fa2b29d87839511e9
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-08-16 15:59:28 +0200

    add another tests, checking stack frame skipping for empty resumption parts
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/SuspendTraceReportingTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/SuspendTraceReportingTest.kt
@@ -113,3 +113,64 @@ class SuspendTraceResumptionReportingTest {
         .checkImpl(this::class.java)
         .checkLincheckOutput("suspend_trace_resumption_reporting_test.txt")
 }
+
+/* This test checks the trace reporting in case when a nested sequence of `suspend` functions is resumed ---
+ * upon the resumption the stack trace should be correctly restored.
+ *
+ * The difference w.r.t `SuspendTraceResumptionReportingTest` is that this test
+ * checks the case when some of the suspended functions do not have the resumption part,
+ * and thus their stack frames should be omitted in the resumption trace.
+ */
+class SuspendTraceResumptionFrameSkippingReportingTest {
+    private var canEnterForbiddenBlock: Boolean = false
+    private var barStarted: Boolean = false
+    private var counter: Int = 0
+    private var continuation: Continuation<Unit>? = null
+
+    @Operation(cancellableOnSuspension = false)
+    suspend fun foo() {
+        if (barStarted) canEnterForbiddenBlock = true
+        function1()
+        canEnterForbiddenBlock = false
+    }
+
+    @Operation
+    fun bar(): Int {
+        barStarted = true
+        if (continuation != null) {
+            continuation!!.resume(Unit)
+            if (canEnterForbiddenBlock) return -1
+        }
+        return 0
+    }
+
+    private suspend fun function1() {
+        counter++
+        function2()
+        counter++
+    }
+
+    private suspend fun function2() {
+        counter++
+        function3()
+    }
+
+    private suspend fun function3() {
+        counter++
+        suspendCoroutine<Unit> { continuation ->
+            this.continuation = continuation
+        }
+    }
+
+    @Test
+    fun test() = ModelCheckingOptions().apply {
+        addCustomScenario {
+            parallel {
+                thread { actor(::foo) }
+                thread { actor(::bar) }
+            }
+        }
+    }
+        .checkImpl(this::class.java)
+        .checkLincheckOutput("suspend_trace_resumption_frame_skipping_reporting_test.txt")
+}
