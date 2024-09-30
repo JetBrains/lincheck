92f6aedba9a4c8e80e22543d0d0d9215b5ce24a2
Nikita Koval <nikita.koval@jetbrains.com>
2024-09-30 23:12:17 +0200

    Do not depend on the `bootstrap` module in tests (#352)
    ---------
    
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
    Co-authored-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/isolated/FixedActiveThreadsExecutorIsolatedTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/isolated/FixedActiveThreadsExecutorIsolatedTest.kt
@@ -10,64 +10,72 @@
 package org.jetbrains.kotlinx.lincheck_test.isolated
 
 import org.jetbrains.kotlinx.lincheck.runner.*
+import org.jetbrains.kotlinx.lincheck.transformation.*
+import org.jetbrains.kotlinx.lincheck.transformation.withLincheckJavaAgent
 import org.junit.*
 import sun.nio.ch.lincheck.TestThread
 import java.util.concurrent.*
 
 class FixedActiveThreadsExecutorIsolatedTest {
     @Test
-    fun testSubmit() = FixedActiveThreadsExecutor("FixedActiveThreadsExecutorTest.testSubmit", 2).use { executor ->
-        val executed = arrayOf(false, false)
-        val tasks = Array<TestThreadExecution>(2) { iThread ->
-            object : TestThreadExecution(iThread) {
-                override fun run() {
-                    executed[iThread] = true
+    fun testSubmit() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
+        FixedActiveThreadsExecutor("FixedActiveThreadsExecutorTest.testSubmit", 2).use { executor ->
+            val executed = arrayOf(false, false)
+            val tasks = Array<TestThreadExecution>(2) { iThread ->
+                object : TestThreadExecution(iThread) {
+                    override fun run() {
+                        executed[iThread] = true
+                    }
                 }
             }
+            executor.submitAndAwait(tasks, Long.MAX_VALUE / 2)
+            check(executed.all { it })
         }
-        executor.submitAndAwait(tasks, Long.MAX_VALUE / 2)
-        check(executed.all { it })
     }
 
     @Test
-    fun testResubmit() = FixedActiveThreadsExecutor("FixedActiveThreadsExecutorTest.testResubmit", 2).use { executor ->
-        val executed = arrayOf(false, false)
-        val tasks = Array<TestThreadExecution>(2) { iThread ->
-            object : TestThreadExecution(iThread) {
-                override fun run() {
-                    executed[iThread] = true
+    fun testResubmit() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
+        FixedActiveThreadsExecutor("FixedActiveThreadsExecutorTest.testResubmit", 2).use { executor ->
+            val executed = arrayOf(false, false)
+            val tasks = Array<TestThreadExecution>(2) { iThread ->
+                object : TestThreadExecution(iThread) {
+                    override fun run() {
+                        executed[iThread] = true
+                    }
                 }
             }
+            executor.submitAndAwait(tasks, Long.MAX_VALUE / 2)
+            executed.fill(false)
+            executor.submitAndAwait(tasks, Long.MAX_VALUE / 2)
+            check(executed.all { it })
         }
-        executor.submitAndAwait(tasks, Long.MAX_VALUE / 2)
-        executed.fill(false)
-        executor.submitAndAwait(tasks, Long.MAX_VALUE / 2)
-        check(executed.all { it })
     }
 
     @Test(timeout = 100_000)
-    fun testSubmitTimeout() = FixedActiveThreadsExecutor(
-        "FixedActiveThreadsExecutorTest.testSubmitTimeout",
-        2
-    ).use { executor ->
-        val tasks = Array<TestThreadExecution>(2) { iThread ->
-            object : TestThreadExecution(iThread) {
-                init {
-                    this.iThread = iThread
-                }
+    fun testSubmitTimeout() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
+        FixedActiveThreadsExecutor(
+            "FixedActiveThreadsExecutorTest.testSubmitTimeout",
+            2
+        ).use { executor ->
+            val tasks = Array<TestThreadExecution>(2) { iThread ->
+                object : TestThreadExecution(iThread) {
+                    init {
+                        this.iThread = iThread
+                    }
 
-                override fun run() {
-                    if (iThread == 1)
-                        while (true);
+                    override fun run() {
+                        if (iThread == 1)
+                            while (true);
+                    }
                 }
             }
+            try {
+                executor.submitAndAwait(tasks, 200)
+            } catch (e: TimeoutException) {
+                return // TimeoutException is expected
+            }
+            check(false) { "TimeoutException was expected" }
         }
-        try {
-            executor.submitAndAwait(tasks, 200)
-        } catch (e: TimeoutException) {
-            return // TimeoutException is expected
-        }
-        check(false) { "TimeoutException was expected" }
     }
 
     @Test(timeout = 100_000)
---
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/runner/TestThreadExecutionHelperTest.kt
@@ -0,0 +1,133 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2023 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+package org.jetbrains.kotlinx.lincheck_test.runner
+
+import org.jetbrains.kotlinx.lincheck.*
+import org.jetbrains.kotlinx.lincheck.runner.*
+import org.jetbrains.kotlinx.lincheck.ExceptionResult.Companion.create
+import org.jetbrains.kotlinx.lincheck.execution.ExecutionScenario
+import org.jetbrains.kotlinx.lincheck.strategy.Strategy
+import org.jetbrains.kotlinx.lincheck.transformation.InstrumentationMode
+import org.jetbrains.kotlinx.lincheck.transformation.withLincheckJavaAgent
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import java.util.*
+
+class TestThreadExecutionHelperTest {
+    private var runner: Runner? = null
+
+    @Before
+    fun setUp() {
+        val scenario = ExecutionScenario(emptyList(), emptyList(), emptyList(), null)
+        val strategy: Strategy = object : Strategy(scenario) {
+            override val runner: Runner
+                get() {
+                    throw UnsupportedOperationException()
+                }
+
+            override fun runInvocation(): InvocationResult {
+                throw UnsupportedOperationException()
+            }
+        }
+        runner = object : Runner(
+            strategy,
+            ArrayDeque::class.java,
+            null,
+            null,
+        ) {
+            override fun isCoroutineResumed(iThread: Int, actorId: Int): Boolean {
+                return false
+            }
+
+            override fun afterCoroutineCancelled(iThread: Int) {}
+
+            override fun afterCoroutineResumed(iThread: Int) {}
+
+            override fun afterCoroutineSuspended(iThread: Int) {}
+
+            override fun onFailure(iThread: Int, e: Throwable) {}
+
+            override fun onFinish(iThread: Int) {}
+
+            override fun onStart(iThread: Int) {}
+
+            override fun run(): InvocationResult {
+                throw UnsupportedOperationException()
+            }
+
+            override fun isCurrentRunnerThread(thread: Thread): Boolean {
+                return false
+            }
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBase() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
+        val ex = TestThreadExecutionGenerator.create(
+            runner, 0,
+            Arrays.asList(
+                Actor(Queue::class.java.getMethod("add", Any::class.java), mutableListOf<Int?>(1)),
+                Actor(Queue::class.java.getMethod("add", Any::class.java), mutableListOf<Int?>(2)),
+                Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
+                Actor(Queue::class.java.getMethod("element"), emptyList<Any>()),
+                Actor(Queue::class.java.getMethod("peek"), emptyList<Any>())
+            ),
+            emptyList(),
+            false
+        )
+        ex.testInstance = ArrayDeque<Any>()
+        ex.results = arrayOfNulls(5)
+        ex.run()
+        Assert.assertArrayEquals(
+            arrayOf<Result>(
+                ValueResult(true),
+                ValueResult(true),
+                ValueResult(1),
+                ValueResult(2),
+                ValueResult(2)
+            ),
+            ex.results
+        )
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun testActorExceptionHandling() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
+        val ex = TestThreadExecutionGenerator.create(
+            runner, 0,
+            Arrays.asList(
+                Actor(ArrayDeque::class.java.getMethod("addLast", Any::class.java), mutableListOf<Int?>(1)),
+                Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
+                Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
+                Actor(Queue::class.java.getMethod("remove"), emptyList<Any>())
+            ),
+            emptyList(),
+            false
+        )
+        ex.testInstance = ArrayDeque<Any>()
+        ex.results = arrayOfNulls(4)
+        ex.clocks = Array(4) { IntArray(1) }
+        ex.allThreadExecutions = arrayOfNulls(1)
+        ex.allThreadExecutions[0] = ex
+        ex.run()
+        Assert.assertArrayEquals(
+            arrayOf(
+                VoidResult,
+                ValueResult(1),
+                create(NoSuchElementException()),
+                create(NoSuchElementException())
+            ),
+            ex.results
+        )
+    }
+}
