89a0f2314e8907cd0cbb3da075f2d9f2c45eb144
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-10-11 02:40:49 +0200

    optimize `isThreadSubclass` check by using `safeClassWriter`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/LincheckClassVisitor.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/LincheckClassVisitor.kt
@@ -60,7 +60,8 @@ internal class LincheckClassVisitor(
     ) {
         className = name
         classVersion = version
-        isThreadSubclass = isInstanceOf(JAVA_THREAD_CLASSNAME, className) // check if class is instance of `java/lang/Thread`
+        // check if class is instance of `java/lang/Thread`
+        isThreadSubclass = isInstanceOf(className, JAVA_THREAD_CLASSNAME)
         super.visit(version, access, name, signature, superName, interfaces)
     }
 
@@ -176,25 +177,8 @@ internal class LincheckClassVisitor(
     }
 
     @Suppress("SameParameterValue")
-    private fun isInstanceOf(expectedClassName: String, className: String): Boolean {
-        var currentSuperClassName: String? = className
-        while (currentSuperClassName != null) {
-            if (currentSuperClassName == expectedClassName) {
-                return true
-            }
-            val superClassReader = getClassReader(loader, currentSuperClassName)
-            val superClassNode = ClassNode()
-            superClassReader.accept(superClassNode, 0)
-            currentSuperClassName = superClassNode.superName
-        }
-        return false
-    }
-
-    private fun getClassReader(loader: ClassLoader, internalClassName: String): ClassReader {
-        val resource = "$internalClassName.class"
-        val inputStream = loader.getResourceAsStream(resource)
-            ?: error("Cannot create ClassReader for type '$internalClassName' (resource: $resource)")
-        return inputStream.use { ClassReader(inputStream) }
+    private fun isInstanceOf(subClassName: String, superClassName: String): Boolean {
+        return (classVisitor.getCommonSuperClass(subClassName, superClassName) == superClassName)
     }
 
 }
