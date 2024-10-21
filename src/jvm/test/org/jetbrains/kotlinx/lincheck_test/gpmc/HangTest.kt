fc519695292174e92752f7b143edff91c618c2b1
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-10-21 20:03:40 +0200

    add more tests
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

---
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/gpmc/HangTest.kt
@@ -0,0 +1,36 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2024 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.jetbrains.kotlinx.lincheck_test.gpmc
+
+import org.junit.Test
+import kotlin.concurrent.thread
+
+class HangTest {
+
+    fun hang() {
+        val t1 = thread {
+            while (true) { }
+        }
+        val t2 = thread {
+            while (true) { }
+        }
+        t1.join()
+        t2.join()
+    }
+
+    @Test(timeout = TIMEOUT)
+    fun testHang() = modelCheckerTest(
+        testClass = this::class,
+        testOperation = this::hang,
+        outcomes = setOf<Unit>(),
+    )
+
+}
