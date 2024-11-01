fdf1371c7cc98ee4df4955ee1c886fb6f88de405
Dmitrii Art <artuhovdmitrii@gmail.com>
2024-11-01 18:50:36 +0100

    Add energetic traverse for non-transformed classes (such as 'AtomicInteger', etc)

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/LincheckJavaAgent.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/LincheckJavaAgent.kt
@@ -13,6 +13,7 @@ package org.jetbrains.kotlinx.lincheck.transformation
 import net.bytebuddy.agent.ByteBuddyAgent
 import org.jetbrains.kotlinx.lincheck.canonicalClassName
 import org.jetbrains.kotlinx.lincheck.runInIgnoredSection
+import org.jetbrains.kotlinx.lincheck.shouldTransformClass
 import org.jetbrains.kotlinx.lincheck.transformation.InstrumentationMode.MODEL_CHECKING
 import org.jetbrains.kotlinx.lincheck.transformation.InstrumentationMode.STRESS
 import org.jetbrains.kotlinx.lincheck.transformation.LincheckClassFileTransformer.isEagerlyInstrumentedClass
@@ -218,7 +219,6 @@ internal object LincheckJavaAgent {
             Class.forName(canonicalClassName)
             return
         }
-        if (!shouldTransform(canonicalClassName, instrumentationMode)) return
         if (canonicalClassName in instrumentedClasses) return // already instrumented
         ensureClassHierarchyIsTransformed(Class.forName(canonicalClassName), Collections.newSetFromMap(IdentityHashMap()))
     }
@@ -381,7 +381,7 @@ internal object LincheckClassFileTransformer : ClassFileTransformer {
     ): ByteArray = transformedClassesCache.computeIfAbsent(internalClassName.canonicalClassName) {
         val reader = ClassReader(classBytes)
         val writer = SafeClassWriter(reader, loader, ClassWriter.COMPUTE_FRAMES)
-        val visitor = LincheckClassVisitor(writer, instrumentationMode)
+        val visitor = LincheckClassVisitor(instrumentationMode, writer)
         try {
             reader.accept(visitor, ClassReader.EXPAND_FRAMES)
             writer.toByteArray()
@@ -403,56 +403,7 @@ internal object LincheckClassFileTransformer : ClassFileTransformer {
         if (isEagerlyInstrumentedClass(className)) {
             return true
         }
-        // We do not need to instrument most standard Java classes.
-        // It is fine to inject the Lincheck analysis only into the
-        // `java.util.*` ones, ignored the known atomic constructs.
-        if (className.startsWith("java.")) {
-            if (className == "java.lang.Thread") return true
-            if (className.startsWith("java.util.concurrent.") && className.contains("Atomic")) return false
-            if (className.startsWith("java.util.")) return true
-            if (className.startsWith("com.sun.")) return false
-            return false
-        }
-        if (className.startsWith("sun.")) return false
-        if (className.startsWith("javax.")) return false
-        if (className.startsWith("jdk.")) return false
-        // We do not need to instrument most standard Kotlin classes.
-        // However, we need to inject the Lincheck analysis into the classes
-        // related to collections, iterators, random and coroutines.
-        if (className.startsWith("kotlin.")) {
-            if (className.startsWith("kotlin.concurrent.ThreadsKt")) return true
-            if (className.startsWith("kotlin.collections.")) return true
-            if (className.startsWith("kotlin.jvm.internal.Array") && className.contains("Iterator")) return true
-            if (className.startsWith("kotlin.ranges.")) return true
-            if (className.startsWith("kotlin.random.")) return true
-            if (className.startsWith("kotlin.coroutines.jvm.internal.")) return false
-            if (className.startsWith("kotlin.coroutines.")) return true
-            return false
-        }
-        if (className.startsWith("kotlinx.atomicfu.")) return false
-        // We need to skip the classes related to the debugger support in Kotlin coroutines.
-        if (className.startsWith("kotlinx.coroutines.debug.")) return false
-        if (className == "kotlinx.coroutines.DebugKt") return false
-        // We should never transform the coverage-related classes.
-        if (className.startsWith("com.intellij.rt.coverage.")) return false
-        // We can also safely do not instrument some libraries for performance reasons.
-        if (className.startsWith("com.esotericsoftware.kryo.")) return false
-        if (className.startsWith("net.bytebuddy.")) return false
-        if (className.startsWith("net.rubygrapefruit.platform.")) return false
-        if (className.startsWith("io.mockk.")) return false
-        if (className.startsWith("it.unimi.dsi.fastutil.")) return false
-        if (className.startsWith("worker.org.gradle.")) return false
-        if (className.startsWith("org.objectweb.asm.")) return false
-        if (className.startsWith("org.gradle.")) return false
-        if (className.startsWith("org.slf4j.")) return false
-        if (className.startsWith("org.apache.commons.lang.")) return false
-        if (className.startsWith("org.junit.")) return false
-        if (className.startsWith("junit.framework.")) return false
-        // Finally, we should never instrument the Lincheck classes.
-        if (className.startsWith("org.jetbrains.kotlinx.lincheck.")) return false
-        if (className.startsWith("sun.nio.ch.lincheck.")) return false
-        // All the classes that were not filtered out are eligible for transformation.
-        return true
+        return shouldTransformClass(className)
     }
 
     // We should always eagerly transform the following classes.
