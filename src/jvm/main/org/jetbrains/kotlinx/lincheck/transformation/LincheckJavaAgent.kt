eupp/kotlinx-lincheck

fix class-loading issue by enforcing transformation of `StackTraceElement` classes
Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/LincheckJavaAgent.kt
@@ -142,9 +142,10 @@ internal object LincheckJavaAgent {
                 check(instrumentedClasses.isEmpty())
                 // Transform class loaders on the start, because it's the only place where we can do it.
                 val classLoaderClasses = getLoadedClassesToInstrument()
-                    .filter { isClassLoader(it.name) }
+                    .filter { isClassLoader(it.name) || isStackTraceElement(it.name) }
                     .toTypedArray()
                 instrumentation.retransformClasses(*classLoaderClasses)
+                instrumentedClasses.addAll(classLoaderClasses.map { it.name })
             }
         }
     }
@@ -357,7 +358,8 @@ internal object LincheckClassFileTransformer : ClassFileTransformer {
         if (!INSTRUMENT_ALL_CLASSES &&
             instrumentationMode == MODEL_CHECKING &&
             internalClassName.canonicalClassName !in instrumentedClasses &&
-            !isClassLoader(internalClassName)) {
+            !isClassLoader(internalClassName) &&
+            !isStackTraceElement(internalClassName)) {
             return null
         }
         return transformImpl(loader, internalClassName, classBytes)
@@ -389,7 +391,7 @@ internal object LincheckClassFileTransformer : ClassFileTransformer {
             if (className.startsWith("java.") || className.startsWith("kotlin.")) return false
         }
         // We should transform all the ClassLoader-s to wrap `loadClass` methods in the ignored section.
-        if (isClassLoader(className)) {
+        if (isClassLoader(className) || isStackTraceElement(className)) {
             return true
         }
         // We do not need to instrument most standard Java classes.

JetBrains/lincheck

src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/LincheckJavaAgent.kt
@@ -142,7 +142,7 @@ internal object LincheckJavaAgent {
                 check(instrumentedClasses.isEmpty())
                 // Transform class loaders on the start, because it's the only place where we can do it.
                 val classLoaderClasses = getLoadedClassesToInstrument()
-                    .filter { containsClassloaderInName(it.name) }
+                    .filter { containsClassloaderInName(it.name) || isStackTraceElement(it.name) }
                     .toTypedArray()
                 instrumentation.retransformClasses(*classLoaderClasses)
                 instrumentedClasses.addAll(classLoaderClasses.map { it.name })
@@ -359,7 +359,8 @@ internal object LincheckClassFileTransformer : ClassFileTransformer {
             instrumentationMode == MODEL_CHECKING &&
             internalClassName.canonicalClassName !in instrumentedClasses &&
             // Always transform classloaders
-            !containsClassloaderInName(internalClassName)) {
+            !containsClassloaderInName(internalClassName) &&
+            !isStackTraceElement(internalClassName)) {
             return null
         }
         return transformImpl(loader, internalClassName, classBytes)
@@ -391,7 +392,7 @@ internal object LincheckClassFileTransformer : ClassFileTransformer {
             if (className.startsWith("java.") || className.startsWith("kotlin.")) return false
         }
         // We should transform all the ClassLoader-s to wrap `loadClass` methods in the ignored section.
-        if (containsClassloaderInName(className)) {
+        if (containsClassloaderInName(className) || isStackTraceElement(className)) {
             return true
         }
         // We do not need to instrument most standard Java classes.