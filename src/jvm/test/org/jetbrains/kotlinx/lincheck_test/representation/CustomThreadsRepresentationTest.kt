76f2f8f47ba311fbfaeb46d4f86feb621ca9fc4b
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2024-11-28 22:51:55 +0100

    add custom threads representation test
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

---
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/CustomThreadsRepresentationTest.kt
@@ -0,0 +1,121 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2024 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.jetbrains.kotlinx.lincheck_test.representation
+
+
+import org.jetbrains.kotlinx.lincheck.*
+import org.jetbrains.kotlinx.lincheck.execution.ExecutionResult
+import org.jetbrains.kotlinx.lincheck.execution.ExecutionScenario
+import org.jetbrains.kotlinx.lincheck.execution.parallelResults
+import org.jetbrains.kotlinx.lincheck.scenario
+import org.jetbrains.kotlinx.lincheck.strategy.*
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingOptions
+import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.ModelCheckingStrategy
+import org.jetbrains.kotlinx.lincheck.transformation.*
+import org.jetbrains.kotlinx.lincheck.util.UnsafeHolder
+import org.jetbrains.kotlinx.lincheck.verifier.Verifier
+import org.jetbrains.kotlinx.lincheck_test.util.checkLincheckOutput
+import org.junit.Assert
+import java.util.concurrent.atomic.AtomicIntegerFieldUpdater
+import org.junit.Test
+import kotlin.concurrent.thread
+import kotlin.reflect.KClass
+import kotlin.reflect.KFunction
+
+class CustomThreadsRepresentationTest {
+
+    @Volatile
+    private var value: Int = 0
+
+    fun operation() {
+        val block = {
+            value += 1
+            valueUpdater.getAndIncrement(this)
+            unsafe.getAndAddInt(this, valueFieldOffset, 1)
+            synchronized(this) {
+                value += 1
+            }
+        }
+        val threads = List(3) { thread { block() } }
+        threads.forEach { it.join() }
+        check(false) // to trigger failure and trace collection
+    }
+
+    @Test(timeout = TIMEOUT)
+    fun test() = modelCheckerTraceTest(
+        testClass = this::class,
+        testOperation = this::operation,
+        invocations = 1_000,
+        outputFileName = "custom_threads_trace.txt",
+    )
+
+    companion object {
+        val unsafe =
+            UnsafeHolder.UNSAFE
+
+        private val valueField =
+            CustomThreadsRepresentationTest::class.java.getDeclaredField("value")
+
+        private val valueUpdater =
+            AtomicIntegerFieldUpdater.newUpdater(CustomThreadsRepresentationTest::class.java, "value")
+
+        private val valueFieldOffset =
+            unsafe.objectFieldOffset(valueField)
+    }
+
+}
+
+internal fun modelCheckerTraceTest(
+    testClass: KClass<*>,
+    testOperation: KFunction<*>,
+    outputFileName: String,
+    invocations: Int = DEFAULT_INVOCATIONS_COUNT,
+) {
+    val scenario = scenario {
+        parallel { thread { actor(testOperation) } }
+    }
+    val verifier = ExceptionFailingVerifier()
+    withLincheckJavaAgent(InstrumentationMode.MODEL_CHECKING) {
+        val strategy = createStrategy(testClass.java, scenario)
+        val failure = strategy.runIteration(invocations, verifier)
+        assert(failure != null)
+        failure.checkLincheckOutput(outputFileName)
+    }
+}
+
+private fun createStrategy(testClass: Class<*>, scenario: ExecutionScenario): ModelCheckingStrategy {
+    return createConfiguration(testClass)
+        .createStrategy(
+            testClass = testClass,
+            scenario = scenario,
+            validationFunction = null,
+            stateRepresentationMethod = null,
+        ) as ModelCheckingStrategy
+}
+
+private fun createConfiguration(testClass: Class<*>) =
+    ModelCheckingOptions()
+        .invocationTimeout(5_000) // 5 sec
+        .createTestConfigurations(testClass)
+
+
+private class ExceptionFailingVerifier : Verifier {
+    override fun verifyResults(scenario: ExecutionScenario?, results: ExecutionResult?): Boolean {
+        return when (results!!.parallelResults[0][0]!!) {
+            is ExceptionResult -> false
+            else -> true
+        }
+    }
+}
+
+private const val DEFAULT_INVOCATIONS_COUNT = 100
+
+private const val TIMEOUT = 30_000L // 30 sec
