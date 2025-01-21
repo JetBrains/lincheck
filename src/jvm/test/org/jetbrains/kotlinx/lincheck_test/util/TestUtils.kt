07657e9682d71610d35a02e00494239f4e58da7b
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-01-21 23:08:25 +0100

    add separate expected trace files for each jdk version for the thread pool test
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/util/TestUtils.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/util/TestUtils.kt
@@ -114,10 +114,34 @@ class StringPoolGenerator(randomProvider: RandomProvider, configuration: String)
 }
 
 /**
- * Indicates whether the current Java Development Kit (JDK) version is JDK 8.
- *
- * This property checks the system's Java specification version
- * and determines if the major version corresponds to '8', signifying JDK 8.
+ * Represents a set of Java Development Kit (JDK) versions on which the tests are run.
  */
-// java.specification.version is "1.$x" for Java prior to 8 and "$x" for the newer ones
-internal val isJdk8 = System.getProperty("java.specification.version").removePrefix("1.") == "8"
+internal enum class TestJdkVersion {
+    JDK_8, JDK_11, JDK_13, JDK_15, JDK_17, JDK_19, JDK_20, JDK_21;
+}
+
+/**
+ * Determines the current JDK version based on the `java.specification.version` system property.
+ * If the system property indicates an unsupported JDK version, an error is thrown.
+ */
+internal val testJdkVersion: TestJdkVersion = run {
+    val jdkVersion = System.getProperty("java.specification.version")
+    // java.specification.version is "1.x" for Java prior to 8 and "x" for the newer ones
+    when {
+        jdkVersion.removePrefix("1.") == "8"    -> TestJdkVersion.JDK_8
+        jdkVersion == "11"                      -> TestJdkVersion.JDK_11
+        jdkVersion == "13"                      -> TestJdkVersion.JDK_13
+        jdkVersion == "15"                      -> TestJdkVersion.JDK_15
+        jdkVersion == "17"                      -> TestJdkVersion.JDK_17
+        jdkVersion == "19"                      -> TestJdkVersion.JDK_19
+        jdkVersion == "20"                      -> TestJdkVersion.JDK_20
+        jdkVersion == "21"                      -> TestJdkVersion.JDK_21
+        else ->
+            error("Unsupported JDK version: $jdkVersion")
+    }
+}
+
+/**
+ * Indicates whether the current Java Development Kit (JDK) version is JDK 8.
+ */
+internal val isJdk8 = (testJdkVersion == TestJdkVersion.JDK_8)
