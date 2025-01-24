57189c8e4913130bee70bfcf8250af462a660290
Evgeniy Zhelenskiy <55230817+zhelenskiy@users.noreply.github.com>
2025-01-24 12:11:17 +0100

    Fix JDK version used in tests (#446)

--- bootstrap/build.gradle.kts
+++ bootstrap/build.gradle.kts
@@ -15,6 +15,5 @@ tasks.jar {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    toolchain { languageVersion.set(JavaLanguageVersion.of(8)) }
 }
