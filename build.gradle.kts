a2aed609f570d60eb47b0819ffaee9a77dbc36b5
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-01-28 23:51:43 +0100

    add comment explaining the `overwriteRepresentationTestsOutput` flag
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -144,6 +144,17 @@ tasks {
         if (instrumentAllClasses.toBoolean()) {
             systemProperty("lincheck.instrumentAllClasses", "true")
         }
+        // The `overwriteRepresentationTestsOutput` flag is used to automatically repair representation tests.
+        // Representation tests work by comparing an actual output of the test (execution trace in most cases)
+        // with the expected output stored in a file (which is kept in resources).
+        // Normally, if the actual and expected outputs differ, the test fails,
+        // but when this flag is set, instead the expected output is overwritten with the actual output.
+        // This helps to quickly repair the tests when the output difference is non-essential
+        // or when the output logic actually has changed in the code.
+        // The test system relies on that the gradle test task is run from the root directory of the project,
+        // to search for the directory where the expected output files are stored.
+        //
+        // PLEASE USE CAREFULLY: always first verify that the changes in the output are expected!
         val overwriteRepresentationTestsOutput: String by project
         if (overwriteRepresentationTestsOutput.toBoolean()) {
             systemProperty("lincheck.overwriteRepresentationTestsOutput", "true")
