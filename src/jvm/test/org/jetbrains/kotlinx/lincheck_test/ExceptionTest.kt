bbadfd6137437cdd4cd1ad5fc6f7074b3b17fcb3
Nikita Koval <ndkoval@ya.ru>
2025-01-28 21:50:15 +0300

    Remove excessive logging

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/ExceptionTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/ExceptionTest.kt
@@ -153,7 +153,6 @@ class EmptyStackTraceExceptionTest : AbstractLincheckTest(IncorrectResultsFailur
         actorsPerThread(200)
         minimizeFailedScenario(true)
         sequentialSpecification(ExceptionTestSequentialImplementation::class.java)
-        logLevel(LoggingLevel.INFO)
     }
 
 }
