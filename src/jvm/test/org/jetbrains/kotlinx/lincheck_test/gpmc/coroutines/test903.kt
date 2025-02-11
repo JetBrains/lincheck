4e724ff848df36fdc27df7ba4a5852c6800e15fc
Bob.Brockbernd <bobbrockbernd@gmail.com>
2025-02-11 14:24:54 +0100

    Add coroutine tests from deadlock test repo

---
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/gpmc/coroutines/test903.kt
@@ -0,0 +1,57 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2025 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.jetbrains.kotlinx.lincheck_test.gpmc.coroutines.test903
+import org.jetbrains.kotlinx.lincheck_test.gpmc.coroutines.test903.RunChecker903.Companion.pool
+import org.jetbrains.kotlinx.lincheck_test.gpmc.coroutines.BaseRunCoroutineTests
+import java.util.concurrent.Executors
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+
+@OptIn(DelicateCoroutinesApi::class)
+fun producerA(channelA: Channel<Int>) = GlobalScope.launch(pool) {
+    repeat(5) {
+        channelA.send(it)
+    }
+    channelA.close()
+}
+
+@OptIn(DelicateCoroutinesApi::class)
+fun producerB(channelB: Channel<Int>, channelA: Channel<Int>) = GlobalScope.launch(pool) {
+    repeat(5) {
+        val value = channelA.receive() * 2
+        channelB.send(value)
+    }
+    channelB.close()
+}
+
+fun main(): Unit = runBlocking(pool) {
+    val channelA = Channel<Int>()
+    val channelB = Channel<Int>()
+
+    producerA(channelA)
+    producerB(channelB, channelA)
+
+    repeat(5) {
+        launch(pool) {
+            println("Received from channelB: ${channelB.receive()}")
+        }
+    }
+}
+
+class RunChecker903: BaseRunCoroutineTests(false) {
+        companion object {
+        lateinit var pool: ExecutorCoroutineDispatcher
+    }
+    override fun block() {
+        pool = Executors.newFixedThreadPool(4).asCoroutineDispatcher()
+        runBlocking(pool) { main() }
+    }
+}
---
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/gpmc/coroutines/test911.kt
@@ -0,0 +1,54 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2025 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.jetbrains.kotlinx.lincheck_test.gpmc.coroutines.test911
+import org.jetbrains.kotlinx.lincheck_test.gpmc.coroutines.test911.RunChecker911.Companion.pool
+import org.jetbrains.kotlinx.lincheck_test.gpmc.coroutines.BaseRunCoroutineTests
+import java.util.concurrent.Executors
+import kotlinx.coroutines.*
+import kotlinx.coroutines.channels.Channel
+
+class Producer {
+    suspend fun produce(channel: Channel<Int>) {
+        channel.send(1)  // Sends value 1 to the channel
+    }
+}
+
+class Consumer {
+    suspend fun consume(channel: Channel<Int>) {
+        channel.receive()  // Tries to receive a value from the channel
+    }
+}
+
+fun main(): Unit = runBlocking(pool) {
+    val channel = Channel<Int>()
+    val producer = Producer()
+    val consumer = Consumer()
+
+    // Coroutine 1: Producer sends a value to the channel
+    launch(pool) {
+        producer.produce(channel)
+    }
+
+    // Coroutine 2: Consumer tries to receive a value from the channel
+    launch(pool) {
+        consumer.consume(channel)
+    }
+}
+
+class RunChecker911: BaseRunCoroutineTests(false) {
+        companion object {
+        lateinit var pool: ExecutorCoroutineDispatcher
+    }
+    override fun block() {
+        pool = Executors.newFixedThreadPool(4).asCoroutineDispatcher()
+        runBlocking(pool) { main() }
+    }
+}
