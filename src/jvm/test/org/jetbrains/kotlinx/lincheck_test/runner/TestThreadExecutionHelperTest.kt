3dd37fcc0fffdb98eb1ee446dc0825c6f77bbe97
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-02-11 18:06:10 +0100

    restore `TestThreadExecutionHelperTest`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/runner/TestThreadExecutionHelperTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/runner/TestThreadExecutionHelperTest.kt
@@ -13,6 +13,7 @@ import org.jetbrains.kotlinx.lincheck.*
 import org.jetbrains.kotlinx.lincheck.runner.*
 import org.jetbrains.kotlinx.lincheck.ExceptionResult.Companion.create
 import org.jetbrains.kotlinx.lincheck.execution.ExecutionScenario
+import org.jetbrains.kotlinx.lincheck.execution.emptyScenario
 import org.jetbrains.kotlinx.lincheck.strategy.Strategy
 import org.jetbrains.kotlinx.lincheck.transformation.InstrumentationMode
 import org.jetbrains.kotlinx.lincheck.transformation.withLincheckJavaAgent
@@ -21,89 +22,91 @@ import org.junit.Before
 import org.junit.Test
 import java.util.*
 
-// TODO: repair these tests once new runner api is finalized
-// class TestThreadExecutionHelperTest {
-//     private var runner: Runner? = null
-//
-//     @Before
-//     fun setUp() {
-//         val scenario = ExecutionScenario(emptyList(), emptyList(), emptyList(), null)
-//         val strategy: Strategy = object : Strategy(scenario) {
-//             override val runner: Runner
-//                 get() {
-//                     throw UnsupportedOperationException()
-//                 }
-//
-//             override fun runInvocation(): InvocationResult {
-//                 throw UnsupportedOperationException()
-//             }
-//         }
-//         runner = object : Runner {
-//             override fun run(): InvocationResult {
-//                 throw UnsupportedOperationException()
-//             }
-//         }
-//     }
-//
-//     @Test
-//     @Throws(Exception::class)
-//     fun testBase() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
-//         val ex = TestThreadExecutionGenerator.create(
-//             runner, 0,
-//             Arrays.asList(
-//                 Actor(Queue::class.java.getMethod("add", Any::class.java), mutableListOf<Int?>(1)),
-//                 Actor(Queue::class.java.getMethod("add", Any::class.java), mutableListOf<Int?>(2)),
-//                 Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
-//                 Actor(Queue::class.java.getMethod("element"), emptyList<Any>()),
-//                 Actor(Queue::class.java.getMethod("peek"), emptyList<Any>())
-//             ),
-//             emptyList(),
-//             false
-//         )
-//         ex.testInstance = ArrayDeque<Any>()
-//         ex.results = arrayOfNulls(5)
-//         ex.run()
-//         Assert.assertArrayEquals(
-//             arrayOf<Result>(
-//                 ValueResult(true),
-//                 ValueResult(true),
-//                 ValueResult(1),
-//                 ValueResult(2),
-//                 ValueResult(2)
-//             ),
-//             ex.results
-//         )
-//     }
-//
-//
-//     @Test
-//     @Throws(Exception::class)
-//     fun testActorExceptionHandling() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
-//         val ex = TestThreadExecutionGenerator.create(
-//             runner, 0,
-//             Arrays.asList(
-//                 Actor(ArrayDeque::class.java.getMethod("addLast", Any::class.java), mutableListOf<Int?>(1)),
-//                 Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
-//                 Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
-//                 Actor(Queue::class.java.getMethod("remove"), emptyList<Any>())
-//             ),
-//             emptyList(),
-//             false
-//         )
-//         ex.testInstance = ArrayDeque<Any>()
-//         ex.results = arrayOfNulls(4)
-//         ex.clocks = Array(4) { IntArray(1) }
-//         ex.allThreadExecutions = arrayOfNulls(1)
-//         ex.allThreadExecutions[0] = ex
-//         ex.run()
-//         Assert.assertArrayEquals(
-//             arrayOf(
-//                 VoidResult,
-//                 ValueResult(1),
-//                 create(NoSuchElementException()),
-//                 create(NoSuchElementException())
-//             ),
-//             ex.results
-//         )
-//     }
-// }
+class TestThreadExecutionHelperTest {
+    private var runner: ExecutionScenarioRunner? = null
+
+    @Before
+    fun setUp() {
+        val strategy: Strategy = object : Strategy() {
+            override val runner: Runner get() {
+                throw UnsupportedOperationException()
+            }
+
+            override fun runInvocation(): InvocationResult {
+                throw UnsupportedOperationException()
+            }
+        }
+        runner = ExecutionScenarioRunner(
+            scenario = emptyScenario(),
+            testClass = ArrayDeque::class.java,
+            validationFunction = null,
+            stateRepresentationFunction = null,
+            timeoutMs = 0L,
+            useClocks = UseClocks.ALWAYS,
+        ).apply {
+            initializeStrategy(strategy)
+        }
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testBase() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
+        val ex = TestThreadExecutionGenerator.create(
+            runner, 0,
+            listOf(
+                Actor(Queue::class.java.getMethod("add", Any::class.java), mutableListOf<Int?>(1)),
+                Actor(Queue::class.java.getMethod("add", Any::class.java), mutableListOf<Int?>(2)),
+                Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
+                Actor(Queue::class.java.getMethod("element"), emptyList<Any>()),
+                Actor(Queue::class.java.getMethod("peek"), emptyList<Any>())
+            ),
+            emptyList(),
+            false
+        )
+        ex.testInstance = ArrayDeque<Any>()
+        ex.results = arrayOfNulls(5)
+        ex.run()
+        Assert.assertArrayEquals(
+            arrayOf<Result>(
+                ValueResult(true),
+                ValueResult(true),
+                ValueResult(1),
+                ValueResult(2),
+                ValueResult(2)
+            ),
+            ex.results
+        )
+    }
+
+
+    @Test
+    @Throws(Exception::class)
+    fun testActorExceptionHandling() = withLincheckJavaAgent(InstrumentationMode.STRESS) {
+        val ex = TestThreadExecutionGenerator.create(
+            runner, 0,
+            listOf(
+                Actor(ArrayDeque::class.java.getMethod("addLast", Any::class.java), mutableListOf<Int?>(1)),
+                Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
+                Actor(Queue::class.java.getMethod("remove"), emptyList<Any>()),
+                Actor(Queue::class.java.getMethod("remove"), emptyList<Any>())
+            ),
+            emptyList(),
+            false
+        )
+        ex.testInstance = ArrayDeque<Any>()
+        ex.results = arrayOfNulls(4)
+        ex.clocks = Array(4) { IntArray(1) }
+        ex.allThreadExecutions = arrayOfNulls(1)
+        ex.allThreadExecutions[0] = ex
+        ex.run()
+        Assert.assertArrayEquals(
+            arrayOf(
+                VoidResult,
+                ValueResult(1),
+                create(NoSuchElementException()),
+                create(NoSuchElementException())
+            ),
+            ex.results
+        )
+    }
+}
