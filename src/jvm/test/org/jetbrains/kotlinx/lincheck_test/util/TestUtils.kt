e3b67f5b476870efb20cc091664efd816e789f3e
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2025-02-20 16:04:17 +0100

    Remove inner class indices when comparing representation test data

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/util/TestUtils.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/util/TestUtils.kt
@@ -177,6 +177,8 @@ private val String.filtered: String get() {
     }.joinToString("\n")
     // Remove line numbers
     filtered = filtered.replace(LINE_NUMBER_REGEX, "")
+    // Remove inner class indexes
+    filtered = filtered.replace(INNER_CLASS_INDEX_REGEX, "")
     return filtered
 }
 
@@ -190,6 +192,7 @@ private val TEST_EXECUTION_TRACE_ELEMENT_REGEX = listOf(
 ).joinToString(separator = ")|(", prefix = "(", postfix = ")").toRegex()
 
 private val LINE_NUMBER_REGEX = Regex(":(\\d+)")
+private val INNER_CLASS_INDEX_REGEX = Regex("#(\\d+)")
 
 fun checkTraceHasNoLincheckEvents(trace: String) {
     val testPackageOccurrences = trace.split("org.jetbrains.kotlinx.lincheck_test.").size - 1
