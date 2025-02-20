cb81b5298ebb0610f1aa1116585cd82fa4eef3f5
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2025-02-20 18:43:23 +0100

    Remove repeating hyphens when comparing representation test data

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/util/TestUtils.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/util/TestUtils.kt
@@ -181,6 +181,8 @@ private val String.filtered: String get() {
     filtered = filtered.replace(INNER_CLASS_INDEX_REGEX, "")
     // Remove trailing spaces
     filtered = filtered.replace(TRAILING_STACKTRACE_SPACES, " |")
+    // Remove repeating hyphens
+    filtered = filtered.replace(REPEATING_HYPHENS, " - ")
     return filtered
 }
 
@@ -196,6 +198,7 @@ private val TEST_EXECUTION_TRACE_ELEMENT_REGEX = listOf(
 private val LINE_NUMBER_REGEX = Regex(":(\\d+)")
 private val INNER_CLASS_INDEX_REGEX = Regex("#(\\d+)")
 private val TRAILING_STACKTRACE_SPACES = Regex(" +\\|")
+private val REPEATING_HYPHENS = Regex(" -+ ")
 
 fun checkTraceHasNoLincheckEvents(trace: String) {
     val testPackageOccurrences = trace.split("org.jetbrains.kotlinx.lincheck_test.").size - 1
