4ef23ca8ca65684ffb7f4ba6a7156debe76b65b4
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2025-02-20 04:56:13 +0100

    Use true random calls for trace debugger

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/KotlinRandomRepresentationTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/KotlinRandomRepresentationTest.kt
@@ -1,8 +1,16 @@
 package org.jetbrains.kotlinx.lincheck_test.representation
 
+import org.jetbrains.kotlinx.lincheck.isInTraceDebuggerMode
+import org.junit.Assume.assumeFalse
+import org.junit.Before
 import kotlin.random.*
 
 class KotlinRandomRepresentationTest : BaseTraceRepresentationTest("kotlin_random_representation") {
+    @Before
+    fun setUp() {
+        assumeFalse(isInTraceDebuggerMode)
+    }
+    
     private var a = 0
 
     override fun operation() {
