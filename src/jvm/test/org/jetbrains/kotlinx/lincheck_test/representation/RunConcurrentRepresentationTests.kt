61f67fffab8d7aefa76f12d64994d7f743f6d70e
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-03-06 18:04:03 +0100

    Remove @Ignore from test

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/RunConcurrentRepresentationTests.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/RunConcurrentRepresentationTests.kt
@@ -374,7 +374,6 @@ class ThreadPoolRunConcurrentRepresentationTest : BaseRunConcurrentRepresentatio
     }
 }
 
-@Ignore("Unstable on many configurations, not only java 8")
 class CoroutinesRunConcurrentRepresentationTest : BaseRunConcurrentRepresentationTest<Unit>(
     "run_concurrent_test/coroutines/coroutines"
 ) {
