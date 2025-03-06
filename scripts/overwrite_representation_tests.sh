8b16ed36080ada237c8d931a8fd9e357a994c5bf
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-03-07 00:35:45 +0100

    put JDK 17 on the first place in the script
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- scripts/overwrite_representation_tests.sh
+++ scripts/overwrite_representation_tests.sh
@@ -6,7 +6,8 @@
 # This Source Code Form is subject to the terms of the
 # Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
 # with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
-#
+
+
 # This script runs all representation tests in overwrite mode.
 # It runs the tests on all jdks and both in trace and non-trace modes,
 # in the order jdk8 non-trace -> jdk8 trace -> jdk 11 non-trace -> jdk 11 trace, etc..
@@ -15,10 +16,20 @@
 # One can use this script to verify trace outputs by looking at the local changes after the script ran.
 # And commit the changes if accepted, to update test outputs.
 
-# jdk 11 is SLOW! So remove it if you don't expect files to change
 cd ../
-jdks=("8" "11" "13" "15" "17" "19" "20" "21")
+
+# We put JDK 17 on the first place because it is the default JDK version for tests.
+# By default, when run on other JDK versions, representation tests will first
+# try to compare the output with the default log file (i.e. log file for default JDK version),
+# and only if they are not equal a JDK-specific expected log file will be created.
+# Thus by first running tests on default JDK version we ensure that the default log file
+# always created at the beginning.
+#
+# JDK 11 is SLOW! So remove it if you don't expect files to change
+jdks=("17" "8" "11" "13" "15" "19" "20" "21")
+
 testFilter="org.jetbrains.kotlinx.lincheck_test.representation.*"
+
 for jdk in "${jdks[@]}" 
 do
   echo "[Representation Tests Overwrite] Running tests for jdk: $jdk in non-trace mode  ----------------------"
