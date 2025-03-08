86715db09a97ecd2fbb44c9ec7353c1cca8db8c6
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-03-08 01:42:20 +0100

    increase invocation timeout for gpmc tests
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/gpmc/ModelCheckerTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/gpmc/ModelCheckerTest.kt
@@ -77,7 +77,7 @@ private fun createStrategy(testClass: Class<*>, scenario: ExecutionScenario): Mo
 
 private fun createConfiguration(testClass: Class<*>) =
     ModelCheckingOptions()
-        .invocationTimeout(5_000) // 5 sec
+        .invocationTimeout(60_000) // 60 sec TODO: find minimal value for tests to pass
         .createTestConfigurations(testClass)
 
 private class CollectResultsVerifier : Verifier {
