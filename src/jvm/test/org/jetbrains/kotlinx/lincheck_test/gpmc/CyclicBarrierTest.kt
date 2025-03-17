49d502b48ac0f59451022d8687ee66c052779833
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-03-17 13:31:09 +0100

    Put CyclicBarrierTest into separate file

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/gpmc/CyclicBarrierTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/gpmc/CyclicBarrierTest.kt
@@ -12,35 +12,23 @@ package org.jetbrains.kotlinx.lincheck_test.gpmc
 
 import org.jetbrains.kotlinx.lincheck.ExperimentalModelCheckingAPI
 import org.jetbrains.kotlinx.lincheck.runConcurrentTest
-import org.junit.Ignore
 import org.junit.Test
 import java.util.concurrent.CyclicBarrier
 import kotlin.concurrent.thread
 
 @OptIn(ExperimentalModelCheckingAPI::class)
-class PrintingTest {
+class CyclicBarrierTest {
 
-    /**
-     * This test would pass if `println`'s were not used.
-     * See [CyclicBarrierTest] for identical test but with no printing.
-     */
-    @Ignore("Println's hang the execution")
-    @Test(timeout = TIMEOUT)
-    fun testPrints() {
-        runConcurrentTest(1) {
-            val barrier = CyclicBarrier(2) {
-                println("All threads have reached the barrier. Proceeding...")
-            }
+    @Test
+    fun testBarrier() {
+        runConcurrentTest(10000) {
+            val barrier = CyclicBarrier(2)
 
             val t1 = thread {
-                println("Thread 1 is waiting at the barrier")
                 barrier.await()
-                println("Thread 1 has passed the barrier")
             }
             val t2 = thread {
-                println("Thread 1 is waiting at the barrier")
                 barrier.await()
-                println("Thread 1 has passed the barrier")
             }
 
             t1.join()
