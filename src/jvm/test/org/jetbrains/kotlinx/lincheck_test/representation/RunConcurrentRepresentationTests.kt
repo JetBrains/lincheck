26c678594e6a0f23966f6317ada95da1d566574e
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-04-24 13:08:15 +0200

    revert "disable `CoroutinesRunConcurrentRepresentationTest` on some JDKs"
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/RunConcurrentRepresentationTests.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/RunConcurrentRepresentationTests.kt
@@ -437,13 +437,8 @@ class CoroutinesRunConcurrentRepresentationTest : BaseRunConcurrentRepresentatio
     @Before
     fun setUp() {
         assumeFalse(isInTraceDebuggerMode) // unstable hash-code
-        // TODO: investigate why test is unstable on these JDKs
+        // TODO: investigate why this test is unstable on these JDK 8
         assumeFalse(testJdkVersion == TestJdkVersion.JDK_8)
-        assumeFalse(testJdkVersion == TestJdkVersion.JDK_11)
-        assumeFalse(testJdkVersion == TestJdkVersion.JDK_13)
-        assumeFalse(testJdkVersion == TestJdkVersion.JDK_15)
-        assumeFalse(testJdkVersion == TestJdkVersion.JDK_20)
-        assumeFalse(testJdkVersion == TestJdkVersion.JDK_21)
     }
 
     companion object {
