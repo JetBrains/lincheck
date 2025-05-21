b68c24cc50dcaa5d0d64f24e65f35017b7d25483
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-05-21 02:13:31 +0200

    add test for new data structures Kotlin API
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

---
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/datastructures/KotlinAPITest.kt
@@ -0,0 +1,25 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2025 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.jetbrains.kotlinx.lincheck_test.datastructures
+
+import org.jetbrains.kotlinx.lincheck.Lincheck
+import org.junit.Test
+
+class KotlinAPITest {
+
+    @Test
+    fun hashMapTest() = Lincheck.runConcurrentDataStructureTest {
+        constructor { HashMap<String, String>() }
+
+        operation(HashMap<String, String>::get, argumentTypes = listOf(String::class))
+        operation(HashMap<String, String>::put, argumentTypes = listOf(String::class, String::class))
+    }
+}
