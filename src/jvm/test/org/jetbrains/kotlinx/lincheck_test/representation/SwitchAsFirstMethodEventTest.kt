729eb6b62f7cfee35349c4ec55b2637a8b636b74
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-06-06 03:26:20 +0200

    update expected trace log files
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/SwitchAsFirstMethodEventTest.kt
+++ src/jvm/test/org/jetbrains/kotlinx/lincheck_test/representation/SwitchAsFirstMethodEventTest.kt
@@ -10,11 +10,11 @@
 
 package org.jetbrains.kotlinx.lincheck_test.representation
 
-import kotlinx.atomicfu.*
 import org.jetbrains.kotlinx.lincheck.*
 import org.jetbrains.kotlinx.lincheck.annotations.*
 import org.jetbrains.kotlinx.lincheck.strategy.managed.modelchecking.*
 import org.jetbrains.kotlinx.lincheck_test.util.*
+import java.util.concurrent.atomic.AtomicInteger
 import org.junit.*
 
 /**
@@ -47,7 +47,7 @@ import org.junit.*
  * ```
  */
 class SwitchAsFirstMethodEventTest {
-    private val counter = atomic(0)
+    private val counter = AtomicInteger(0)
 
     @Operation
     fun incTwiceAndGet(): Int {
