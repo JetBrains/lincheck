28c2ae4815ff5162f557f297bc478f5177e6ea3e
Lev Serebryakov <lev.serebryakov@jetbrains.com>
2025-06-08 21:16:58 +0200

    Change error messages and comments to be more clear.

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/traceagent/TraceAgent.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/traceagent/TraceAgent.kt
@@ -39,20 +39,34 @@ typealias MethodVisitorProvider = (
 internal object TraceAgent {
     @JvmStatic
     fun premain(agentArgs: String?, inst: Instrumentation) {
+        /*
+         * Static agent requires one of two modes: Trace Debugger or Trace Recorder.
+         * For now, the mode is selected by system property.
+         * If you want to run Trace Debugger, you must set `-Dlincheck.traceDebuggerMode=true`.
+         * If you want to run Trace Recorder, you must set `-Dlincheck.traceRecorderMode=true`.
+         *
+         * It is an error not to set one of them, and it is an error to set both.
+         */
+        // Check if one of the required parameters is set.
         check(isInTraceDebuggerMode || isInTraceRecorderMode) {
-            "When trace debugger agent is attached to process, " +
-            "One of VM parameters `lincheck.traceDebuggerMode` or `lincheck.traceRecorderMode` is expected to be true. " +
-            "Rerun with -Dlincheck.traceDebuggerMode=true or -Dlincheck.traceRecorderMode=true."
+            "When lincheck agent is attached to process, " +
+            "mode should be selected by one of VM parameters `lincheck.traceDebuggerMode` or " +
+            "`lincheck.traceRecorderMode`. One of them expected to be `true`. " +
+            "Rerun with `-Dlincheck.traceDebuggerMode=true` or `-Dlincheck.traceRecorderMode=true`."
         }
-        check(isInTraceDebuggerMode != isInTraceRecorderMode) {
-            "When trace debugger agent is attached to process, " +
-            "Only one of VM parameters `lincheck.traceDebuggerMode` or `lincheck.traceRecorderMode` is expected to be true. " +
-            "Remove one of -Dlincheck.traceDebuggerMode=true or -Dlincheck.traceRecorderMode=true."
+        // Check that only one parameter is set: one of two must be `false`
+        check(!isInTraceDebuggerMode || !isInTraceRecorderMode) {
+            "When lincheck agent is attached to process, " +
+            "mode should be selected by one of VM parameters `lincheck.traceDebuggerMode` or " +
+            "`lincheck.traceRecorderMode`. Only one of them expected to be `true`. " +
+            "Rerun with `-Dlincheck.traceDebuggerMode=true` or `-Dlincheck.traceRecorderMode=true` but not both."
         }
         TraceAgentParameters.parseArgs(agentArgs)
         if (isInTraceDebuggerMode) {
+            // We are in Trace debugger mode
             inst.addTransformer(TraceAgentTransformer(::TraceDebuggerMethodTransformer), true)
         } else {
+            // We are in Trace Recorder mode (by exclusion)
             // This adds turn-on and turn-off of tracing to the method in question
             inst.addTransformer(TraceAgentTransformer(::TraceRecorderMethodTransformer), true)
             // This prepares instrumentation of all future classes
