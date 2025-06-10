aa41f3a683164f7387829df7a6f69095befd9194
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2025-06-10 16:42:20 +0200

    Do not apply 'ConstructorArgumentsSnapshotTrackerTransformer' when instrumentation mode is TRACE_RECORDING

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/LincheckClassVisitor.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/transformation/LincheckClassVisitor.kt
@@ -152,7 +152,8 @@ internal class LincheckClassVisitor(
                 )
             }
             mv = run {
-                val st = ConstructorArgumentsSnapshotTrackerTransformer(fileName, className, methodName, mv.newAdapter(), classVisitor::isInstanceOf)
+                val st = if (instrumentationMode == TRACE_RECORDING) mv
+                         else ConstructorArgumentsSnapshotTrackerTransformer(fileName, className, methodName, mv.newAdapter(), classVisitor::isInstanceOf)
                 val sv = SharedMemoryAccessTransformer(fileName, className, methodName, st.newAdapter())
                 val aa = AnalyzerAdapter(className, access, methodName, desc, sv)
                 sv.analyzer = aa
@@ -207,7 +208,8 @@ internal class LincheckClassVisitor(
         // which should be put in front of the byte-code transformer chain,
         // so that it can correctly analyze the byte-code and compute required type-information
         mv = run {
-            val st = ConstructorArgumentsSnapshotTrackerTransformer(fileName, className, methodName, mv.newAdapter(), classVisitor::isInstanceOf)
+            val st = if (instrumentationMode == TRACE_RECORDING) mv
+                     else ConstructorArgumentsSnapshotTrackerTransformer(fileName, className, methodName, mv.newAdapter(), classVisitor::isInstanceOf)
             val sv = SharedMemoryAccessTransformer(fileName, className, methodName, st.newAdapter())
             val aa = AnalyzerAdapter(className, access, methodName, desc, sv)
             sv.analyzer = aa
