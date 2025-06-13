b246d2790193232cebc7e7f376fa60128c6aa489
Evgeniy.Zhelenskiy <Evgeniy.Zhelenskiy@jetbrains.com>
2025-06-13 14:44:00 +0200

    Refactor trace point creation handling for shared memory access and array read operations

--- scripts/overwrite_representation_tests.sh
+++ scripts/overwrite_representation_tests.sh
@@ -34,7 +34,7 @@ testFilter="org.jetbrains.kotlinx.lincheck_test.representation.*"
 for jdk in "${jdks[@]}" 
 do
   echo "[Representation Tests Overwrite] Running tests for jdk: $jdk in non-trace mode  ----------------------"
-  ./gradlew clean jvmTest --tests "$testFilter" -PjdkToolchainVersion="$jdk" -PoverwriteRepresentationTestsOutput=true -PtestInTraceDebuggerMode=false
+  ./gradlew clean test --tests "$testFilter" -PjdkToolchainVersion="$jdk" -PoverwriteRepresentationTestsOutput=true -PtestInTraceDebuggerMode=false
 
   #https://github.com/JetBrains/lincheck/issues/500
   if [ "$jdk" = "8" ]; then
@@ -42,5 +42,5 @@ do
   fi 
   
   echo "[Representation Tests Overwrite] Running tests for jdk: $jdk in trace mode  --------------------------"
-  ./gradlew clean jvmTest --tests "$testFilter" -PjdkToolchainVersion="$jdk" -PoverwriteRepresentationTestsOutput=true -PtestInTraceDebuggerMode=true
+  ./gradlew clean test --tests "$testFilter" -PjdkToolchainVersion="$jdk" -PoverwriteRepresentationTestsOutput=true -PtestInTraceDebuggerMode=true
 done 
