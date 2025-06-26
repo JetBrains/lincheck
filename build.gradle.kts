1ee96e26eb897f94d36b38ebe38bf516a25787ce
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-06-26 15:23:40 +0200

    WIP: moving common configuration to `buildSrc`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,9 +143,7 @@ fun SourceSet.configureClasspath() {
 }
 
 kotlin {
-    compilerOptions {
-        allWarningsAsErrors = true
-    }
+    configureKotlin()
 }
 
 java {
---
+++ buildSrc/src/main/kotlin/Toolchain.kt
@@ -0,0 +1,17 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2025 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
+
+fun KotlinJvmProjectExtension.configureKotlin() {
+    compilerOptions {
+        allWarningsAsErrors.set(true)
+    }
+}
