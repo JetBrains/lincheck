8fd061b1710c2601ed38f6856618401a2eee5644
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-06-27 14:30:25 +0200

    add deprecated `org.jetbrains.kotlinx.lincheck` verifiers for backward compatibility (#735)
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

---
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/verifier/quiescent/QuiescentConsistencyVerifier.kt
@@ -0,0 +1,24 @@
+/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2025 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.jetbrains.kotlinx.lincheck.verifier.quiescent
+
+import org.jetbrains.lincheck.datastructures.verifier.QuiescentConsistencyVerifier
+import org.jetbrains.lincheck.datastructures.verifier.Verifier
+
+@Deprecated(
+    message = "Use org.jetbrains.lincheck.datastructures.verifier.QuiescentConsistencyVerifier instead.",
+    level = DeprecationLevel.WARNING
+)
+class QuiescentConsistencyVerifier
+    private constructor(val delegate: QuiescentConsistencyVerifier) : Verifier by delegate
+{
+    constructor(sequentialSpecification: Class<*>) : this(QuiescentConsistencyVerifier(sequentialSpecification))
+}
