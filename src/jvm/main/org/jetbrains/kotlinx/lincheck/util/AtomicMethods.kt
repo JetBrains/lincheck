f23e6c4a681ac0439f16ce679821e2c5a2a7318e
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-07-10 01:47:25 +0200

    group `VarHandle` class names in `AtomicMethods.kt`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/util/AtomicMethods.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/util/AtomicMethods.kt
@@ -221,9 +221,14 @@ internal fun isAtomicFieldUpdaterClass(className: String) =
 internal fun isAtomicFieldUpdaterMethod(className: String, methodName: String) =
     isAtomicFieldUpdaterClass(className) && methodName in atomicFieldUpdaterMethods
 
-private val varHandleClassNames = setOf(
+// Base VarHandle classes
+private val varHandleBaseClassNames = setOf(
     "java.lang.invoke.VarHandle",
-    "java.lang.invoke.IndirectVarHandle",
+    "java.lang.invoke.IndirectVarHandle"
+)
+
+// Segment-related VarHandle classes
+private val varHandleSegmentClassNames = setOf(
     "java.lang.invoke.VarHandleSegmentViewBase",
     "java.lang.invoke.VarHandleSegmentAsBytes",
     "java.lang.invoke.VarHandleSegmentAsChars",
@@ -231,7 +236,11 @@ private val varHandleClassNames = setOf(
     "java.lang.invoke.VarHandleSegmentAsFloats",
     "java.lang.invoke.VarHandleSegmentAsInts",
     "java.lang.invoke.VarHandleSegmentAsLongs",
-    "java.lang.invoke.VarHandleSegmentAsShorts",
+    "java.lang.invoke.VarHandleSegmentAsShorts"
+)
+
+// ByteArray-related VarHandle classes
+private val varHandleByteArrayClassNames = setOf(
     "java.lang.invoke.VarHandleByteArrayAsChars${'$'}ByteArrayViewVarHandle",
     "java.lang.invoke.VarHandleByteArrayAsChars${'$'}ArrayHandle",
     "java.lang.invoke.VarHandleByteArrayAsChars${'$'}ByteBufferHandle",
@@ -249,58 +258,79 @@ private val varHandleClassNames = setOf(
     "java.lang.invoke.VarHandleByteArrayAsLongs${'$'}ByteBufferHandle",
     "java.lang.invoke.VarHandleByteArrayAsShorts${'$'}ByteArrayViewVarHandle",
     "java.lang.invoke.VarHandleByteArrayAsShorts${'$'}ArrayHandle",
-    "java.lang.invoke.VarHandleByteArrayAsShorts${'$'}ByteBufferHandle",
-    "java.lang.invoke.VarHandleBooleans${'$'}Array",
+    "java.lang.invoke.VarHandleByteArrayAsShorts${'$'}ByteBufferHandle"
+)
+
+// Instance field VarHandle classes
+private val varHandleInstanceFieldClassNames = setOf(
     "java.lang.invoke.VarHandleBooleans${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleBooleans${'$'}FieldInstanceReadWrite",
-    "java.lang.invoke.VarHandleBooleans${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleBooleans${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleBytes${'$'}Array",
     "java.lang.invoke.VarHandleBytes${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleBytes${'$'}FieldInstanceReadWrite",
-    "java.lang.invoke.VarHandleBytes${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleBytes${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleChars${'$'}Array",
     "java.lang.invoke.VarHandleChars${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleChars${'$'}FieldInstanceReadWrite",
-    "java.lang.invoke.VarHandleChars${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleChars${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleDoubles${'$'}Array",
     "java.lang.invoke.VarHandleDoubles${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleDoubles${'$'}FieldInstanceReadWrite",
-    "java.lang.invoke.VarHandleDoubles${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleDoubles${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleFloats${'$'}Array",
     "java.lang.invoke.VarHandleFloats${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleFloats${'$'}FieldInstanceReadWrite",
-    "java.lang.invoke.VarHandleFloats${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleFloats${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleInts${'$'}Array",
     "java.lang.invoke.VarHandleInts${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleInts${'$'}FieldInstanceReadWrite",
-    "java.lang.invoke.VarHandleInts${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleInts${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleLongs${'$'}Array",
     "java.lang.invoke.VarHandleLongs${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleLongs${'$'}FieldInstanceReadWrite",
-    "java.lang.invoke.VarHandleLongs${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleLongs${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleReferences${'$'}Array",
     "java.lang.invoke.VarHandleReferences${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleReferences${'$'}FieldInstanceReadWrite",
-    "java.lang.invoke.VarHandleReferences${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleReferences${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleObjects${'$'}Array",
     "java.lang.invoke.VarHandleObjects${'$'}FieldInstanceReadOnly",
     "java.lang.invoke.VarHandleObjects${'$'}FieldInstanceReadWrite",
+    "java.lang.invoke.VarHandleShorts${'$'}FieldInstanceReadOnly",
+    "java.lang.invoke.VarHandleShorts${'$'}FieldInstanceReadWrite"
+)
+
+// Static field VarHandle classes
+private val varHandleStaticFieldClassNames = setOf(
+    "java.lang.invoke.VarHandleBooleans${'$'}FieldStaticReadOnly",
+    "java.lang.invoke.VarHandleBooleans${'$'}FieldStaticReadWrite",
+    "java.lang.invoke.VarHandleBytes${'$'}FieldStaticReadOnly",
+    "java.lang.invoke.VarHandleBytes${'$'}FieldStaticReadWrite",
+    "java.lang.invoke.VarHandleChars${'$'}FieldStaticReadOnly",
+    "java.lang.invoke.VarHandleChars${'$'}FieldStaticReadWrite",
+    "java.lang.invoke.VarHandleDoubles${'$'}FieldStaticReadOnly",
+    "java.lang.invoke.VarHandleDoubles${'$'}FieldStaticReadWrite",
+    "java.lang.invoke.VarHandleFloats${'$'}FieldStaticReadOnly",
+    "java.lang.invoke.VarHandleFloats${'$'}FieldStaticReadWrite",
+    "java.lang.invoke.VarHandleInts${'$'}FieldStaticReadOnly",
+    "java.lang.invoke.VarHandleInts${'$'}FieldStaticReadWrite",
+    "java.lang.invoke.VarHandleLongs${'$'}FieldStaticReadOnly",
+    "java.lang.invoke.VarHandleLongs${'$'}FieldStaticReadWrite",
+    "java.lang.invoke.VarHandleReferences${'$'}FieldStaticReadOnly",
+    "java.lang.invoke.VarHandleReferences${'$'}FieldStaticReadWrite",
     "java.lang.invoke.VarHandleObjects${'$'}FieldStaticReadOnly",
     "java.lang.invoke.VarHandleObjects${'$'}FieldStaticReadWrite",
-    "java.lang.invoke.VarHandleShorts${'$'}Array",
-    "java.lang.invoke.VarHandleShorts${'$'}FieldInstanceReadOnly",
-    "java.lang.invoke.VarHandleShorts${'$'}FieldInstanceReadWrite",
     "java.lang.invoke.VarHandleShorts${'$'}FieldStaticReadOnly",
-    "java.lang.invoke.VarHandleShorts${'$'}FieldStaticReadWrite",
+    "java.lang.invoke.VarHandleShorts${'$'}FieldStaticReadWrite"
 )
+
+// Array VarHandle classes
+private val varHandleArrayClassNames = setOf(
+    "java.lang.invoke.VarHandleBooleans${'$'}Array",
+    "java.lang.invoke.VarHandleBytes${'$'}Array",
+    "java.lang.invoke.VarHandleChars${'$'}Array",
+    "java.lang.invoke.VarHandleDoubles${'$'}Array",
+    "java.lang.invoke.VarHandleFloats${'$'}Array",
+    "java.lang.invoke.VarHandleInts${'$'}Array",
+    "java.lang.invoke.VarHandleLongs${'$'}Array",
+    "java.lang.invoke.VarHandleReferences${'$'}Array",
+    "java.lang.invoke.VarHandleObjects${'$'}Array",
+    "java.lang.invoke.VarHandleShorts${'$'}Array"
+)
+
+// All VarHandle class names
+private val varHandleClassNames =
+    varHandleBaseClassNames +
+    varHandleSegmentClassNames + 
+    varHandleByteArrayClassNames + 
+    varHandleInstanceFieldClassNames + 
+    varHandleStaticFieldClassNames + 
+    varHandleArrayClassNames
 
 @OptIn(ExperimentalContracts::class)
 internal fun isVarHandle(obj: Any?): Boolean {
