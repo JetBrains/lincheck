c8c3542928063c3c18616865877b1ba17bb6a15b
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-07-10 02:09:32 +0200

    implement `AtomicMethodDescriptor.getVarHandleAccessLocation`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/ObjectLocation.kt
+++ src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/ObjectLocation.kt
@@ -11,6 +11,8 @@
 package org.jetbrains.kotlinx.lincheck.strategy.managed
 
 import org.jetbrains.kotlinx.lincheck.util.*
+import org.jetbrains.kotlinx.lincheck.util.AtomicApiKind
+import org.jetbrains.kotlinx.lincheck.util.UnsafeHolder
 
 abstract class ObjectLocation
 
@@ -135,3 +137,98 @@ internal fun AtomicMethodDescriptor.getAtomicFieldUpdaterAccessLocation(receiver
     }
 }
 
+internal fun AtomicMethodDescriptor.getVarHandleAccessLocation(varHandle: Any, arguments: Array<Any?>): ObjectAccessMethodInfo {
+    require(apiKind == AtomicApiKind.VAR_HANDLE) {
+        "Method is not a VarHandle method: $this"
+    }
+    require(isVarHandle(varHandle)) {
+        "Receiver is not a recognized VarHandle type: ${varHandle.javaClass.name}"
+    }
+
+    val varHandleClassName = varHandle.javaClass.name
+    return when {
+        // Array access case
+        isVarHandleArrayClass(varHandleClassName) -> {
+            require(arguments.size >= 2) {
+                "Expected at least 2 arguments for array VarHandle, but got ${arguments.size}"
+            }
+            require(arguments[1] is Int) {
+                "Expected array index to be Int, but got ${arguments[1]?.javaClass?.name ?: "null"}"
+            }
+            val array = arguments[0]
+            val index = arguments[1] as Int
+            ObjectAccessMethodInfo(
+                obj = array,
+                location = ArrayIndexLocation(index),
+                arguments = arguments.drop(2)
+            )
+        }
+
+        // Static field access case
+        isVarHandleStaticFieldClass(varHandleClassName) -> {
+            // For static field access, we need to extract the field information from the VarHandle
+            try {
+                val baseField = varHandle.javaClass.getDeclaredField("base")
+                val baseOffset = UnsafeHolder.UNSAFE.objectFieldOffset(baseField)
+                val ownerClass = UnsafeHolder.UNSAFE.getObject(varHandle, baseOffset) as Class<*>
+
+                val fieldOffsetField = varHandle.javaClass.getDeclaredField("fieldOffset")
+                val fieldOffsetOffset = UnsafeHolder.UNSAFE.objectFieldOffset(fieldOffsetField)
+                val fieldOffset = UnsafeHolder.UNSAFE.getLong(varHandle, fieldOffsetOffset)
+
+                val fieldName = findFieldNameByOffsetViaUnsafe(ownerClass, fieldOffset)
+                    ?: error("Failed to find field name by offset $fieldOffset in ${ownerClass.name}")
+
+                ObjectAccessMethodInfo(
+                    obj = null,
+                    location = StaticFieldLocation(
+                        className = ownerClass.name,
+                        fieldName = fieldName
+                    ),
+                    arguments = arguments.toList()
+                )
+            } catch (t: Throwable) {
+                error("Failed to extract field information from static field VarHandle ${varHandleClassName}: ${t.message}")
+            }
+        }
+
+        // Instance field access case
+        isVarHandleInstanceFieldClass(varHandleClassName) -> {
+            require(arguments.isNotEmpty()) {
+                "Expected at least 1 argument for instance field VarHandle, but got ${arguments.size}"
+            }
+
+            val targetObject = arguments[0]
+            require(targetObject != null) {
+                "Target object cannot be null for instance field VarHandle"
+            }
+
+            try {
+                val receiverTypeField = varHandle.javaClass.getDeclaredField("receiverType")
+                val receiverTypeOffset = UnsafeHolder.UNSAFE.objectFieldOffset(receiverTypeField)
+                val ownerType = UnsafeHolder.UNSAFE.getObject(varHandle, receiverTypeOffset) as Class<*>
+
+                val fieldOffsetField = varHandle.javaClass.getDeclaredField("fieldOffset")
+                val fieldOffsetOffset = UnsafeHolder.UNSAFE.objectFieldOffset(fieldOffsetField)
+                val fieldOffset = UnsafeHolder.UNSAFE.getLong(varHandle, fieldOffsetOffset)
+
+                val fieldName = findFieldNameByOffsetViaUnsafe(ownerType, fieldOffset)
+                    ?: error("Failed to find field name by offset $fieldOffset in ${ownerType.name}")
+
+                ObjectAccessMethodInfo(
+                    obj = targetObject,
+                    location = ObjectFieldLocation(
+                        className = ownerType.name,
+                        fieldName = fieldName
+                    ),
+                    arguments = arguments.drop(1)
+                )
+            } catch (t: Throwable) {
+                error("Failed to extract field information from instance field VarHandle ${varHandleClassName}: ${t.message}")
+            }
+        }
+
+        // Unknown VarHandle class
+        else -> error("Unsupported VarHandle type: $varHandleClassName")
+    }
+}
