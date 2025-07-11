913a2fc20409f28f3a1132b3fa6c6025ee2cc589
Lev Serebryakov <lev.serebryakov@jetbrains.com>
2025-07-11 19:06:24 +0200

    Restore TRPlayground in tests.

---
+++ trace/src/test/org/jetbrains/lincheck/trace/util/TRPlayground.kt
@@ -0,0 +1,39 @@
+﻿/*
+ * Lincheck
+ *
+ * Copyright (C) 2019 - 2025 JetBrains s.r.o.
+ *
+ * This Source Code Form is subject to the terms of the
+ * Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+ * with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+package org.jetbrains.lincheck.trace.util
+
+import org.jetbrains.lincheck.trace.LazyTraceReader
+import org.jetbrains.lincheck.trace.TRMethodCallTracePoint
+import org.jetbrains.lincheck.trace.loadRecordedTrace
+import org.jetbrains.lincheck.trace.printRecorderTrace
+import java.io.FileInputStream
+
+fun main(args: Array<String>) {
+    // Choose one!
+
+    // eagerLoadAndPrintTrace("<path to binary file>")
+
+    // lazyLoadAndPrintTrace("<path to binary file>")
+}
+
+private fun eagerLoadAndPrintTrace(fileName: String) {
+    val trace = loadRecordedTrace(FileInputStream(fileName))
+    printRecorderTrace(System.out, trace.context, trace.roots, true)
+}
+
+private fun lazyLoadAndPrintTrace(fileName: String) {
+    val reader = LazyTraceReader(fileName)
+    val trace = reader.readRoots()
+    reader.loadChildrenRange(trace.first() as TRMethodCallTracePoint, 5, 5)
+    // Or you could run or other variant to load children
+    // reader.loadAllChildren(trace.first() as TRMethodCallTracePoint)
+    printRecorderTrace(System.out, reader.context, trace, true)
+}
