87826ba7982637c12d0fc6752d50b6b3e217bd5d
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-09-04 17:38:45 +0200

    test
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- common/src/main/org/jetbrains/lincheck/util/Logger.kt
+++ common/src/main/org/jetbrains/lincheck/util/Logger.kt
@@ -82,7 +82,7 @@ object Logger {
 
 private val LINE_SEPARATOR = System.lineSeparator()
 
-@JvmField val DEFAULT_LOG_LEVEL = LoggingLevel.OFF
+@JvmField val DEFAULT_LOG_LEVEL = LoggingLevel.WARN
 
 enum class LoggingLevel {
     DEBUG, INFO, WARN, ERROR, OFF
