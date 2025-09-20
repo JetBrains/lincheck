986fc219ad8f046ee5c3749e9e1e4124ec4861f9
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-09-20 23:18:25 +0200

    ignore non-deterministic coroutines test
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- integration-test/trace-recorder/src/main/org/jetbrains/trace/recorder/test/KotlinCoroutinesTraceRecorderIntegrationTest.kt
+++ integration-test/trace-recorder/src/main/org/jetbrains/trace/recorder/test/KotlinCoroutinesTraceRecorderIntegrationTest.kt
@@ -10,6 +10,7 @@
 
 package org.jetbrains.trace.recorder.test
 
+import org.junit.Ignore
 import org.junit.Test
 import java.nio.file.Paths
 
@@ -17,6 +18,7 @@ class KotlinCoroutinesTraceRecorderIntegrationTest : AbstractTraceRecorderIntegr
     override val projectPath: String = Paths.get("build", "integrationTestProjects", "kotlinx.coroutines").toString()
 
     @Test
+    @Ignore("Flaky test")
     fun `kotlinx_coroutines_ExecutorsTest testDefaultDispatcherToExecutor`() {
         runGradleTest(
             testClassName = "kotlinx.coroutines.ExecutorsTest",
