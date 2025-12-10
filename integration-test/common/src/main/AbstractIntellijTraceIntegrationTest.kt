e10c96c89f2f4715fdabba1b266f11ae2543076b
Bob.Brockbernd <bobbrockbernd@gmail.com>
2025-12-10 11:04:16 +0100

    Apply suggested buildList change

--- integration-test/common/src/main/AbstractIntellijTraceIntegrationTest.kt
+++ integration-test/common/src/main/AbstractIntellijTraceIntegrationTest.kt
@@ -30,18 +30,19 @@ abstract class AbstractIntellijTraceIntegrationTest: AbstractTraceIntegrationTes
         // We need to escape it twice, as our argument parser will de-escape it when split into array
         val pathToOutput = outputFile.absolutePath.escape().escape()
         
-        val cmd = mutableListOf(
-            "./tests.cmd",
-            "-Dintellij.build.test.patterns=${testClassName.escapeDollar()}",
-            "-Dintellij.build.test.trace.recorder.enabled=true",
-            "-Dintellij.build.test.trace.recorder.className=${testClassName.escapeDollar()}",
-            "-Dintellij.build.test.trace.recorder.methodName=${testMethodName.escapeDollar()}",
-            "-Dintellij.build.test.trace.recorder.traceDump=$pathToOutput",
-            "-Dintellij.build.test.trace.recorder.agentJar=$pathToFatJar",
-            "-Dintellij.build.test.jvm.memory.options=-Xmx4g"
-        )
-        extraAgentArgs["format"]?.let { cmd.add("-Dintellij.build.test.trace.recorder.format=$it") }
-        extraAgentArgs["formatOption"]?.let { cmd.add("-Dintellij.build.test.trace.recorder.formatOption=$it") }
+        val cmd = buildList {
+            add("./tests.cmd")
+            add("-Dintellij.build.test.patterns=${testClassName.escapeDollar()}")
+            add("-Dintellij.build.test.trace.recorder.enabled=true")
+            add("-Dintellij.build.test.trace.recorder.className=${testClassName.escapeDollar()}")
+            add("-Dintellij.build.test.trace.recorder.methodName=${testMethodName.escapeDollar()}")
+            add("-Dintellij.build.test.trace.recorder.traceDump=$pathToOutput")
+            add("-Dintellij.build.test.trace.recorder.agentJar=$pathToFatJar")
+            add("-Dintellij.build.test.jvm.memory.options=-Xmx4g")
+
+            extraAgentArgs["format"]?.let { add("-Dintellij.build.test.trace.recorder.format=$it") }
+            extraAgentArgs["formatOption"]?.let { add("-Dintellij.build.test.trace.recorder.formatOption=$it") }
+        }
 
         ProcessBuilder(cmd).directory(File(projectPath))
             .inheritIO().start().waitFor()
