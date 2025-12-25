f3d113c7263d897c8ef8a9b332219691b0fa4cc8
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2025-12-25 19:36:01 +0100

    WIP: JMX controller for trace recording
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- trace-recorder/src/main/org/jetbrains/lincheck/trace/recorder/jmx/TraceRecorderJmxController.kt
+++ trace-recorder/src/main/org/jetbrains/lincheck/trace/recorder/jmx/TraceRecorderJmxController.kt
@@ -14,11 +14,35 @@ import org.jetbrains.lincheck.jvm.agent.LincheckInstrumentation
 import org.jetbrains.lincheck.trace.jmx.TracingJmxController
 import org.jetbrains.lincheck.trace.recorder.TraceRecorder
 import org.jetbrains.lincheck.util.Logger
+import java.lang.management.ManagementFactory
+import javax.management.ObjectName
+import javax.management.StandardMBean
 
 /**
  * Provides a JMX controller interface implementation for the [TraceRecorder].
  */
 object TraceRecorderJmxController : TracingJmxController {
+
+    fun register() {
+        try {
+            val mbs = ManagementFactory.getPlatformMBeanServer()
+            val objectName = ObjectName(MBEAN_OBJECT_NAME)
+
+            if (mbs.isRegistered(objectName)) {
+                Logger.error { "JMX MBean already registered at $MBEAN_OBJECT_NAME" }
+                return
+            }
+
+            val mbean = StandardMBean(this, TracingJmxController::class.java)
+            mbs.registerMBean(mbean, objectName)
+
+            Logger.info { "JMX MBean registered successfully at $MBEAN_OBJECT_NAME" }
+        } catch (e: Exception) {
+            Logger.error { "Failed to register JMX MBean" }
+            Logger.error(e)
+        }
+    }
+    
     override fun install(format: String?, formatOption: String?, traceDumpFilePath: String?) {
         TraceRecorder.install(format, formatOption, traceDumpFilePath, LincheckInstrumentation.context)
     }
@@ -54,3 +78,5 @@ object TraceRecorderJmxController : TracingJmxController {
         TraceRecorder.uninstall()
     }
 }
+
+private const val MBEAN_OBJECT_NAME = "org.jetbrains.lincheck:type=TracingController"
