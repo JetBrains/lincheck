7d5156d432eb2f451822f0a39c1abf96de3cdfdf
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2026-03-19 12:54:10 +0100

    cache class reflection data via `ClassValue`
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- common/src/main/org/jetbrains/lincheck/util/Reflection.kt
+++ common/src/main/org/jetbrains/lincheck/util/Reflection.kt
@@ -13,13 +13,11 @@ package org.jetbrains.lincheck.util
 import java.lang.reflect.Field
 import java.util.concurrent.ConcurrentHashMap
 
-
-// We store the class references in a local map to avoid repeated Class.forName calls and reflection overhead
-
 /**
  * Provides a thread-safe caching mechanism for [Class] lookup by its fully qualified class name.
  */
 object ClassCache {
+    // We store the class references in a local map to avoid repeated Class.forName calls and reflection overhead
     private val cache = ConcurrentHashMap<String, Class<*>>()
 
     /**
@@ -38,7 +36,7 @@ object ClassCache {
  * Returns the companion object's class of this class if it exists.
  */
 val Class<*>.companionClass: Class<*>? get() =
-    declaredFields.firstOrNull { it.name == "Companion" }?.type
+    ClassReflectionCache.getCompanionClass(this)
 
 /**
  * Returns all declared fields in the class, including fields from superclasses and interfaces.
@@ -46,24 +44,8 @@ val Class<*>.companionClass: Class<*>? get() =
  * Multiple fields with the same name and the same type may be returned
  * if they appear in the subclass and a parent class.
  */
-val Class<*>.allDeclaredFields get(): List<Field> {
-    if (superclass == null && interfaces.isEmpty()) {
-        return this.declaredFields.asList()
-    }
-
-    val fields: MutableList<Field> = mutableListOf()
-    val queue: MutableList<Class<*>> = mutableListOf(this)
-    while (queue.isNotEmpty()) {
-        val currentClass = queue.removeLast()
-        fields.addAll(currentClass.declaredFields)
-
-        queue.addAll(currentClass.interfaces)
-        if (currentClass.superclass != null) {
-            queue.add(currentClass.superclass)
-        }
-    }
-    return fields
-}
+val Class<*>.allDeclaredFields get(): List<Field> =
+    ClassReflectionCache.getAllDeclaredFields(this)
 
 /**
  * Finds the field name of [this] object that directly references the given object [obj].
@@ -79,4 +61,50 @@ fun Any.findInstanceFieldReferringTo(obj: Any): Field? {
         }
     }
     return null
+}
+
+/**
+ * A utility object for caching class reflection metadata in a thread-safe and gc-friendly manner.
+ *
+ * The implementation relies on a [ClassValue] to lazily compute the cached values.
+ */
+private object ClassReflectionCache {
+
+    private val allDeclaredFieldsCache = object : ClassValue<List<Field>>() {
+        override fun computeValue(type: Class<*>): List<Field> =
+            computeAllDeclaredFields(type)
+    }
+
+    private fun computeAllDeclaredFields(clazz: Class<*>): List<Field> {
+        if (clazz.superclass == null && clazz.interfaces.isEmpty()) {
+            return clazz.declaredFields.asList()
+        }
+
+        val fields: MutableList<Field> = mutableListOf()
+        val queue: MutableList<Class<*>> = mutableListOf(clazz)
+        while (queue.isNotEmpty()) {
+            val currentClass = queue.removeLast()
+            fields.addAll(currentClass.declaredFields)
+
+            queue.addAll(currentClass.interfaces)
+            if (currentClass.superclass != null) {
+                queue.add(currentClass.superclass)
+            }
+        }
+        return fields
+    }
+
+    fun getAllDeclaredFields(clazz: Class<*>): List<Field> =
+        allDeclaredFieldsCache.get(clazz)
+
+    private val companionClassCache = object : ClassValue<Class<*>?>() {
+        override fun computeValue(type: Class<*>): Class<*>? =
+            computeCompanionClass(type)
+    }
+
+    private fun computeCompanionClass(clazz: Class<*>): Class<*>? =
+        clazz.declaredFields.firstOrNull { it.name == "Companion" }?.type
+
+    fun getCompanionClass(clazz: Class<*>): Class<*>? =
+        companionClassCache.get(clazz)
 }
