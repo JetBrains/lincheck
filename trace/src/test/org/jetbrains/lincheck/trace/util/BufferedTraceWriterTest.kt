61d51c9d0da32187e79f509be0eb89ea79c03249
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2026-04-02 21:13:06 +0200

    Add method descriptor deduplication check to the BufferedTraceWriterTest

--- trace/src/test/org/jetbrains/lincheck/trace/util/BufferedTraceWriterTest.kt
+++ trace/src/test/org/jetbrains/lincheck/trace/util/BufferedTraceWriterTest.kt
@@ -28,6 +28,8 @@ class BufferedTraceWriterTest {
         val context = TraceContext()
         val tr1 = createBasicMethodCallTracePoint(context, 1, "com.example.SomeClass", "methodName1")
         val tr2 = createBasicMethodCallTracePoint(context, 2, "com.example.SomeClass", "methodName2")
+        val sharedCall1 = createBasicMethodCallTracePoint(context, tr1.threadId, "com.example.SomeClass", "sharedMethod")
+        val sharedCall2 = createBasicMethodCallTracePoint(context, tr2.threadId, "com.example.SomeClass", "sharedMethod")
 
         val traceFile = File.createTempFile("trace_test", ".trace").apply { deleteOnExit() }
         val collector = FileStreamingTraceCollecting(traceFile.absolutePath, context)
@@ -38,6 +40,11 @@ class BufferedTraceWriterTest {
         val t1 = thread(name = "TestThread-1") {
             collector.registerCurrentThread(tr1.threadId)
             collector.tracePointCreated(parent = null, tr1)
+
+            // Add nested shared method call
+            collector.tracePointCreated(parent = tr1, sharedCall1)
+            collector.completeContainerTracePoint(Thread.currentThread(), sharedCall1)
+
             collector.completeContainerTracePoint(Thread.currentThread(), tr1)
 
             latch.countDown()
@@ -51,6 +58,11 @@ class BufferedTraceWriterTest {
         val t2 = thread(name = "TestThread-2") {
             collector.registerCurrentThread(tr2.threadId)
             collector.tracePointCreated(parent = null, tr2)
+
+            // Add nested shared method call (same method as in thread 1)
+            collector.tracePointCreated(parent = tr2, sharedCall2)
+            collector.completeContainerTracePoint(Thread.currentThread(), sharedCall2)
+
             collector.completeContainerTracePoint(Thread.currentThread(), tr2)
 
             barrier.await()
@@ -66,19 +78,24 @@ class BufferedTraceWriterTest {
         collector.traceEnded()
 
         val loadedTrace = loadRecordedTrace(traceFile.absolutePath)
+        printRecorderTrace(System.out, loadedTrace.context, loadedTrace.roots, true)
         val blocks = collectSavedBlocks(loadedTrace.context, traceFile)
         check(blocks.size == 2 && blocks.containsKey(1) && blocks.containsKey(2)) { "Expected 2 blocks for both threads, got thread ids: ${blocks.keys}" }
 
-        val expectedClassDescriptorIds = setOf(0)
-        val expectedStringIds1 = setOf(0 /* Example.java */, 1 /* com.example.SomeClass */, 2 /* methodName1 */)
-        val expectedStringIds2 = setOf(0 /* Example.java */, 1 /* com.example.SomeClass */, 3 /* methodName2 */)
+        val expectedClassDescriptorIds = setOf(0 /* SomeClass */)
+        val expectedMethodDescriptorIds1 = setOf(0 /* methodName1 */, 2 /* sharedMethod */)
+        val expectedMethodDescriptorIds2 = setOf(1 /* methodName2 */, 2 /* sharedMethod */)
+        val expectedStringIds1 = setOf(0 /* Example.java */, 1 /* com.example.SomeClass */, 2 /* methodName1 */, 3 /* sharedMethod */)
+        val expectedStringIds2 = setOf(0 /* Example.java */, 1 /* com.example.SomeClass */, 4 /* methodName2 */, 3 /* sharedMethod */)
 
         blocks[1]!!.run {
             checkClassDescriptors(expectedClassDescriptorIds)
+            checkMethodDescriptors(expectedMethodDescriptorIds1)
             checkStrings(expectedStringIds1)
         }
         blocks[2]!!.run {
             checkClassDescriptors(expectedClassDescriptorIds)
+            checkMethodDescriptors(expectedMethodDescriptorIds2)
             checkStrings(expectedStringIds2)
         }
     }
@@ -88,6 +105,8 @@ class BufferedTraceWriterTest {
         val context = TraceContext()
         val tr1 = createBasicMethodCallTracePoint(context, 1, "com.example.SomeClass", "methodName1")
         val tr2 = createBasicMethodCallTracePoint(context, 2, "com.example.SomeClass", "methodName2")
+        val sharedCall1 = createBasicMethodCallTracePoint(context, tr1.threadId, "com.example.SomeClass", "sharedMethod")
+        val sharedCall2 = createBasicMethodCallTracePoint(context, tr2.threadId, "com.example.SomeClass", "sharedMethod")
 
         val traceFile = File.createTempFile("trace_test", ".trace").apply { deleteOnExit() }
         val collector = FileStreamingTraceCollecting(traceFile.absolutePath, context)
@@ -97,6 +116,11 @@ class BufferedTraceWriterTest {
         val t1 = thread(name = "TestThread-1") {
             collector.registerCurrentThread(tr1.threadId)
             collector.tracePointCreated(parent = null, tr1)
+
+            // Add nested shared method call
+            collector.tracePointCreated(parent = tr1, sharedCall1)
+            collector.completeContainerTracePoint(Thread.currentThread(), sharedCall1)
+
             collector.completeContainerTracePoint(Thread.currentThread(), tr1)
             collector.completeThread(Thread.currentThread())
         }
@@ -108,6 +132,11 @@ class BufferedTraceWriterTest {
             latch.await()
 
             collector.tracePointCreated(parent = null, tr2)
+
+            // Add nested shared method call (same method as in thread 1)
+            collector.tracePointCreated(parent = tr2, sharedCall2)
+            collector.completeContainerTracePoint(Thread.currentThread(), sharedCall2)
+
             collector.completeContainerTracePoint(Thread.currentThread(), tr2)
             collector.completeThread(Thread.currentThread())
         }
@@ -123,16 +152,20 @@ class BufferedTraceWriterTest {
         val blocks = collectSavedBlocks(loadedTrace.context, traceFile)
         check(blocks.size == 2 && blocks.containsKey(1) && blocks.containsKey(2)) { "Expected 2 blocks for both threads, got thread ids: ${blocks.keys}" }
 
-        val expectedClassDescriptorIds = setOf(0)
-        val expectedStringIds1 = setOf(0 /* Example.java */, 1 /* com.example.SomeClass */, 2 /* methodName1 */)
-        val expectedStringIds2 = setOf(3 /* methodName2 */)
+        val expectedClassDescriptorIds1 = setOf(0 /* SomeClass */)
+        val expectedMethodDescriptorIds1 = setOf(0 /* methodName1 */, 2 /* sharedMethod */)
+        val expectedStringIds1 = setOf(0 /* Example.java */, 1 /* com.example.SomeClass */, 2 /* methodName1 */, 3 /* sharedMethod */)
+        val expectedMethodDescriptorIds2 = setOf(1 /* methodName2 */)
+        val expectedStringIds2 = setOf(4 /* methodName2 */)
 
         blocks[1]!!.run {
-            checkClassDescriptors(expectedClassDescriptorIds)
+            checkClassDescriptors(expectedClassDescriptorIds1)
+            checkMethodDescriptors(expectedMethodDescriptorIds1)
             checkStrings(expectedStringIds1)
         }
         blocks[2]!!.run {
             checkClassDescriptors(emptySet())
+            checkMethodDescriptors(expectedMethodDescriptorIds2)
             checkStrings(expectedStringIds2)
         }
     }
@@ -146,6 +179,12 @@ class BufferedTraceWriterTest {
     private fun BlockAnalysis.checkClassDescriptors(expected: Set<Int>) {
         check(classDescriptors == expected) {
             "Thread must have saved the following class descriptors: $expected, got: $classDescriptors"
+        }
+    }
+
+    private fun BlockAnalysis.checkMethodDescriptors(expected: Set<Int>) {
+        check(methodDescriptors == expected) {
+            "Thread must have saved the following method descriptors: $expected, got: $methodDescriptors"
         }
     }
 
@@ -190,9 +229,9 @@ class BufferedTraceWriterTest {
         var currentBlock: BlockAnalysis? = null
 
         dataInput.use { dataInput ->
+            val tracePointsStack = mutableListOf<TRTracePoint>()
             while (true) {
-                val kindByte = dataInput.readByte()
-                when (val kind = ObjectKind.entries[kindByte.toInt()]) {
+                when (val kind = dataInput.readKind()) {
                     ObjectKind.BLOCK_START -> {
                         val threadId = dataInput.readInt()
                         currentBlock = BlockAnalysis(threadId)
@@ -212,7 +251,7 @@ class BufferedTraceWriterTest {
                             Logger.info { "Block ended for thread ${currentBlock!!.threadId}" }
                             Logger.info { "  - ClassDescriptors: ${currentBlock!!.classDescriptors}" }
                             Logger.info { "  - MethodDescriptors: ${currentBlock!!.methodDescriptors}" }
-                            Logger.info { "  - Strings: ${currentBlock!!.strings.size}" }
+                            Logger.info { "  - Strings: ${currentBlock!!.strings}" }
                             Logger.info { "  - CodeLocations: ${currentBlock!!.codeLocations}\n" }
                             currentBlock = null
                         }
@@ -250,11 +289,8 @@ class BufferedTraceWriterTest {
                         val tr = loadTRTracePoint(loadedContext, dataInput)
                         Logger.info { "  Tracepoint: ${tr.toText(true)}" }
 
-                        check(dataInput.readKind() == ObjectKind.TRACEPOINT_FOOTER) {
-                            "Simplistic reader expected to consume TRACEPOINT_FOOTER"
-                        }
                         if (tr is TRContainerTracePoint) {
-                            tr.loadFooter(dataInput)
+                            tracePointsStack.add(tr)
                         }
                     }
 
@@ -267,6 +303,12 @@ class BufferedTraceWriterTest {
                     ObjectKind.EOF -> {
                         Logger.info { "End of file" }
                         break
+                    }
+
+                    ObjectKind.TRACEPOINT_FOOTER -> {
+                        check(tracePointsStack.isNotEmpty()) { "Tracepoint footer without container trace point" }
+                        val back = tracePointsStack.removeLast() as TRContainerTracePoint
+                        back.loadFooter(dataInput)
                     }
 
                     else -> {
