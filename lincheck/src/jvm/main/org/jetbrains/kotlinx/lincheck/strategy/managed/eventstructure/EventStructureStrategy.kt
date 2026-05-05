viliamDokov/kotlinx-lincheck

Keeping track of thread fork events for LambdaRunner in EventStructureStrategy.kt

src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/EventStructureStrategy.kt
@@ -354,9 +354,11 @@ internal class EventStructureStrategy(
 
     private fun registerTestInstance() {
         check(!isTestInstanceRegistered)
-        val testInstance = (runner as ExecutionScenarioRunner).testInstance
+        val testInstance = (runner as? ExecutionScenarioRunner)?.testInstance
         //NOTE: The threadID may be messed up. See how this can be fixed.
-        (objectTracker as EventStructureObjectTracker).registerExternalObject(testInstance)
+        if(testInstance != null) {
+            (objectTracker as EventStructureObjectTracker).registerExternalObject(testInstance)
+        }
         isTestInstanceRegistered = true
     }
 
@@ -365,6 +367,19 @@ internal class EventStructureStrategy(
         if (threadId != eventStructure.mainThreadId && threadId != eventStructure.initThreadId) {
             eventStructure.addThreadStartEvent(threadId)
         }
+    }
+
+    override fun beforeThreadStart(
+        threadDescriptor: ThreadDescriptor,
+        startingThread: Thread,
+        startingThreadDescriptor: ThreadDescriptor
+    ) : ThreadId {
+        val newThreadId = super.beforeThreadStart(threadDescriptor, startingThread, startingThreadDescriptor)
+        if ( newThreadId != -1 || newThreadId != eventStructure.mainThreadId ) {
+            val currentThreadId = threadScheduler.getCurrentThreadId()
+            eventStructure.addThreadForkEvent(currentThreadId, setOf(newThreadId))
+        }
+        return newThreadId
     }
 
     override fun onThreadFinish(threadId: Int) {

JetBrains/lincheck

lincheck/src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/EventStructureStrategy.kt
@@ -350,11 +350,13 @@ internal class EventStructureStrategy(
 
     private fun registerTestInstance() {
         check(!isTestInstanceRegistered)
-        val testInstance = (runner as ExecutionScenarioRunner).testInstance
+        val testInstance = (runner as? ExecutionScenarioRunner)?.testInstance
         //NOTE: Since the IdentityHashCode of the test instance changes between invocations
         // we cannot keep it as an external object. Instead, we register it as an "internal"
         // object on each invocation
-        (objectTracker as EventStructureObjectTracker).registerNewObject(testInstance)
+        if(testInstance != null) {
+            (objectTracker as EventStructureObjectTracker).registerNewObject(testInstance)
+        }
         isTestInstanceRegistered = true
     }
 
@@ -363,6 +365,19 @@ internal class EventStructureStrategy(
         if (threadId != eventStructure.mainThreadId && threadId != eventStructure.initThreadId) {
             eventStructure.addThreadStartEvent(threadId)
         }
+    }
+
+    override fun beforeThreadStart(
+        threadDescriptor: ThreadDescriptor,
+        startingThread: Thread,
+        startingThreadDescriptor: ThreadDescriptor
+    ) : ThreadId {
+        val newThreadId = super.beforeThreadStart(threadDescriptor, startingThread, startingThreadDescriptor)
+        if ( newThreadId != -1 || newThreadId != eventStructure.mainThreadId ) {
+            val currentThreadId = threadScheduler.getCurrentThreadId()
+            eventStructure.addThreadForkEvent(currentThreadId, setOf(newThreadId))
+        }
+        return newThreadId
     }
 
     override fun onThreadFinish(threadId: Int) {