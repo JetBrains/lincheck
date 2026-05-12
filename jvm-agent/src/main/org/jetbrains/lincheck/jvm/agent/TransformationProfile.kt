0328e207f44c2729f4ef0af2479eef1ef7727ddb
Dmitrii Artiukhov <artuhovdmitrii@gmail.com>
2026-05-12 19:58:20 +0200

    Fix transformation profile application for TR

--- jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/TransformationProfile.kt
+++ jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/TransformationProfile.kt
@@ -224,6 +224,14 @@ class FilteredTransformationProfile(
     private val excludeRegexes: List<Regex> = excludeClasses.map { it.toGlobRegex() }
 
     override fun shouldTransform(className: String): Boolean {
+        // Check the base profile first, because it sorts out
+        // some standard library classes, like `java/util/regex/Matcher` which is used
+        // in the excludeRegexes/includeRegexes lists.
+        // An attempt to transform it leads to the ClassCircularityError
+        if (!baseProfile.shouldTransform(className)) {
+            return false
+        }
+
         // exclude has a higher priority
         if (excludeRegexes.any { it.matches(className) }) {
             return false
@@ -234,8 +242,8 @@ class FilteredTransformationProfile(
             return false
         }
 
-        // otherwise, delegate decision to the base profile
-        return baseProfile.shouldTransform(className)
+        // otherwise, transform the class
+        return true
     }
 
     override fun getMethodConfiguration(className: String, methodName: String, descriptor: String): TransformationConfiguration {
