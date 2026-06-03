937413f957c79c750d885f2648844c32b2852e6b
Evgeniy Moiseenko <evgeniy.moiseenko@jetbrains.com>
2026-06-03 00:17:24 +0000

    Track field accesses inside constructors

--- jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/transformers/SharedMemoryAccessTransformer.kt
+++ jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/transformers/SharedMemoryAccessTransformer.kt
@@ -21,7 +21,6 @@ import org.jetbrains.lincheck.descriptors.toType
 import org.jetbrains.lincheck.descriptors.FieldKind
 import org.jetbrains.lincheck.trace.TraceContext
 import org.jetbrains.lincheck.trace.createAndRegisterFieldDescriptor
-import org.jetbrains.lincheck.util.isInTraceRecorderMode
 import org.objectweb.asm.MethodVisitor
 import sun.nio.ch.lincheck.*
 
@@ -49,9 +48,7 @@ internal class SharedMemoryAccessTransformer(
         if (
             isCoroutineInternalClass(owner.toCanonicalClassName()) ||
             isCoroutineStateMachineClass(owner.toCanonicalClassName()) ||
-            // TODO: JBRes-6844 Further investigate constructors support for lincheck.
-            //       In lincheck mode we do not fully support tracking events inside constructor bodies, some tests fail.
-            (!isInTraceRecorderMode && methodName == "<init>" && className == owner)
+            (methodName == "<init>" && isPlatformConstructorClass(className.toCanonicalClassName()))
         ) {
             super.visitFieldInsn(opcode, owner, fieldName, desc)
             return
