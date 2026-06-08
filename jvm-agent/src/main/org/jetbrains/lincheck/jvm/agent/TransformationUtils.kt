570fa5189f8e64e806c039012c5d7c5128cbff94
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2026-06-08 19:33:39 +0200

    fix IntelliJ plugin regression
    Root cause: in the commit 59550fac3 the thread descriptor and ignored sections machinery were changed -- the injection functions are expected to call `getCurrentThreadDescriptorIfInAnalyzedCode()` to both obtain current thread descriptor and check if the current thread is currently in analyzed code (i.e., not inside ignored section).
    
    `Injections::shouldInvokeBeforeEvent` was also modified to use this new function.
    However, it was called from injected instrumentation util function `invokeBeforeEvent` which wraps the whole injected block into an ignored section. As such, the whole `shouldInvokeBeforeEvent` was effectively skipped, and `beforeEvent` calls were never called, breaking the Lincheck <-> IntelliJ Debugger communication protocol.
    
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/TransformationUtils.kt
+++ jvm-agent/src/main/org/jetbrains/lincheck/jvm/agent/TransformationUtils.kt
@@ -293,27 +293,29 @@ private val Type.requiresBoxing: Boolean
  * The injected method **must** be called from the user code due to the contract
  * between the IDEA plugin and Lincheck.
  */
-internal fun GeneratorAdapter.invokeBeforeEvent(debugMessage: String) = invokeInsideIgnoredSection {
+internal fun GeneratorAdapter.invokeBeforeEvent(debugMessage: String) {
     ifStatement(
         condition = {
             invokeStatic(Injections::shouldInvokeBeforeEvent)
         },
         thenClause = {
-            push(debugMessage)
-            invokeStatic(Injections::getCurrentEventId)
-            dup()
-            ifStatement(
-                condition = {
-                    invokeStatic(Injections::isBeforeEventRequested)
-                },
-                thenClause = {
-                    push(debugMessage)
-                    invokeStatic(Injections::beforeEvent)
-                },
-                elseClause = {
-                    pop()
-                }
-            )
+            invokeInsideIgnoredSection {
+                push(debugMessage)
+                invokeStatic(Injections::getCurrentEventId)
+                dup()
+                ifStatement(
+                    condition = {
+                        invokeStatic(Injections::isBeforeEventRequested)
+                    },
+                    thenClause = {
+                        push(debugMessage)
+                        invokeStatic(Injections::beforeEvent)
+                    },
+                    elseClause = {
+                        pop()
+                    }
+                )
+            }
         }
     )
 }
