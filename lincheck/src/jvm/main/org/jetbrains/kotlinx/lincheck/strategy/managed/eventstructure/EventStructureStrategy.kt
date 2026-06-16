9b6b6e5b3e53a968348c65a972fd8cc07188d477
Wiliam Dokov <viliam.dokov@gmail.com>
2026-06-16 18:31:48 +0200

    Enable loop detector for EventStructureStrategy.kt

--- lincheck/src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/EventStructureStrategy.kt
+++ lincheck/src/jvm/main/org/jetbrains/kotlinx/lincheck/strategy/managed/eventstructure/EventStructureStrategy.kt
@@ -297,14 +297,21 @@ internal class EventStructureStrategy(
     override fun chooseThread(iThread: Int): Int {
         // see comment in `shouldSwitch` method
         // TODO: make scheduling strategy configurable
-        return if (currentExecutionPart == ExecutionPart.PARALLEL)
-            switchableThreads(iThread).first()
+        return if (currentExecutionPart == ExecutionPart.PARALLEL) {
+            val threads = switchableThreads(iThread)
+            // We prefer to switch to a non-livelocked thread
+            val preferredChoice = threads.firstOrNull { !threadScheduler.isLiveLocked(it) }
+            // If there is no such option, we choose the first livelocked thread instead
+            val choice = threads.firstOrNull()
+            preferredChoice ?: choice ?: -1
+        }
         else
             eventStructure.mainThreadId
     }
 
     override fun isActive(iThread: Int): Boolean {
-        return super.isActive(iThread) && (eventStructure.inReplayPhase() implies {
+        // Note: We may want to switch to a livelocked thread in case it you can schedule it
+        return (super.isActive(iThread) || threadScheduler.isLiveLocked(iThread)) && (eventStructure.inReplayPhase() implies {
             eventStructure.inReplayPhase(iThread) && eventStructure.canReplayNextEvent(iThread)
         })
     }
