6e0550c4e4b4161f588554fec9e464f83785fd75
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2026-06-22 14:43:46 +0200

    cosmetics
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- common/src/main/org/jetbrains/lincheck/util/Libs.kt
+++ common/src/main/org/jetbrains/lincheck/util/Libs.kt
@@ -212,8 +212,12 @@ internal fun isStackTraceElementClass(className: String): Boolean =
 internal fun isJavaUtilArraysClass(className: String): Boolean =
     className == "java.util.Arrays"
 
+/**
+ * Checks whether the provided [className] corresponds to the [kotlin.collections.ArraysUtilJVM] file.
+ */
 internal fun isKotlinArraysUtilJVMClass(className: String): Boolean =
     className == "kotlin.collections.ArraysUtilJVM"
+
 /**
  * Checks if the provided class name matches the [jdk.internal.access.JavaLangAccess] class.
  */
