1259a16f7298f80d20dc6dff410daf4ac5071ba1
Evgeniy Moiseenko <evg.moiseenko94@gmail.com>
2026-06-23 13:51:17 +0200

    try to disable `LocksTest.testSynchronizedIncrement3` for debugging purposes
    Signed-off-by: Evgeniy Moiseenko <evg.moiseenko94@gmail.com>

--- lincheck/src/jvm/test/org/jetbrains/kotlinx/lincheck_test/strategy/eventstructure/LocksTest.kt
+++ lincheck/src/jvm/test/org/jetbrains/kotlinx/lincheck_test/strategy/eventstructure/LocksTest.kt
@@ -12,6 +12,7 @@ package org.jetbrains.kotlinx.lincheck_test.strategy.eventstructure
 
 import org.jetbrains.kotlinx.lincheck.execution.parallelResults
 import org.jetbrains.lincheck.datastructures.scenario
+import org.junit.Ignore
 import org.junit.Test
 import java.util.concurrent.atomic.AtomicInteger
 import kotlin.concurrent.thread
@@ -49,6 +50,8 @@ class LocksTest {
     }
 
     @Test
+    @Ignore // TODO: triggers weird JDK-21 hung bugs,
+            //   likely due to some Java/Kotlin arrays/collections classes instrumentation
     fun testSynchronizedIncrement3() {
         val expectedOutcomes: Set<List<Int>> = setOf(
             listOf(0,1,2,3),
